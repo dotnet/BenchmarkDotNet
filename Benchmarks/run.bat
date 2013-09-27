b5c154dd88514975af8b8bb611cd30b9e25dd251
Andrey Akinshin <andrey.akinshin@gmail.com>
2013-09-27 18:51:09 +0700

    Add Mono support to run.bat

--- Benchmarks/run.bat
+++ Benchmarks/run.bat
@@ -2,34 +2,60 @@ SET NAME=Benchmarks.exe
 SET RUNFOLDER=bin\Runs\%TIME:~0,2%%TIME:~3,2%%TIME:~6,2%
 mkdir %RUNFOLDER%
 cd %RUNFOLDER%
-..\..\Builds\v3.5-x86\%Name% %* -of=v3.5-x86.txt
-..\..\Builds\v4.0-x86\%Name% %* -of=v4.0-x86.txt
-..\..\Builds\v4.5-x86\%Name% %* -of=v4.5-x86.txt
-..\..\Builds\v3.5-x64\%Name% %* -of=v3.5-x64.txt
-..\..\Builds\v4.0-x64\%Name% %* -of=v4.0-x64.txt
-..\..\Builds\v4.5-x64\%Name% %* -of=v4.5-x64.txt
+..\..\Builds\v3.5-x86\%Name% %* -of=ms-v3.5-x86.txt
+..\..\Builds\v4.0-x86\%Name% %* -of=ms-v4.0-x86.txt
+..\..\Builds\v4.5-x86\%Name% %* -of=ms-v4.5-x86.txt
+..\..\Builds\v3.5-x64\%Name% %* -of=ms-v3.5-x64.txt
+..\..\Builds\v4.0-x64\%Name% %* -of=ms-v4.0-x64.txt
+..\..\Builds\v4.5-x64\%Name% %* -of=ms-v4.5-x64.txt
+mono ..\..\Builds\v3.5-x86\%Name% %* -of=mono-v3.5-x86.txt
+mono ..\..\Builds\v4.0-x86\%Name% %* -of=mono-v4.0-x86.txt
+mono ..\..\Builds\v4.5-x86\%Name% %* -of=mono-v4.5-x86.txt
+mono ..\..\Builds\v3.5-x64\%Name% %* -of=mono-v3.5-x64.txt
+mono ..\..\Builds\v4.0-x64\%Name% %* -of=mono-v4.0-x64.txt
+mono ..\..\Builds\v4.5-x64\%Name% %* -of=mono-v4.5-x64.txt
 
 echo Total Results for '%*' > total.txt
 echo. >> total.txt
 
-echo v3.5-x86 >> total.txt
-cat v3.5-x86.txt >> total.txt
+echo ms-v3.5-x86 >> total.txt
+cat ms-v3.5-x86.txt >> total.txt
 echo. >> total.txt
-echo v4.0-x86 >> total.txt
-cat v4.0-x86.txt >> total.txt
+echo ms-v4.0-x86 >> total.txt
+cat ms-v4.0-x86.txt >> total.txt
 echo. >> total.txt
-echo v4.5-x86 >> total.txt
-cat v4.5-x86.txt >> total.txt
+echo ms-v4.5-x86 >> total.txt
+cat ms-v4.5-x86.txt >> total.txt
 echo. >> total.txt
 
-echo v3.5-x64 >> total.txt
-cat v3.5-x64.txt >> total.txt
+echo ms-v3.5-x64 >> total.txt
+cat ms-v3.5-x64.txt >> total.txt
 echo. >> total.txt
-echo v4.0-x64 >> total.txt
-cat v4.0-x64.txt >> total.txt
+echo ms-v4.0-x64 >> total.txt
+cat ms-v4.0-x64.txt >> total.txt
 echo. >> total.txt
-echo v4.5-x64 >> total.txt
-cat v4.5-x64.txt >> total.txt
+echo ms-v4.5-x64 >> total.txt
+cat ms-v4.5-x64.txt >> total.txt
+echo. >> total.txt
+
+echo mono-v3.5-x86 >> total.txt
+cat mono-v3.5-x86.txt >> total.txt
+echo. >> total.txt
+echo mono-v4.0-x86 >> total.txt
+cat mono-v4.0-x86.txt >> total.txt
+echo. >> total.txt
+echo mono-v4.5-x86 >> total.txt
+cat mono-v4.5-x86.txt >> total.txt
+echo. >> total.txt
+
+echo mono-v3.5-x64 >> total.txt
+cat mono-v3.5-x64.txt >> total.txt
+echo. >> total.txt
+echo mono-v4.0-x64 >> total.txt
+cat mono-v4.0-x64.txt >> total.txt
+echo. >> total.txt
+echo mono-v4.5-x64 >> total.txt
+cat mono-v4.5-x64.txt >> total.txt
 echo. >> total.txt
 
 cd ..\..\..
