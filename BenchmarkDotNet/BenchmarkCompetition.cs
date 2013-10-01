d4317b4a5ea7de695fe07afdcf2ad6b5042d7023
Andrey Akinshin <andrey.akinshin@gmail.com>
2013-10-02 00:54:48 +0700

    Add MedianTicks to CompetitionResult report in DetailedMode fixed #1

--- BenchmarkDotNet/BenchmarkCompetition.cs
+++ BenchmarkDotNet/BenchmarkCompetition.cs
@@ -43,14 +43,16 @@ namespace BenchmarkDotNet
         {
             var nameWidth = tasks.Max(task => task.Name.Length) + 1;
             var msWidth = tasks.Max(task => task.Info.Result.MedianMilliseconds.ToString().Length);
+            var ticksWidth = tasks.Max(task => task.Info.Result.MedianTicks.ToString().Length);
             var stdDevWidth = tasks.Max(task => string.Format("{0:0.00}", task.Info.Result.StandardDeviationMilliseconds).Length);
             foreach (var task in tasks)
             {
                 if (BenchmarkSettings.Instance.DetailedMode)
-                    ConsoleHelper.WriteLineStatistic("{0}: {1}ms [Error = {2:00.00}%, StdDev = {3}]",
+                    ConsoleHelper.WriteLineStatistic("{0}: {1}ms {2} ticks [Error = {3:00.00}%, StdDev = {4}]",
                         task.Name.PadRight(nameWidth),
                         task.Info.Result.MedianMilliseconds.ToString().PadLeft(msWidth),
-                        task.Info.Result.Error*100,
+                        task.Info.Result.MedianTicks.ToString().PadLeft(ticksWidth),
+                        task.Info.Result.Error * 100,
                         string.Format("{0:0.00}", task.Info.Result.StandardDeviationMilliseconds).PadLeft(stdDevWidth));
                 else
                     ConsoleHelper.WriteLineStatistic("{0}: {1}ms",
