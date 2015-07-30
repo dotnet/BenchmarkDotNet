a8e6411c47ed65dcd122155efbe9b56df158a245
Andrey Akinshin <andrey.akinshin@gmail.com>
2015-07-30 10:11:22 +0300

    Fixes in Jit_BoolToInt

--- BenchmarkDotNet.Samples/Jit_BoolToInt.cs
+++ BenchmarkDotNet.Samples/Jit_BoolToInt.cs
@@ -10,42 +10,52 @@ namespace BenchmarkDotNet.Samples
     [Task(platform: BenchmarkPlatform.X64, jitVersion: BenchmarkJitVersion.RyuJit)]
     public class Jit_BoolToInt
     {
-        private bool first;
-        private bool second;
+        private bool p1, p2, p3, p4, p5, p6;
+        public int q1, q2, q3, q4, q5, q6;
 
         public Jit_BoolToInt()
         {
-            first = true;
-            second = false;
+            p1 = p3 = p5 = true;
+            p2 = p4 = p6 = false;
         }
 
         [Benchmark]
-        public int Framework()
+        [OperationsPerInvoke(6)]
+        public void Framework()
         {
-            int sum = Convert.ToInt32(first);
-            sum += Convert.ToInt32(second);
-            return sum;
+            q1 = Convert.ToInt32(p1);
+            q2 = Convert.ToInt32(p2);
+            q3 = Convert.ToInt32(p3);
+            q4 = Convert.ToInt32(p4);
+            q5 = Convert.ToInt32(p5);
+            q6 = Convert.ToInt32(p6);
         }
 
         [Benchmark]
-        public int IfThenElse()
+        [OperationsPerInvoke(6)]
+        public void IfThenElse()
         {
-            int sum = first ? 1 : 0;
-            sum += second ? 1 : 0;
-            return sum;
+            q1 = p1 ? 1 : 0;
+            q2 = p2 ? 1 : 0;
+            q3 = p3 ? 1 : 0;
+            q4 = p4 ? 1 : 0;
+            q5 = p5 ? 1 : 0;
+            q6 = p6 ? 1 : 0;
         }
 
         [Benchmark]
-        public int UnsafeConvert()
+        [OperationsPerInvoke(6)]
+        public void UnsafeConvert()
         {
             unsafe
             {
-                bool v1 = first;
-                int sum = *(byte*)(&v1);
-
-                bool v2 = second;
-                sum += *(byte*)(&v2);
-                return sum;
+                bool v1 = p1, v2 = p2, v3 = p3, v4 = p4, v5 = p5, v6 = p6;
+                q1 = *(byte*)(&v1);
+                q2 = *(byte*)(&v2);
+                q3 = *(byte*)(&v3);
+                q4 = *(byte*)(&v4);
+                q5 = *(byte*)(&v5);
+                q6 = *(byte*)(&v6);
             }
         }
     }
