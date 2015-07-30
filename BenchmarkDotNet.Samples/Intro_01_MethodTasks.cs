adc6c232a2b12360282930f858965a864af68a4d
Andrey Akinshin <andrey.akinshin@gmail.com>
2015-07-30 10:56:31 +0300

    Fix Intro_01_MethodTasks

--- BenchmarkDotNet.Samples/Intro_01_MethodTasks.cs
+++ BenchmarkDotNet.Samples/Intro_01_MethodTasks.cs
@@ -1,24 +1,23 @@
 ﻿using System;
+using System.Threading;
 using BenchmarkDotNet.Tasks;
 
 namespace BenchmarkDotNet.Samples
 {
     // For some methods, logic may differ for different environments.
     public class Intro_01_MethodTasks
-    {        
+    {
         [Benchmark]
         // In this case, you can declare several tasks with the Task attribute.
         // For example, we can run the benchmark methods for the x86 and x64 platforms.
         [BenchmarkTask(platform: BenchmarkPlatform.X86)]
         [BenchmarkTask(platform: BenchmarkPlatform.X64)]
-        public double Foo()
+        public void Foo()
         {
-            int sum = 0;
             // x86: IntPtr.Size == 4
             // x64: IntPtr.Size == 8
             for (int i = 0; i < IntPtr.Size; i++)
-                sum += i;
-            return sum;
+                Thread.Sleep(100);
         }
     }
 }
