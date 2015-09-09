36e0608e8cfbc7ab9d95c6c02c2c0f21e45f08c7
Matt Warren <matt.warren@live.co.uk>
2015-09-09 18:12:22 +0100

    Integration tests and sample for Params attribute

---
+++ BenchmarkDotNet.IntegrationTests/ParamsAttributeTest.cs
@@ -0,0 +1,93 @@
+﻿using BenchmarkDotNet.Tasks;
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using Xunit;
+
+namespace BenchmarkDotNet.IntegrationTests
+{
+    public class ParamsTestProperty : IntegrationTestBase
+    {
+        [Params(1, 2, 3, 8, 9, 10)]
+        public int ParamProperty { get; set; }
+
+        private HashSet<int> collectedParams = new HashSet<int>();
+
+        [Fact]
+        public void Test()
+        {
+            var reports = new BenchmarkRunner().RunCompetition(new ParamsTestProperty());
+            foreach (var param in new[] { 1, 2, 3, 8, 9, 10 })
+                Assert.Contains($"// ### New Parameter {param} ###" +  Environment.NewLine, GetTestOutput());
+        }
+
+        [Benchmark]
+        [BenchmarkTask(mode: BenchmarkMode.SingleRun, processCount: 1, warmupIterationCount: 1, targetIterationCount: 1)]
+        public void Benchmark()
+        {
+            if (collectedParams.Contains(ParamProperty) == false)
+            {
+                Console.WriteLine($"// ### New Parameter {ParamProperty} ###");
+                collectedParams.Add(ParamProperty);
+            }
+            Thread.Sleep(5 + ParamProperty);
+        }
+    }
+
+    public class ParamsTestField : IntegrationTestBase
+    {
+        [Params(1, 2, 3, 8, 9, 10)]
+        public int ParamField = 0;
+
+        private HashSet<int> collectedParams = new HashSet<int>();
+
+        [Fact]
+        public void Test()
+        {
+            var reports = new BenchmarkRunner().RunCompetition(new ParamsTestField());
+            foreach (var param in new[] { 1, 2, 3, 8, 9, 10 })
+                Assert.Contains($"// ### New Parameter {param} ###" + Environment.NewLine, GetTestOutput());
+        }
+
+        [Benchmark]
+        [BenchmarkTask(mode: BenchmarkMode.SingleRun, processCount: 1, warmupIterationCount: 1, targetIterationCount: 1)]
+        public void Benchmark()
+        {
+            if (collectedParams.Contains(ParamField) == false)
+            {
+                Console.WriteLine($"// ### New Parameter {ParamField} ###");
+                collectedParams.Add(ParamField);
+            }
+            Thread.Sleep(5 + ParamField);
+        }
+    }
+
+    // Delibrately made everything "static" (as well as using a Field) to ensure that Params also work okay in this scenario
+    public class ParamsTestStaticField : IntegrationTestBase
+    {
+        [Params(1, 2, 3, 8, 9, 10)]
+        public static int StaticParamField = 0;
+
+        private static HashSet<int> collectedParams = new HashSet<int>();
+
+        [Fact]
+        public static void Test()
+        {
+            var reports = new BenchmarkRunner().RunCompetition(new ParamsTestField());
+            foreach (var param in new[] { 1, 2, 3, 8, 9, 10 })
+                Assert.Contains($"// ### New Parameter {param} ###" + Environment.NewLine, GetTestOutput());
+        }
+
+        [Benchmark]
+        [BenchmarkTask(mode: BenchmarkMode.SingleRun, processCount: 1, warmupIterationCount: 1, targetIterationCount: 1)]
+        public static void Benchmark()
+        {
+            if (collectedParams.Contains(StaticParamField) == false)
+            {
+                Console.WriteLine($"// ### New Parameter {StaticParamField} ###");
+                collectedParams.Add(StaticParamField);
+            }
+            Thread.Sleep(5 + StaticParamField);
+        }
+    }
+}
--- BenchmarkDotNet.Samples/Program.cs
+++ BenchmarkDotNet.Samples/Program.cs
@@ -13,13 +13,16 @@ namespace BenchmarkDotNet.Samples
         static void Main(string[] args)
         {
             var competitionSwitch = new BenchmarkCompetitionSwitch(new[] {
+                // Introduction
                 typeof(Intro_00_Basic),
                 typeof(Intro_01_MethodTasks),
                 typeof(Intro_02_ClassTasks),
                 typeof(Intro_03_SingleRun),
                 typeof(Intro_04_UniformReportingTest),
+                // IL
                 typeof(Il_ReadonlyFields),
                 typeof(Il_Switch),
+                // JIT
                 typeof(Jit_LoopUnrolling),
                 typeof(Jit_ArraySumLoopUnrolling),
                 typeof(Jit_Inlining),
@@ -27,20 +30,25 @@ namespace BenchmarkDotNet.Samples
                 typeof(Jit_Bce),
                 typeof(Jit_InterfaceMethod),
                 typeof(Jit_RegistersVsStack),
+                // CPU
                 typeof(Cpu_Ilp_Inc),
                 typeof(Cpu_Ilp_Max),
                 typeof(Cpu_Ilp_VsBce),
                 typeof(Cpu_Ilp_RyuJit),
                 typeof(Cpu_MatrixMultiplication),
                 typeof(Cpu_BranchPerdictor),
+                // Framework
                 typeof(Framework_SelectVsConvertAll),
                 typeof(Framework_StackFrameVsStackTrace),
                 typeof(Framework_StopwatchVsDateTime),
-                typeof(Math_DoubleSqrt),
-                typeof(Math_DoubleSqrtAvx),
+                // Algorithms
                 typeof(Algo_BitCount),
                 typeof(Algo_MostSignificantBit),
-                typeof(Algo_Md5VsSha256)
+                typeof(Algo_Md5VsSha256),
+                // Other
+                typeof(Math_DoubleSqrt),
+                typeof(Math_DoubleSqrtAvx),
+                typeof(Array_HeapAllocVsStackAlloc),
             });
             competitionSwitch.Run(args);
         }
