e73ce4fe6348436968e426f17819d51417d4f4a1
Andrey.Akinshin <andrey.akinshin@jetbrains.com>
2015-10-17 14:54:18 +0300

    Update Intro_03_SingleRun

--- BenchmarkDotNet.Samples/Introduction/Intro_03_SingleRun.cs
+++ BenchmarkDotNet.Samples/Introduction/Intro_03_SingleRun.cs
@@ -26,6 +26,15 @@ namespace BenchmarkDotNet.Samples.Introduction
                 x[i]++;
         }
 
+        [Benchmark]
+        [BenchmarkTask(5, mode: BenchmarkMode.Throughput, platform: BenchmarkPlatform.X86, warmupIterationCount: 0, targetIterationCount: 10)]
+        [OperationsPerInvoke(N / 16)]
+        public void Transition()
+        {
+            for (int i = 0; i < x.Length; i += 16)
+                x[i]++;
+        }
+
         // See also: https://msdn.microsoft.com/en-us/library/cc656914.aspx
         // See also: http://en.wikipedia.org/wiki/CPU_cache
     }
