724970753111c96a62bfbc9d58d7954fb2847c54
Federico Andres Lois <federico.lois@corvalius.com>
2015-10-27 16:09:02 -0300

    Added and special case which we know it must not be optimized at all (for comparison).

--- BenchmarkDotNet.Samples/JIT/Jit_RotateBits.cs
+++ BenchmarkDotNet.Samples/JIT/Jit_RotateBits.cs
@@ -11,22 +11,44 @@ namespace BenchmarkDotNet.Samples.JIT
     //[BenchmarkTask(platform: BenchmarkPlatform.X64, runtime: BenchmarkRuntime.CoreClr)]
     public class Jit_RotateBits
     {
-        private ulong a = 2340988;
-        private ulong b = 123444;
-        private ulong c = 1;
-        private ulong d = 23444111111;
+        private ulong au = 2340988;
+        private ulong bu = 123444;
+        private ulong cu = 1;
+        private ulong du = 23444111111;
+
+        private long ax = 2340988;
+        private long bx = 123444;
+        private long cx = 1;
+        private long dx = 23444111111;
 
         [Benchmark]
-        [OperationsPerInvoke(4)]        
-        public void RotateBits()
+        [OperationsPerInvoke(4)]
+        public void ShouldOptimize()
         {
-            RotateRight64(a, 16);
-            RotateRight64(b, 24);
-            RotateRight64(c, 32);
-            RotateRight64(d, 48);
+            RotateRight64(au, 16);
+            RotateRight64(bu, 24);
+            RotateRight64(cu, 32);
+            RotateRight64(du, 48);
         }
-        
+
+        [Benchmark]
+        [OperationsPerInvoke(4)]
+        public void ShouldNotOptimize()
+        {
+            RotateRight64(ax, 16);
+            RotateRight64(bx, 24);
+            RotateRight64(cx, 32);
+            RotateRight64(dx, 48);
+        }
+
         public static ulong RotateRight64(ulong value, int count)
+        {
+            return (value >> count) | (value << (64 - count));
+        }
+
+        /// This case should not be optimized because signed Right-Shift have special treatment for the negative case.
+        /// https://github.com/dotnet/coreclr/issues/1619#issuecomment-151609929
+        public static long RotateRight64(long value, int count)
         {
             return (value >> count) | (value << (64 - count));
         }
