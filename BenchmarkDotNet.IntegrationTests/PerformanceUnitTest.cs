7c9c6d75b7a77b0b5ab2ca4e3c74e8d0cd105e05
Matt Warren <matt.warren@live.co.uk>
2015-11-24 21:56:16 +0000

    Helper methods for Performance Unit Tests

--- BenchmarkDotNet.IntegrationTests/PerformanceUnitTest.cs
+++ BenchmarkDotNet.IntegrationTests/PerformanceUnitTest.cs
@@ -1,4 +1,5 @@
-﻿using BenchmarkDotNet.Tasks;
+﻿using BenchmarkDotNet.Extensions;
+using BenchmarkDotNet.Tasks;
 using System;
 using System.Linq;
 using System.Threading;
@@ -19,13 +20,9 @@ namespace BenchmarkDotNet.IntegrationTests
             Assert.Contains("// ### Slow Benchmark called ###" + Environment.NewLine, testOutput);
             Assert.Contains("// ### Fast Benchmark called ###" + Environment.NewLine, testOutput);
 
-            // TODO Wrap this up in some helper methods, so it's easier to get the reports/runs for a given method
-            var slowBenchmarkReport = reports.First(r => r.Benchmark.Target.Method.Name == "SlowBenchmark");
-            var slowBenchmarkRun = slowBenchmarkReport.Runs.First();
-            var fastBenchmarkReport = reports.First(r => r.Benchmark.Target.Method.Name == "FastBenchmark");
-            var fastBenchmarkRun = fastBenchmarkReport.Runs.First();
-
             // Check that slow benchmark is actually slower than the fast benchmark!
+            var slowBenchmarkRun = reports.GetRunsFor<PerformanceUnitTest>(r => r.SlowBenchmark()).First();
+            var fastBenchmarkRun = reports.GetRunsFor<PerformanceUnitTest>(r => r.FastBenchmark()).First();
             Assert.True(slowBenchmarkRun.AverageNanoseconds > fastBenchmarkRun.AverageNanoseconds,
                         string.Format("Expected SlowBenchmark: {0:N2} ns to be MORE than FastBenchmark: {1:N2} ns",
                                       slowBenchmarkRun.AverageNanoseconds, fastBenchmarkRun.AverageNanoseconds));
@@ -36,13 +33,15 @@ namespace BenchmarkDotNet.IntegrationTests
             // Whilst we're at it, let's do more specific Asserts as we know what the elasped time should be
             Assert.InRange(slowBenchmarkRun.AverageNanoseconds / 1000.0 / 1000.0, low: 499, high: 501);
             Assert.InRange(fastBenchmarkRun.AverageNanoseconds / 1000.0 / 1000.0, low: 4.5, high: 5.5);
+            var slowBenchmarkReport = reports.GetReportFor<PerformanceUnitTest>(r => r.SlowBenchmark());
+            var fastBenchmarkReport = reports.GetReportFor<PerformanceUnitTest>(r => r.FastBenchmark());
         }
 
         [Benchmark]
         public void FastBenchmark()
         {
             Console.WriteLine("// ### Fast Benchmark called ###");
-            Thread.Sleep(5);
+            Thread.Sleep(15);
         }
 
         [Benchmark]
---
+++ BenchmarkDotNet/Extensions/ReportExtensions.cs
@@ -0,0 +1,28 @@
+﻿using BenchmarkDotNet.Reports;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace BenchmarkDotNet.Extensions
+{
+    public static class ReportExtensions
+    {
+        public static BenchmarkReport GetReportFor<T>(this IEnumerable<BenchmarkReport> reports, Expression<Action<T>> actionExp)
+        {
+            if (actionExp.Body == null)
+                throw new ArgumentException("Extend a an Expression with a valid Body", "actionExp");
+
+            var methodExp = actionExp.Body as MethodCallExpression;
+            if (methodExp == null)
+                throw new ArgumentException("Extend a MethodCallExpression, but got a " + actionExp.Body.GetType().Name, "actionExp");
+
+            return reports.First(r => r.Benchmark.Target.Method == methodExp.Method);
+        }
+
+        public static IList<BenchmarkRunReport> GetRunsFor<T>(this IEnumerable<BenchmarkReport> reports, Expression<Action<T>> actionExp)
+        {
+            return reports.GetReportFor<T>(actionExp).Runs;
+        }
+    }
+}
