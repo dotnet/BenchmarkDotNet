a2d0eca30026ffdcce7f528b27c0cd9bab91e614
asmirnova <alinasmirnova915@gmail.com>
2016-01-15 23:56:34 +0600

    Separate class for string extensions.

---
+++ BenchmarkDotNet/Extensions/StringExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace BenchmarkDotNet.Extensions
+{
+    public static class StringExtensions
+    {
+        internal static string WithoutSuffix(this string str, string suffix, StringComparison stringComparison = StringComparison.CurrentCulture)
+        {
+            return str.EndsWith(suffix, stringComparison) ? str.Substring(0, str.Length - suffix.Length) : str;
+        }
+
+        public static string ToCamelCase(this string value)
+        {
+            return string.IsNullOrEmpty(value) ? value : char.ToLowerInvariant(value[0]) + value.Substring(1);
+        }
+
+    }
+}
