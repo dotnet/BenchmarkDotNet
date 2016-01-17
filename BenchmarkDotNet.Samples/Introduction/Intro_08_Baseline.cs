e3f420817979008aba4369bd8e87897354c28d2e
Andrey Akinshin <andrey.akinshin@gmail.com>
2016-01-17 15:26:01 +0200

    Samples: add Intro_08_Baseline

---
+++ BenchmarkDotNet.Samples/Introduction/Intro_08_Baseline.cs
@@ -0,0 +1,27 @@
+﻿using System.Threading;
+using BenchmarkDotNet.Tasks;
+
+namespace BenchmarkDotNet.Samples.Introduction
+{
+    [BenchmarkTask(1, BenchmarkMode.SingleRun, targetIterationCount: 1, warmupIterationCount: 0)]
+    public class Intro_08_Baseline
+    {
+        [Benchmark]
+        public void Time50()
+        {
+            Thread.Sleep(50);
+        }
+
+        [Benchmark(Baseline = true)]
+        public void Time100()
+        {
+            Thread.Sleep(100);
+        }
+
+        [Benchmark]
+        public void Time150()
+        {
+            Thread.Sleep(150);
+        }
+    }
+}
