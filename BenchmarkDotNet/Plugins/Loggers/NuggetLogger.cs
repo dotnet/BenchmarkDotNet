e51d4d296350b276777769cfb838f45f9e2afcd4
Adam Sitnik <adam.sitnik@gmail.com>
2016-01-25 01:15:40 +0100

    Road to DNX: alternative to MS dnu. reuses nuget and roslyn but as for now it FAILS

---
+++ BenchmarkDotNet/Plugins/Loggers/NuggetLogger.cs
@@ -0,0 +1,41 @@
+﻿#if DNX451
+using ILogger = NuGet.Logging.ILogger;
+
+namespace BenchmarkDotNet.Plugins.Loggers
+{
+    internal class NugetLogger : ILogger
+    {
+        public NugetLogger(IBenchmarkLogger benchmarkLogger)
+        {
+            this.BenchmarkLogger = benchmarkLogger;
+        }
+
+        private IBenchmarkLogger BenchmarkLogger { get; }
+
+        public void LogDebug(string data)
+        {
+            BenchmarkLogger.WriteLine(BenchmarkLogKind.Default, data);
+        }
+
+        public void LogVerbose(string data)
+        {
+            BenchmarkLogger.WriteLine(BenchmarkLogKind.Default, data);
+        }
+
+        public void LogInformation(string data)
+        {
+            BenchmarkLogger.WriteLineInfo(data);
+        }
+
+        public void LogWarning(string data)
+        {
+            BenchmarkLogger.WriteLine(BenchmarkLogKind.Default, data); // todo: maybe we should add warnings?
+        }
+
+        public void LogError(string data)
+        {
+            BenchmarkLogger.WriteLineError(data);
+        }
+    }
+}
+#endif
