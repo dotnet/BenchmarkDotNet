ef41f685e7c11447c9182f849692bc29c5114260
Andrey Akinshin <andrey.akinshin@gmail.com>
2016-02-08 20:09:56 +0200

    Fix a bug in ClassicBuilder

--- BenchmarkDotNet/Toolchains/Classic/ClassicBuilder.cs
+++ BenchmarkDotNet/Toolchains/Classic/ClassicBuilder.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using BenchmarkDotNet.Loggers;
 using BenchmarkDotNet.Toolchains.Results;
@@ -18,11 +19,33 @@ namespace BenchmarkDotNet.Toolchains.Classic
             lock (buildLock)
             {
                 var projectFileName = Path.Combine(generateResult.DirectoryPath, ClassicGenerator.MainClassName + ".csproj");
+                var exeFileName = Path.Combine(generateResult.DirectoryPath, ClassicGenerator.MainClassName + ".exe");
                 var consoleLogger = new MsBuildConsoleLogger(logger);
                 var globalProperties = new Dictionary<string, string>();
                 var buildRequest = new BuildRequestData(projectFileName, globalProperties, null, new[] { "Build" }, null);
                 var buildParameters = new BuildParameters(new ProjectCollection()) { DetailedSummary = false, Loggers = new Microsoft.Build.Framework.ILogger[] { consoleLogger } };
                 var buildResult = BuildManager.DefaultBuildManager.Build(buildParameters, buildRequest);
+
+                if (buildResult.OverallResult != BuildResultCode.Success && !File.Exists(exeFileName))
+                {
+                    logger.WriteLineInfo("BuildManager.DefaultBuildManager can't build this project. =(");
+                    logger.WriteLineInfo("Let's try to build it via BuildBenchmark.bat!");
+                    var buildProcess = new Process
+                    {
+                        StartInfo =
+                        {
+                            FileName = Path.Combine(generateResult.DirectoryPath, "BuildBenchmark.bat"),
+                            WorkingDirectory = generateResult.DirectoryPath,
+                            UseShellExecute = false,
+                            RedirectStandardOutput = false,
+                        }
+                    };
+                    buildProcess.Start();
+                    buildProcess.WaitForExit();
+                    if (File.Exists(exeFileName))
+                        return new BuildResult(generateResult, true, null);
+                }
+
                 return new BuildResult(generateResult, buildResult.OverallResult == BuildResultCode.Success, buildResult.Exception);
             }
         }
