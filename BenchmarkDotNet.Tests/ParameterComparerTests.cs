74789dac32b63dc68ef9fc9a5e76a9b3b6cdc47a
Matt Warren <matt.warren@live.co.uk>
2016-02-11 11:19:07 +0000

    Sort results by Parameter, in a Natural Sort Order
    See http://blog.codinghorror.com/sorting-for-humans-natural-sort-order/
    for an explanation of "Natural Sort Order"

---
+++ BenchmarkDotNet.Tests/ParameterComparerTests.cs
@@ -0,0 +1,127 @@
+﻿using BenchmarkDotNet.Parameters;
+using BenchmarkDotNet.Reports;
+using System.Linq;
+using Xunit;
+
+namespace BenchmarkDotNet.Tests
+{
+    public class ParameterComparerTests
+    {
+        [Fact]
+        public void BasicComparisionTest()
+        {
+            var comparer = new ParameterComparer();
+
+            var sharedDefinition = new ParameterDefinition("Testing", isStatic: false, values: new object[0]);
+            var originalData = new[]
+            {
+                new ParameterInstances(new []
+                {
+                    new ParameterInstance(sharedDefinition, 5),
+                }),
+                new ParameterInstances(new []
+                {
+                    new ParameterInstance(sharedDefinition, 1),
+                }),
+                new ParameterInstances(new []
+                {
+                    new ParameterInstance(sharedDefinition, 3),
+                })
+            };
+            var sortedData = originalData.OrderBy(d => d, comparer).ToArray();
+
+            Assert.Equal(1, sortedData[0].Items[0].Value);
+            Assert.Equal(3, sortedData[1].Items[0].Value);
+            Assert.Equal(5, sortedData[2].Items[0].Value);
+        }
+
+        [Fact]
+        public void MultiParameterComparisionTest()
+        {
+            var comparer = new ParameterComparer();
+
+            var sharedDefinition = new ParameterDefinition("Testing", isStatic: false, values: new object[0]);
+            var originalData = new []
+            {
+                new ParameterInstances(new []
+                {
+                    new ParameterInstance(sharedDefinition, 5),
+                    new ParameterInstance(sharedDefinition, "z"),
+                    new ParameterInstance(sharedDefinition, 1.0),
+                }),
+                new ParameterInstances(new []
+                {
+                    new ParameterInstance(sharedDefinition, 5),
+                    new ParameterInstance(sharedDefinition, "a"),
+                    new ParameterInstance(sharedDefinition, 0.0),
+                }),
+                new ParameterInstances(new []
+                {
+                    new ParameterInstance(sharedDefinition, 5),
+                    new ParameterInstance(sharedDefinition, "a"),
+                    new ParameterInstance(sharedDefinition, 1.0),
+                }),
+                new ParameterInstances(new []
+                {
+                    new ParameterInstance(sharedDefinition, 3),
+                    new ParameterInstance(sharedDefinition, "a"),
+                    new ParameterInstance(sharedDefinition, 97.5)
+                })
+            };
+
+            // We should sort by the parameters in order, i.e. first by 5/3, then if they match by "z"/"a"
+            var sortedData = originalData.OrderBy(d => d, comparer).ToArray();
+
+            Assert.Equal(3, sortedData[0].Items[0].Value);
+            Assert.Equal("a", sortedData[0].Items[1].Value);
+            Assert.Equal(97.5, sortedData[0].Items[2].Value);
+
+            Assert.Equal(5, sortedData[1].Items[0].Value);
+            Assert.Equal("a", sortedData[1].Items[1].Value);
+            Assert.Equal(0.0, sortedData[1].Items[2].Value);
+
+            Assert.Equal(5, sortedData[2].Items[0].Value);
+            Assert.Equal("a", sortedData[2].Items[1].Value);
+            Assert.Equal(1.0, sortedData[2].Items[2].Value);
+
+            Assert.Equal(5, sortedData[3].Items[0].Value);
+            Assert.Equal("z", sortedData[3].Items[1].Value);
+            Assert.Equal(1.0, sortedData[3].Items[2].Value);
+        }
+
+        [Fact]
+        public void AlphaNumericComparisionTest()
+        {
+            var comparer = new ParameterComparer();
+
+            var sharedDefinition = new ParameterDefinition("Testing", isStatic: false, values: new object[0]);
+            var originalData = new[]
+            {
+                new ParameterInstances(new []
+                {
+                    new ParameterInstance(sharedDefinition, 100),
+                }),
+                new ParameterInstances(new []
+                {
+                    new ParameterInstance(sharedDefinition, 1000),
+                }),
+                new ParameterInstances(new []
+                {
+                    new ParameterInstance(sharedDefinition, 2000),
+                }),
+                new ParameterInstances(new []
+                {
+                    new ParameterInstance(sharedDefinition, 500),
+                })
+            };
+
+            var sortedData = originalData.OrderBy(d => d, comparer).ToArray();
+
+            // Check that we sory by numeric value, not string order!!
+            Assert.Equal(100, sortedData[0].Items[0].Value);
+            Assert.Equal(500, sortedData[1].Items[0].Value);
+            Assert.Equal(1000, sortedData[2].Items[0].Value);
+            Assert.Equal(2000, sortedData[3].Items[0].Value);
+        }
+    }
+}
---
+++ BenchmarkDotNet/Reports/ParameterComparer.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using BenchmarkDotNet.Parameters;
+
+namespace BenchmarkDotNet.Reports
+{
+    internal class ParameterComparer : IComparer<ParameterInstances>
+    {
+        public int Compare(ParameterInstances x, ParameterInstances y)
+        {
+            if (x.Items.Count == 0 || y.Items.Count == 0)
+                return x.FullInfo.CompareTo(y.FullInfo);
+
+            // Have a hard-stop, so we don't loop forever (5 Param fields/properties should be enough)!!!
+            var skip = 0;
+            while (skip < 5)
+            {
+                var xItem = x.Items.Skip(skip).FirstOrDefault();
+                var yItem = y.Items.Skip(skip).FirstOrDefault();
+                skip++;
+
+                if (xItem != null && yItem != null)
+                {
+                    // If x and y match (at the same position), goto the next position
+                    if (CompareTo(xItem, yItem) == 0)
+                        continue;
+
+                    return CompareTo(xItem, yItem);
+                }
+                else
+                {
+                    // If we can't get a value, use the fallback comparison
+                    return x.FullInfo.CompareTo(y.FullInfo);
+                }
+            }
+
+            // Fallback comparison
+            return x.FullInfo.CompareTo(y.FullInfo);
+        }
+
+        private int CompareTo(ParameterInstance x, ParameterInstance y)
+        {
+            // This is a best-effort and we only try and compare if the types match!!
+            if (x.Value.GetType() != y.Value.GetType())
+                return x.Value.ToString().CompareTo(y.Value.ToString());
+
+            if (x.Value is string && y.Value is string)
+                return x.Value.ToString().CompareTo(y.Value.ToString());
+
+            // We will only worry about common, basic types, i.e. int, long, double, etc
+            // (e.g. you can't write [Params(10.0m, 20.0m, 100.0m, 200.0m)], the compiler won't let you!)
+            if (x.Value is int && y.Value is int)
+                return ((int)x.Value).CompareTo((int)y.Value);
+            if (x.Value is long && y.Value is long)
+                return ((long)x.Value).CompareTo((long)y.Value);
+            if (x.Value is short && y.Value is short)
+                return ((short)x.Value).CompareTo((short)y.Value);
+
+            if (x.Value is float && y.Value is float)
+                return ((float)x.Value).CompareTo((float)y.Value);
+            if (x.Value is double && y.Value is double)
+                return ((double)x.Value).CompareTo((double)y.Value);
+
+            // Fallback, if all else fails
+            return x.Value.ToString().CompareTo(y.Value.ToString());
+        }
+    }
+}
