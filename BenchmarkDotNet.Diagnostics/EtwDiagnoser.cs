fa6c627776d2560e709150a50a7af1fdfae8af3e
Sasha Goldshtein <goldshtn@gmail.com>
2016-02-28 14:29:53 +0200

    Changed GCDiagnoser to inspect live ETW event stream instead of recording to a file

--- BenchmarkDotNet.Diagnostics/EtwDiagnoser.cs
+++ BenchmarkDotNet.Diagnostics/EtwDiagnoser.cs
@@ -1,48 +1,18 @@
-﻿using System.Collections.Generic;
-using System.Diagnostics;
-using System.IO;
+﻿using BenchmarkDotNet.Parameters;
 using BenchmarkDotNet.Running;
-using BenchmarkDotNet.Parameters;
+using System.Collections.Generic;
 
 namespace BenchmarkDotNet.Diagnostics
 {
     public abstract class ETWDiagnoser
     {
-        protected readonly string ExecutedOkayMessage = "The command completed successfully.";
-        protected readonly string ProfilingFolder = "ProfilingData";
-
-        // This is the CLR runtime provider GUID for ETW Events
-        protected readonly string CLRRuntimeProvider = "{e13c0d23-ccbc-4e12-931b-d9cc2eee27e4}";
-
         protected readonly List<int> ProcessIdsUsedInRuns = new List<int>();
 
-        protected string RunProcess(string fileName, string arguments)
-        {
-            string output = string.Empty;
-            using (var process = new Process())
-            {
-                process.StartInfo.FileName = fileName;
-                process.StartInfo.Arguments = arguments;
-                process.StartInfo.UseShellExecute = false;
-                process.StartInfo.RedirectStandardOutput = true;
-                process.Start();
-                output = process.StandardOutput.ReadToEnd();
-                process.WaitForExit();
-            }
-            return output;
-        }
-
-        protected string GetFileName(string prefix, Benchmark benchmark, ParameterInstances parameters = null)
+        protected string GetSessionName(string prefix, Benchmark benchmark, ParameterInstances parameters = null)
         {
             if (parameters != null && parameters.Items.Count > 0)
                 return $"{prefix}-{benchmark.ShortInfo}-{parameters.FullInfo}";
             return $"{prefix}-{benchmark.ShortInfo}";
-        }
-
-        protected static void DeleteIfFileExists(string fileName)
-        {
-            if (File.Exists(fileName))
-                File.Delete(fileName);
         }
     }
 }
