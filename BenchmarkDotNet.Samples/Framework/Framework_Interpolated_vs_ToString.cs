8ab1a2b65f78384d1aa667a65ec4bd11d1404eda
Matt Warren <matt.warren@live.co.uk>
2016-03-22 12:10:50 +0000

    More samples

---
+++ BenchmarkDotNet.Samples/Framework/Framework_Interpolated_vs_ToString.cs
@@ -0,0 +1,22 @@
+﻿using BenchmarkDotNet.Attributes;
+
+namespace BenchmarkDotNet.Samples.Framework
+{
+    // Inspired by https://twitter.com/Nick_Craver/status/702693060472414208
+    public class Framework_Interpolated_vs_ToString
+    {
+        private long counter = 0;
+
+        [Benchmark]
+        public new string ToString()
+        {
+            return counter.ToString() + " ms";
+        }
+
+        [Benchmark]
+        public string InterpolatedString()
+        {
+            return $"{counter} ms";
+        }
+    }
+}
---
+++ BenchmarkDotNet.Samples/Framework/Framework_IterativeVsLINQ.cs
@@ -0,0 +1,43 @@
+﻿using BenchmarkDotNet.Attributes;
+using System;
+using System.Linq;
+
+namespace BenchmarkDotNet.Samples.Framework
+{
+    public class Framework_IterativeVsLINQ
+    {
+        private string[] parameters = new[]
+        {
+            "One",
+            "Two",
+            "Three",
+            "Four",
+            "Five",
+            "Six",
+            "Seven",
+            "Eight",
+            "Nine",
+            "Ten"
+        };
+
+        [Benchmark]
+        public string Iterative()
+        {
+            for (var i = 0; i < parameters.Length; i++)
+            {
+                var parameter = parameters[i];
+                if (string.Equals(parameter, "Five", StringComparison.OrdinalIgnoreCase))
+                {
+                    return parameter;
+                }
+            }
+            return null;
+        }
+
+        [Benchmark]
+        public string LINQ()
+        {
+            return parameters.FirstOrDefault(p => string.Equals(p, "Five", StringComparison.OrdinalIgnoreCase));
+        }
+    }
+}
