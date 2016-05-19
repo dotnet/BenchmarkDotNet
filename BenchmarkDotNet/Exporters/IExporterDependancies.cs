1371eae3cfffa22d1fe8dd4c4a1af64b99060fdf
Matt Warren <matt.warren@live.co.uk>
2016-05-19 15:51:55 +0100

    Allow Exporters to have dependencies, fixes #172

---
+++ BenchmarkDotNet/Exporters/IExporterDependancies.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace BenchmarkDotNet.Exporters
+{
+    /// <summary>
+    /// This is an internal interface, it allows Exporters to specify that 
+    /// they depends on another Exporter (see RPlotExporter for example)
+    /// </summary>
+    internal interface IExporterDependancies
+    {
+        IEnumerable<IExporter> Dependancies { get; }
+    }
+}
