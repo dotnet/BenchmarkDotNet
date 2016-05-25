78af456094a99c4efc10cd5b977b4551cdf86d82
Matt Warren <matt.warren@live.co.uk>
2016-05-25 22:22:42 +0100

    File missing from previous check-in "ae330c4" (#189)

--- BenchmarkDotNet/Exporters/Json/JsonExporterBase.cs
+++ BenchmarkDotNet/Exporters/Json/JsonExporterBase.cs
@@ -1,5 +1,6 @@
 ﻿using BenchmarkDotNet.Loggers;
 using BenchmarkDotNet.Reports;
+using System.Collections.Generic;
 using System.Linq;
 using JsonSerialiser = SimpleJson.SimpleJson;
 
@@ -12,10 +13,10 @@ namespace BenchmarkDotNet.Exporters.Json
         private bool IndentJson { get; set; }
         private bool ExcludeMeasurments { get; set; }
 
-        public JsonExporterBase(bool indentJson = false, bool excludeMeasurments = false)
+        public JsonExporterBase(bool indentJson = false, bool excludeMeasurements = false)
         {
             IndentJson = indentJson;
-            ExcludeMeasurments = excludeMeasurments;
+            ExcludeMeasurments = excludeMeasurements;
         }
 
         public override void ExportToLog(Summary summary, ILogger logger)
@@ -42,32 +43,44 @@ namespace BenchmarkDotNet.Exporters.Json
 
             // If we just ask SimpleJson to serialise the entire "summary" object it throws several errors.
             // So we are more specific in what we serialise (plus some fields/properties aren't relevant)
-            logger.Write(JsonSerialiser.SerializeObject(new
+
+            var benchmarks = summary.Reports.Select(r =>
             {
-                summary.Title,
-                HostEnvironmentInfo = environmentInfo,
-                Benchmarks = summary.Reports.Select(r => new
+                var data  = new Dictionary<string, object>
                 {
                     // We don't need Benchmark.ShortInfo, that info is available via Benchmark.Parameters below
-                    r.Benchmark.ShortInfo,
-                    Type = r.Benchmark.Target.Type.Name,
-                    Method = r.Benchmark.Target.Method.Name,
-                    r.Benchmark.Target.MethodTitle,
-                    Parameters = r.Benchmark.Parameters.PrintInfo,
-                    Properties = r.Benchmark.Job.AllProperties.ToDictionary(p => p.Name, p => p.Value),
-                    Statistics = r.ResultStatistics,
+                    { "ShortInfo", r.Benchmark.ShortInfo },
+                    { "Type", r.Benchmark.Target.Type.Name },
+                    { "Method", r.Benchmark.Target.Method.Name },
+                    { "MethodTitle", r.Benchmark.Target.MethodTitle },
+                    { "Parameters", r.Benchmark.Parameters.PrintInfo },
+                    { "Properties", r.Benchmark.Job.AllProperties.ToDictionary(p => p.Name, p => p.Value) },
+                    { "Statistics", r.ResultStatistics },
+                };
+
+                if (ExcludeMeasurments == false)
+                {
                     // We construct Measurment manually, so that we can have the IterationMode enum as text, rather than an integer
-                    Measurements = 
-                        (ExcludeMeasurments ? Enumerable.Empty<Measurement>() : r.AllMeasurements)
-                        .Select(m => new
+                    data.Add("Measurements",
+                        r.AllMeasurements.Select(m => new
                         {
                             IterationMode = m.IterationMode.ToString(),
                             m.LaunchIndex,
                             m.IterationIndex,
                             m.Operations,
                             m.Nanoseconds
-                        })
-                })
+                        }));
+                }
+
+                return data;
+            });
+
+            JsonSerialiser.CurrentJsonSerializerStrategy.Indent = IndentJson;
+            logger.Write(JsonSerialiser.SerializeObject(new Dictionary<string, object>
+            {
+                { "Title", summary.Title },
+                { "HostEnvironmentInfo", environmentInfo },
+                { "Benchmarks", benchmarks }
             }));
         }
     }
