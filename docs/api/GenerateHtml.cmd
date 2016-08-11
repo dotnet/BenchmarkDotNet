7b9fdbaefa2fc373d207cd57ea7a6591a00c98a6
Frans Bouma <frans@sd.nl>
2016-08-11 16:06:28 +0200

    Fixed a couple of issues with api doc generation
    Added xml file generation on projects.
    Added the xml files to the shfb file, as these are needed (gee, who
    knew!)
    Commented out vsvars32.bat call in cmd file.
    Fixed references to release builds

--- docs/api/GenerateHtml.cmd
+++ docs/api/GenerateHtml.cmd
@@ -1,3 +1,4 @@
 @echo off
-call vsvars32.bat
+REM Uncomment next line if msbuild isn't in the path but vs.net's common7\tools folder is. 
+REM call vsvars32.bat
 msbuild BenchmarkDotNet.shfbproj /v:m 
