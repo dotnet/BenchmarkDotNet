c1bdbfc3ab9f029c9af721731698952a6ff871a5
Jamie Cansdale <jcansdale@gmail.com>
2016-09-14 14:49:26 +0100

    Add source and tests for BenchmarkDotNet TestRunner adapter for TestDriven.Net.

---
+++ src/BenchmarkDotNet.TestDriven/BenchmarkTestRunner.cs
@@ -0,0 +1,105 @@
+﻿using System.Reflection;
+using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Running;
+using BenchmarkDotNet.Reports;
+using BenchmarkDotNet.Attributes;
+using TestDriven.Framework;
+
+namespace BenchmarkDotNet.TestDriven
+{
+    public abstract class BenchmarkTestRunner : ITestRunner
+    {
+        public abstract IConfig GetConfig();
+
+        public TestRunState RunMember(ITestListener testListener, Assembly assembly, MemberInfo member)
+        {
+            var summary = run(member);
+            if (summary == null)
+            {
+                return TestRunState.NoTests;
+            }
+
+            foreach (var benchmark in summary.Benchmarks)
+            {
+                var testResult = getTestResult(benchmark);
+                testListener.TestFinished(testResult);
+            }
+
+            testListener.TestResultsUrl(summary.ResultsDirectoryPath);
+
+            return TestRunState.Success;
+        }
+
+
+        static TestResult getTestResult(Benchmark benchmark)
+        {
+            var testResult = new TestResult();
+            testResult.TestRunnerName = getTestRunnerName();
+            testResult.Name = getTestName(benchmark.Target.Method);
+            testResult.Method = benchmark.Target.Method;
+            return testResult;
+        }
+
+        static string getTestName(MethodInfo method)
+        {
+            return method.DeclaringType.FullName + "." + method.Name;
+        }
+
+        static string getTestRunnerName()
+        {
+            var version = typeof(BenchmarkAttribute).GetTypeInfo().Assembly.GetName().Version;
+            return string.Format("BenchmarkDotNet {0}.{1}.{2}", version.Major, version.Minor, version.Build);
+        }
+
+        Summary run(MemberInfo member)
+        {
+            if (member is TypeInfo)
+            {
+                var type = (TypeInfo)member;
+                if (isBenchmarkType(type))
+                {
+                    return BenchmarkRunner.Run(type.AsType(), GetConfig());
+                }
+            }
+            else if (member is MethodInfo)
+            {
+                var method = (MethodInfo)member;
+                if (isBenchmarkMethod(method))
+                {
+                    var type = method.DeclaringType;
+                    return BenchmarkRunner.Run(type, new[] { method }, GetConfig());
+                }
+            }
+
+            return null;
+        }
+
+        static bool isBenchmarkType(TypeInfo type)
+        {
+            foreach (var method in type.GetMethods())
+            {
+                if (isBenchmarkMethod(method))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        static bool isBenchmarkMethod(MethodInfo method)
+        {
+            return method.GetCustomAttribute<BenchmarkAttribute>() != null;
+        }
+
+        public TestRunState RunNamespace(ITestListener testListener, Assembly assembly, string ns)
+        {
+            return TestRunState.NoTests;
+        }
+
+        public TestRunState RunAssembly(ITestListener testListener, Assembly assembly)
+        {
+            return TestRunState.NoTests;
+        }
+    }
+}
---
+++ src/BenchmarkDotNet.TestDriven/FastAndDirtyBenchmarkTestRunner.cs
@@ -0,0 +1,22 @@
+﻿using BenchmarkDotNet.Jobs;
+using BenchmarkDotNet.Configs;
+
+namespace BenchmarkDotNet.TestDriven
+{
+    public class FastAndDirtyBenchmarkTestRunner : BenchmarkTestRunner
+    {
+        // See https://perfdotnet.github.io/BenchmarkDotNet/faq.htm
+        public override IConfig GetConfig()
+        {
+            var config = new ManualConfig();
+            config.Add(DefaultConfig.Instance);
+            config.Add(Job.Default
+                .WithLaunchCount(1)     // benchmark process will be launched only once
+                .WithIterationTime(100) // 100ms per iteration
+                .WithWarmupCount(3)     // 3 warmup iteration
+                .WithTargetCount(3)     // 3 target iteration
+            );
+            return config;
+        }
+    }
+}
