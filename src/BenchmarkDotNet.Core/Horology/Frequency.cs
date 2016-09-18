ef3ecfad7ba8fa3dd346cdf7165a5b767591007f
Andrey Akinshin <andrey.akinshin@gmail.com>
2016-09-18 18:49:04 +0300

    Extended TimeInterval and Frequency API

--- src/BenchmarkDotNet.Core/Horology/Frequency.cs
+++ src/BenchmarkDotNet.Core/Horology/Frequency.cs
@@ -1,4 +1,6 @@
-﻿namespace BenchmarkDotNet.Horology
+﻿using JetBrains.Annotations;
+
+namespace BenchmarkDotNet.Horology
 {
     public struct Frequency
     {
@@ -13,17 +15,31 @@
         {
         }
 
-        public TimeInterval ToResolution() => TimeInterval.Second / Hertz;
-        public override string ToString() => Hertz + " " + FrequencyUnit.Hz.Name;
+        [Pure] public TimeInterval ToResolution() => TimeInterval.Second / Hertz;
+
+        [Pure] public double ToHz() => this / Hz;
+        [Pure] public double ToKHz() => this / KHz;
+        [Pure] public double ToMHz() => this / MHz;
+        [Pure] public double ToGHz() => this / GHz;
 
         public static readonly Frequency Hz = FrequencyUnit.Hz.ToFrequency();
         public static readonly Frequency KHz = FrequencyUnit.KHz.ToFrequency();
         public static readonly Frequency MHz = FrequencyUnit.MHz.ToFrequency();
         public static readonly Frequency GHz = FrequencyUnit.GHz.ToFrequency();
 
+        public static Frequency FromHz(double value) => Hz * value;
+        public static Frequency FromKHz(double value) => KHz * value;
+        public static Frequency FromMHz(double value) => MHz * value;
+        public static Frequency FromGHz(double value) => GHz * value;
+
         public static implicit operator Frequency(double value) => new Frequency(value);
         public static implicit operator double(Frequency property) => property.Hertz;
 
         public static double operator /(Frequency a, Frequency b) => 1.0 * a.Hertz / b.Hertz;
+        public static Frequency operator /(Frequency a, double k) => new Frequency(a.Hertz / k);
+        public static Frequency operator *(Frequency a, double k) => new Frequency(a.Hertz * k);
+        public static Frequency operator *(double k, Frequency a) => new Frequency(a.Hertz * k);
+
+        public override string ToString() => Hertz + " " + FrequencyUnit.Hz.Name;
     }
 }
--- src/BenchmarkDotNet.Core/Horology/TimeInterval.cs
+++ src/BenchmarkDotNet.Core/Horology/TimeInterval.cs
@@ -16,31 +16,15 @@ namespace BenchmarkDotNet.Horology
         {
         }
 
-        [Pure]
-        public Frequency ToFrequency() => new Frequency(Second / this);
+        [Pure] public Frequency ToFrequency() => new Frequency(Second / this);
 
-        [Pure]
-        public double ToNanoseconds() => this / Nanosecond;
-
-        [Pure]
-        public double ToMicroseconds() => this / Microsecond;
-
-        [Pure]
-        public double ToMilliseconds() => this / Millisecond;
-
-        [Pure]
-        public double ToSeconds() => this / Second;
-
-        [Pure]
-        public double ToMinutes() => this / Minute;
-
-        [Pure]
-        public double ToHours() => this / Hour;
-
-        [Pure]
-        public double ToDays() => this / Day;
-
-        public override string ToString() => Nanoseconds.ToTimeStr();
+        [Pure] public double ToNanoseconds() => this / Nanosecond;
+        [Pure] public double ToMicroseconds() => this / Microsecond;
+        [Pure] public double ToMilliseconds() => this / Millisecond;
+        [Pure] public double ToSeconds() => this / Second;
+        [Pure] public double ToMinutes() => this / Minute;
+        [Pure] public double ToHours() => this / Hour;
+        [Pure] public double ToDays() => this / Day;
 
         public static readonly TimeInterval Nanosecond = TimeUnit.Nanosecond.ToInterval();
         public static readonly TimeInterval Microsecond = TimeUnit.Microsecond.ToInterval();
@@ -50,9 +34,19 @@ namespace BenchmarkDotNet.Horology
         public static readonly TimeInterval Hour = TimeUnit.Hour.ToInterval();
         public static readonly TimeInterval Day = TimeUnit.Day.ToInterval();
 
+        public static TimeInterval FromNanoseconds(double value) => Nanosecond * value;
+        public static TimeInterval FromMicroseconds(double value) => Microsecond * value;
+        public static TimeInterval FromMilliseconds(double value) => Millisecond * value;
+        public static TimeInterval FromSeconds(double value) => Second * value;
+        public static TimeInterval FromMinutes(double value) => Minute * value;
+        public static TimeInterval FromHours(double value) => Hour * value;
+        public static TimeInterval FromDays(double value) => Day * value;
+
         public static double operator /(TimeInterval a, TimeInterval b) => 1.0 * a.Nanoseconds / b.Nanoseconds;
+        public static TimeInterval operator /(TimeInterval a, double k) => new TimeInterval(a.Nanoseconds / k);
         public static TimeInterval operator *(TimeInterval a, double k) => new TimeInterval(a.Nanoseconds * k);
         public static TimeInterval operator *(double k, TimeInterval a) => new TimeInterval(a.Nanoseconds * k);
-        public static TimeInterval operator /(TimeInterval a, double k) => new TimeInterval(a.Nanoseconds / k);
+
+        public override string ToString() => Nanoseconds.ToTimeStr();
     }
 }
