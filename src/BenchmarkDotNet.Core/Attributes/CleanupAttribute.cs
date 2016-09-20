f7a6a0cc27a1b3eb2a485049d9145ace9e629e50
Adam Sitnik <adam.sitnik@gmail.com>
2016-09-21 00:17:18 +0200

    Added [MeansImplicitUse] and explanation to summary for SetupAttribute and CleanupAttribute

--- src/BenchmarkDotNet.Core/Attributes/CleanupAttribute.cs
+++ src/BenchmarkDotNet.Core/Attributes/CleanupAttribute.cs
@@ -1,11 +1,14 @@
 ﻿using System;
+using JetBrains.Annotations;
 
 namespace BenchmarkDotNet.Attributes
 {
     /// <summary>
     /// Marks method to be executed after benchmark.
+    /// <remarks>It's going to be executed only once, after all benchmark runs.</remarks>
     /// </summary>
     [AttributeUsage(AttributeTargets.Method)]
+    [MeansImplicitUse]
     public class CleanupAttribute : Attribute
     {
         public CleanupAttribute()
--- src/BenchmarkDotNet.Core/Attributes/SetupAttribute.cs
+++ src/BenchmarkDotNet.Core/Attributes/SetupAttribute.cs
@@ -1,11 +1,14 @@
 ﻿using System;
+using JetBrains.Annotations;
 
 namespace BenchmarkDotNet.Attributes
 {
     /// <summary>
-    /// Marks method to be executed before benchmark.
+    /// Marks method to be executed before benchmark. 
+    /// <remarks>It's going to be executed only once, just before warm up.</remarks>
     /// </summary>
     [AttributeUsage(AttributeTargets.Method)]
+    [MeansImplicitUse]
     public class SetupAttribute : Attribute
     {
         public SetupAttribute()
