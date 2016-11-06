134d7446cbc07224466b9d3d2ae74d18c613078c
Andrey Akinshin <andrey.akinshin@gmail.com>
2016-11-06 11:50:23 +0300

    Misc improvements in Exporters

--- src/BenchmarkDotNet.Core/Attributes/Exporters/MarkdownExporterAttribute.cs
+++ src/BenchmarkDotNet.Core/Attributes/Exporters/MarkdownExporterAttribute.cs
@@ -2,10 +2,39 @@
 
 namespace BenchmarkDotNet.Attributes.Exporters
 {
+    // TODO: Find a better way to introduce dialects in the attribute
     public class MarkdownExporterAttribute : ExporterConfigBaseAttribute
     {
-        public MarkdownExporterAttribute() : base(DefaultExporters.Markdown)
+        public MarkdownExporterAttribute() : base(MarkdownExporter.Default)
         {
+        }
+
+        public class Default : ExporterConfigBaseAttribute
+        {
+            public Default() : base(MarkdownExporter.Default)
+            {
+            }
+        }
+
+        public class GitHub : ExporterConfigBaseAttribute
+        {
+            public GitHub() : base(MarkdownExporter.GitHub)
+            {
+            }
+        }
+
+        public class StackOverflow : ExporterConfigBaseAttribute
+        {
+            public StackOverflow() : base(MarkdownExporter.StackOverflow)
+            {
+            }
+        }
+
+        public class Atlassian : ExporterConfigBaseAttribute
+        {
+            public Atlassian() : base(MarkdownExporter.Atlassian)
+            {
+            }
         }
     }
 }
--- src/BenchmarkDotNet.Core/Exporters/Csv/CsvMeasurementsExporter.cs
+++ src/BenchmarkDotNet.Core/Exporters/Csv/CsvMeasurementsExporter.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using BenchmarkDotNet.Characteristics;
 using BenchmarkDotNet.Extensions;
@@ -10,18 +11,50 @@ namespace BenchmarkDotNet.Exporters.Csv
 {
     public class CsvMeasurementsExporter : ExporterBase
     {
-        private readonly string separator;
         private static readonly CharacteristicPresenter Presenter = CharacteristicPresenter.SummaryPresenter;
 
         protected override string FileExtension => "csv";
         protected override string FileCaption => "measurements";
 
-        public static readonly IExporter Default = new CsvMeasurementsExporter(CsvSeparator.CurrentCulture);
+        public string Separator { get; }
+
+        public static readonly CsvMeasurementsExporter Default = new CsvMeasurementsExporter(CsvSeparator.CurrentCulture);
 
         public CsvMeasurementsExporter(CsvSeparator separator)
         {
-            this.separator = separator.ToRealSeparator();
+            Separator = separator.ToRealSeparator();
         }
+
+        private static MeasurementColumn[] BuildColumns()
+        {
+            var columns = new List<MeasurementColumn>();
+
+            // Target
+            columns.Add(new MeasurementColumn("Target", (summary, report, m) => report.Benchmark.Target.Type.Name + "." +report.Benchmark.Target.MethodDisplayInfo));
+            columns.Add(new MeasurementColumn("Target_Namespace", (summary, report, m) => report.Benchmark.Target.Type.Namespace));
+            columns.Add(new MeasurementColumn("Target_Type", (summary, report, m) => report.Benchmark.Target.Type.Name));
+            columns.Add(new MeasurementColumn("Target_Method", (summary, report, m) => report.Benchmark.Target.MethodDisplayInfo));
+
+            // Job
+            foreach (var characteristic in CharacteristicHelper.GetAllPresentableCharacteristics(typeof(Job), true))
+                columns.Add(new MeasurementColumn("Job_" + characteristic.Id, (summary, report, m) => Presenter.ToPresentation(report.Benchmark.Job, characteristic)));
+            columns.Add(new MeasurementColumn("Job_Display", (summary, report, m) => report.Benchmark.Job.DisplayInfo));
+
+            // Params
+            columns.Add(new MeasurementColumn("Params", (summary, report, m) => report.Benchmark.Parameters.PrintInfo));
+
+            // Measurements
+            columns.Add(new MeasurementColumn("Measurement_LaunchIndex", (summary, report, m) => m.LaunchIndex.ToString()));
+            columns.Add(new MeasurementColumn("Measurement_IterationMode", (summary, report, m) => m.IterationMode.ToString()));
+            columns.Add(new MeasurementColumn("Measurement_IterationIndex", (summary, report, m) => m.IterationIndex.ToString()));
+            columns.Add(new MeasurementColumn("Measurement_Nanoseconds", (summary, report, m) => m.Nanoseconds.ToStr()));
+            columns.Add(new MeasurementColumn("Measurement_Operations", (summary, report, m) => m.Operations.ToString()));
+            columns.Add(new MeasurementColumn("Measurement_Value", (summary, report, m) => (m.Nanoseconds / m.Operations).ToStr()));
+
+            return columns.ToArray();
+        }
+
+        private static readonly Lazy<MeasurementColumn[]> lazyColumns = new Lazy<MeasurementColumn[]>(BuildColumns);
 
         private class MeasurementColumn
         {
@@ -35,33 +68,10 @@ namespace BenchmarkDotNet.Exporters.Csv
             }
         }
 
-        // TODO: rewrite
-        private readonly MeasurementColumn[] columns =
-        {
-            new MeasurementColumn("Target", (summary, report, m) => report.Benchmark.Target.Type.Name + "." +report.Benchmark.Target.MethodDisplayInfo),
-            new MeasurementColumn("TargetType", (summary, report, m) => report.Benchmark.Target.Type.Name),
-            new MeasurementColumn("TargetMethod", (summary, report, m) => report.Benchmark.Target.MethodDisplayInfo),
-
-            new MeasurementColumn("Job", (summary, report, m) => report.Benchmark.Job.DisplayInfo),
-            new MeasurementColumn("JobRunStrategy", (summary, report, m) => Presenter.ToPresentation(report.Benchmark.Job, RunMode.RunStrategyCharacteristic)),
-            new MeasurementColumn("JobPlatform", (summary, report, m) => Presenter.ToPresentation(report.Benchmark.Job,EnvMode.PlatformCharacteristic)),
-            new MeasurementColumn("JobJit", (summary, report, m) => Presenter.ToPresentation(report.Benchmark.Job,EnvMode.JitCharacteristic)),
-            new MeasurementColumn("JobToolchain", (summary, report, m) => Presenter.ToPresentation(report.Benchmark.Job,InfrastructureMode.ToolchainCharacteristic)),
-            new MeasurementColumn("JobRuntime", (summary, report, m) => Presenter.ToPresentation(report.Benchmark.Job,EnvMode.RuntimeCharacteristic)),
-
-            new MeasurementColumn("Params", (summary, report, m) => report.Benchmark.Parameters.PrintInfo),
-
-            new MeasurementColumn("MeasurementLaunchIndex", (summary, report, m) => m.LaunchIndex.ToString()),
-            new MeasurementColumn("MeasurementIterationMode", (summary, report, m) => m.IterationMode.ToString()),
-            new MeasurementColumn("MeasurementIterationIndex", (summary, report, m) => m.IterationIndex.ToString()),
-            new MeasurementColumn("MeasurementNanoseconds", (summary, report, m) => m.Nanoseconds.ToStr()),
-            new MeasurementColumn("MeasurementOperations", (summary, report, m) => m.Operations.ToString()),
-            new MeasurementColumn("MeasurementValue", (summary, report, m) => (m.Nanoseconds / m.Operations).ToStr())
-        };
-
         public override void ExportToLog(Summary summary, ILogger logger)
         {
-            logger.WriteLine(string.Join(separator, columns.Select(c => CsvHelper.Escape(c.Title))));
+            var columns = lazyColumns.Value;
+            logger.WriteLine(string.Join(Separator, columns.Select(c => CsvHelper.Escape(c.Title))));
 
             foreach (var report in summary.Reports)
             {
@@ -73,7 +83,7 @@ namespace BenchmarkDotNet.Exporters.Csv
 
                         if (++i < columns.Length)
                         {
-                            logger.Write(separator);
+                            logger.Write(Separator);
                         }
                     }
                     logger.WriteLine();
