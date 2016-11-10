2edb9adf2e1b0aa7545097bb92bde55830ec4dde
Andrey Akinshin <andrey.akinshin@gmail.com>
2016-11-10 17:21:09 +0300

    Set library version: 0.10.0

--- src/BenchmarkDotNet.Core/Properties/BenchmarkDotNetInfo.cs
+++ src/BenchmarkDotNet.Core/Properties/BenchmarkDotNetInfo.cs
@@ -6,8 +6,8 @@
         public const string Title = TitleBase + (IsDevelopVersion ? "-Dev" : "");
         public const string Description = "Powerful .NET library for benchmarking";
         public const string Copyright = "Copyright © .NET Foundation and contributors 2013–2016";
-        public const string Version = "0.9.9";
-        public const string FullVersion = "0.9.9" + (IsDevelopVersion ? "-develop" : "");
+        public const string Version = "0.10.0";
+        public const string FullVersion = "0.10.0" + (IsDevelopVersion ? "-develop" : "");
         public const string FullTitle = Title + " v" + FullVersion;
 
         public const bool IsDevelopVersion = true; // Set to false for NuGet publishing
