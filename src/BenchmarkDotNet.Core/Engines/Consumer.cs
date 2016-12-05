d96b686e507cbc33e1eb37971cb647e8c59e3c2a
Andrey Akinshin <andrey.akinshin@jetbrains.com>
2016-12-05 20:02:34 +0300

    Improved Consumer

--- src/BenchmarkDotNet.Core/Engines/Consumer.cs
+++ src/BenchmarkDotNet.Core/Engines/Consumer.cs
@@ -1,13 +1,69 @@
 ﻿using System.Runtime.CompilerServices;
+using System.Threading;
 
+// ReSharper disable NotAccessedField.Local
 namespace BenchmarkDotNet.Engines
 {
     public class Consumer
     {
-        [MethodImpl(MethodImplOptions.NoInlining)]
-        public void Consume<T>(T x)
-        {
-            // TODO: Investigate how the current approach works with nanoseconds benchmarks
-        }
+        private volatile byte byteHolder;
+        private volatile sbyte sbyteHolder;
+        private volatile short shortHolder;
+        private volatile ushort ushortHolder;
+        private volatile int intHolder;
+        private volatile uint uintHolder;
+        private volatile bool boolHolder;
+        private volatile char charHolder;
+        private volatile float floatHolder;
+        private double doubleHolder;
+        private long longHolder;
+        private ulong ulongHolder;
+        private string stringHolder;
+        private object objectHolder;
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public void Consume(byte byteValue) => byteHolder = byteValue;
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public void Consume(sbyte sbyteValue) => sbyteHolder = sbyteValue;
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public void Consume(short shortValue) => shortHolder = shortValue;
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public void Consume(ushort ushortValue) => ushortHolder = ushortValue;
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public void Consume(int intValue) => intHolder = intValue;
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public void Consume(uint uintValue) => uintHolder = uintValue;
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public void Consume(bool boolValue) => boolHolder = boolValue;
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public void Consume(char charValue) => charHolder = charValue;
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public void Consume(float floatValue) => floatHolder = floatValue;
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public void Consume(double doubleValue) => Volatile.Write(ref doubleHolder, doubleValue);
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public void Consume(long longValue) => Volatile.Write(ref longHolder, longValue);
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public void Consume(ulong ulongValue) => Volatile.Write(ref ulongHolder, ulongValue);
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public void Consume(string stringValue) => Volatile.Write(ref stringHolder, stringValue);
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public void Consume(object objectValue) => Volatile.Write(ref objectHolder, objectValue);
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public void Consume<T>(T objectValue) => Volatile.Write(ref objectHolder, objectValue);
     }
 }
