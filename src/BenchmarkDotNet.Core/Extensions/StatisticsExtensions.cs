6fd9f3d970e01dc7d5f164c7691924e897f76e5b
asmirnova <alinasmirnova915@gmail.com>
2017-01-14 16:47:46 +0500

    Used ToStr to make statistics builder culture invariant (fixed #307)

--- src/BenchmarkDotNet.Core/Extensions/StatisticsExtensions.cs
+++ src/BenchmarkDotNet.Core/Extensions/StatisticsExtensions.cs
@@ -13,12 +13,12 @@ namespace BenchmarkDotNet.Extensions
             if (s == null)
                 return NullSummaryMessage;
             var builder = new StringBuilder();
-            var errorPercent = (s.StandardError / s.Mean * 100).ToStr();
+            string errorPercent = (s.StandardError / s.Mean * 100).ToStr();
             builder.AppendLine($"Mean = {s.Mean.ToStr()}, StdErr = {s.StandardError.ToStr()} ({errorPercent}%); N = {s.N}, StdDev = {s.StandardDeviation.ToStr()}");
             builder.AppendLine($"Min = {s.Min.ToStr()}, Q1 = {s.Q1.ToStr()}, Median = {s.Median.ToStr()}, Q3 = {s.Q3.ToStr()}, Max = {s.Max.ToStr()}");
             builder.AppendLine($"IQR = {s.InterquartileRange.ToStr()}, LowerFence = {s.LowerFence.ToStr()}, UpperFence = {s.UpperFence.ToStr()}");
             builder.AppendLine($"ConfidenceInterval = {s.ConfidenceInterval.ToStr()}");
-            builder.AppendLine($"Skewness = {s.Skewness}, Kurtosis = {s.Kurtosis}");
+            builder.AppendLine($"Skewness = {s.Skewness.ToStr()}, Kurtosis = {s.Kurtosis.ToStr()}");
             return builder.ToString();
         }
 
@@ -29,12 +29,12 @@ namespace BenchmarkDotNet.Extensions
             if (unit == null)
                 unit = TimeUnit.GetBestTimeUnit(s.Mean);
             var builder = new StringBuilder();
-            var errorPercent = (s.StandardError / s.Mean * 100).ToStr();
+            string errorPercent = (s.StandardError / s.Mean * 100).ToStr();
             builder.AppendLine($"Mean = {s.Mean.ToTimeStr(unit)}, StdErr = {s.StandardError.ToTimeStr(unit)} ({errorPercent}%); N = {s.N}, StdDev = {s.StandardDeviation.ToTimeStr(unit)}");
             builder.AppendLine($"Min = {s.Min.ToTimeStr(unit)}, Q1 = {s.Q1.ToTimeStr(unit)}, Median = {s.Median.ToTimeStr(unit)}, Q3 = {s.Q3.ToTimeStr(unit)}, Max = {s.Max.ToTimeStr(unit)}");
             builder.AppendLine($"IQR = {s.InterquartileRange.ToTimeStr(unit)}, LowerFence = {s.LowerFence.ToTimeStr(unit)}, UpperFence = {s.UpperFence.ToTimeStr(unit)}");
             builder.AppendLine($"ConfidenceInterval = {s.ConfidenceInterval.ToTimeStr(unit)}");
-            builder.AppendLine($"Skewness = {s.Skewness}, Kurtosis = {s.Kurtosis}");
+            builder.AppendLine($"Skewness = {s.Skewness.ToStr()}, Kurtosis = {s.Kurtosis.ToStr()}");
             return builder.ToString();
         }
     }
