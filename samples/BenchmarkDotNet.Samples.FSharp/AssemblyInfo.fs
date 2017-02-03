e0b134ce46a2c6f0c8e690df83e153e92764e8e3
Adam Sitnik <adam.sitnik@gmail.com>
2017-02-03 17:55:49 +0100

    taking advantage of the csproj

--- samples/BenchmarkDotNet.Samples.FSharp/AssemblyInfo.fs
+++ samples/BenchmarkDotNet.Samples.FSharp/AssemblyInfo.fs
@@ -3,19 +3,7 @@
 open System.Reflection
 open System.Runtime.CompilerServices
 open System.Runtime.InteropServices
-open BenchmarkDotNet.Properties
 
-[<assembly: AssemblyTitle(BenchmarkDotNetInfo.Title + ".Samples.FSharp")>]
-[<assembly: AssemblyProduct(BenchmarkDotNetInfo.Title + ".Samples.FSharp")>]
-[<assembly: AssemblyDescription(BenchmarkDotNetInfo.Description + " Samples.FSharp")>]
-[<assembly: AssemblyCopyright(BenchmarkDotNetInfo.Copyright)>]
-[<assembly: AssemblyVersion(BenchmarkDotNetInfo.Version)>]
-[<assembly: AssemblyFileVersion(BenchmarkDotNetInfo.Version)>]
-
-[<assembly: AssemblyConfiguration("")>]
-[<assembly: AssemblyCompany("")>]
-[<assembly: AssemblyTrademark("")>]
-[<assembly: AssemblyCulture("")>]
 
 [<assembly: ComVisible(false)>]
 [<assembly: Guid("a329f00e-4b9d-4bc6-b688-92698d773cbf")>]
--- samples/BenchmarkDotNet.Samples/Properties/AssemblyInfo.cs
+++ samples/BenchmarkDotNet.Samples/Properties/AssemblyInfo.cs
@@ -1,16 +1,3 @@
-﻿using System.Reflection;
-using System.Runtime.InteropServices;
-using BenchmarkDotNet.Properties;
+﻿using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle(BenchmarkDotNetInfo.Title + ".Samples")]
-[assembly: AssemblyProduct(BenchmarkDotNetInfo.Title + ".Samples")]
-[assembly: AssemblyDescription(BenchmarkDotNetInfo.Description + " Samples")]
-[assembly: AssemblyCopyright(BenchmarkDotNetInfo.Copyright)]
-
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-[assembly: ComVisible(false)]
 [assembly: Guid("6f2232a9-0d0c-46cf-b08c-f6e28ab612e3")]
