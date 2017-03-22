27fa75e1a1a10eb2e713c19ec574d06e57264c8b
Amadeusz Wieczorek <git@amadeusw.com>
2017-03-21 23:31:15 -0700

    revert sample program

--- samples/BenchmarkDotNet.Samples/Program.cs
+++ samples/BenchmarkDotNet.Samples/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System.Reflection;
 using BenchmarkDotNet.Running;
-using BenchmarkDotNet.Configs;
-using BenchmarkDotNet.Exporters.Csv;
-using BenchmarkDotNet.Diagnosers;
 
 namespace BenchmarkDotNet.Samples
 {
@@ -10,27 +7,7 @@ namespace BenchmarkDotNet.Samples
     {
         static void Main(string[] args)
         {
-            var config = ManualConfig.Create(DefaultConfig.Instance);
-            config.Set(new Reports.SummaryStyle()
-            {
-                PrintUnitsInHeader = false,
-                PrintUnitsInContent = true,
-                TimeUnit = Horology.TimeUnit.Second,
-                SizeUnit = Columns.SizeUnit.B
-            });
-            config.Add(new MemoryDiagnoser());
-            config.Add(new CsvExporter(
-                CsvSeparator.CurrentCulture,
-                new Reports.SummaryStyle()
-                {
-                    PrintUnitsInHeader = true,
-                    PrintUnitsInContent = false,
-                    TimeUnit = Horology.TimeUnit.Millisecond,
-                    SizeUnit = Columns.SizeUnit.kB
-                }
-            ));
-            BenchmarkRunner.Run<Intro.IntroColumns>(config);
-            //BenchmarkSwitcher.FromAssembly(typeof(Program).GetTypeInfo().Assembly).Run(args);
+            BenchmarkSwitcher.FromAssembly(typeof(Program).GetTypeInfo().Assembly).Run(args);
         }
     }
 }
