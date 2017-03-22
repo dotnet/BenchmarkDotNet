6c0d9511879c4eb6502a58fc8807cbf62e94515f
Amadeusz Wieczorek <git@amadeusw.com>
2017-03-21 23:07:15 -0700

    sample

--- samples/BenchmarkDotNet.Samples/Program.cs
+++ samples/BenchmarkDotNet.Samples/Program.cs
@@ -11,6 +11,13 @@ namespace BenchmarkDotNet.Samples
         static void Main(string[] args)
         {
             var config = ManualConfig.Create(DefaultConfig.Instance);
+            config.Set(new Reports.SummaryStyle()
+            {
+                PrintUnitsInHeader = false,
+                PrintUnitsInContent = true,
+                TimeUnit = Horology.TimeUnit.Second,
+                SizeUnit = Columns.SizeUnit.B
+            });
             config.Add(new MemoryDiagnoser());
             config.Add(new CsvExporter(
                 CsvSeparator.CurrentCulture,
@@ -18,8 +25,8 @@ namespace BenchmarkDotNet.Samples
                 {
                     PrintUnitsInHeader = true,
                     PrintUnitsInContent = false,
-                    //TimeUnit = Horology.TimeUnit.Second,
-                    //SizeUnit = Columns.SizeUnit.B
+                    TimeUnit = Horology.TimeUnit.Millisecond,
+                    SizeUnit = Columns.SizeUnit.kB
                 }
             ));
             BenchmarkRunner.Run<Intro.IntroColumns>(config);
