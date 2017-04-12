fa312804267eb3b9b26e2efe64c1d44c608fba12
Amadeusz Wieczorek <git@amadeusw.com>
2017-04-12 00:50:50 -0700

    Exports file to temporary location if target is locked (#416)
    * Exports file to temporary location if target is locked
    
    * builds the unique export filename using current time
    
    * Adds integration tests for the exporter fallback behavior
    
    * Adds integration tests for the exporter fallback behavior

--- src/BenchmarkDotNet.Core/Exporters/ExporterBase.cs
+++ src/BenchmarkDotNet.Core/Exporters/ExporterBase.cs
@@ -20,7 +20,17 @@ namespace BenchmarkDotNet.Exporters
             var filePath = $"{Path.Combine(summary.ResultsDirectoryPath, summary.Title)}-{FileCaption}{FileNameSuffix}.{FileExtension}";
             if (File.Exists(filePath))
             {
-                File.Delete(filePath);
+                try
+                {
+                    File.Delete(filePath);
+                }
+                catch (IOException)
+                {
+                    var uniqueString = System.DateTime.Now.ToString("yyyyMMdd-HHmmss");
+                    var alternativeFilePath = $"{Path.Combine(summary.ResultsDirectoryPath, summary.Title)}-{FileCaption}{FileNameSuffix}-{uniqueString}.{FileExtension}";
+                    consoleLogger.WriteLineError($"Could not overwrite file {filePath}. Exporting to {alternativeFilePath}");
+                    filePath = alternativeFilePath;
+                }
             }
 
             using (var stream = Portability.StreamWriter.FromPath(filePath))
---
+++ tests/BenchmarkDotNet.IntegrationTests/ExporterIOTests.cs
@@ -0,0 +1,94 @@
+﻿﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using BenchmarkDotNet.Exporters;
+using BenchmarkDotNet.Loggers;
+using BenchmarkDotNet.Reports;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace BenchmarkDotNet.IntegrationTests
+{
+    public class ExporterIOTests : BenchmarkTestExecutor
+    {
+        public ExporterIOTests(ITestOutputHelper output) : base(output) { }
+
+        [Fact]
+        public void ExporterWritesToFile()
+        {
+            string resultsDirectoryPath = Path.GetTempPath();
+            var exporter = new MockExporter();
+            var mockSummary = GetMockSummary(resultsDirectoryPath);
+            var filePath = $"{Path.Combine(mockSummary.ResultsDirectoryPath, mockSummary.Title)}-report.txt"; // ExporterBase default
+
+            try
+            {
+                exporter.ExportToFiles(mockSummary, NullLogger.Instance);
+
+                Assert.Equal(1, exporter.ExportCount);
+                Assert.True(File.Exists(filePath));
+            }
+            finally
+            {
+                if (File.Exists(filePath))
+                    File.Delete(filePath);
+            }
+        }
+
+        [Fact]
+        public void ExporterWorksWhenFileIsLocked()
+        {
+            string resultsDirectoryPath = Path.GetTempPath();
+            var exporter = new MockExporter();
+            var mockSummary = GetMockSummary(resultsDirectoryPath);
+            var filePath = $"{Path.Combine(mockSummary.ResultsDirectoryPath, mockSummary.Title)}-report.txt"; // ExporterBase default
+
+            try
+            {
+                exporter.ExportToFiles(mockSummary, NullLogger.Instance);
+
+                Assert.Equal(1, exporter.ExportCount);
+                Assert.True(File.Exists(filePath));
+                using (var handle = File.OpenRead(filePath)) // Gets a lock on the target file
+                {
+                    exporter.ExportToFiles(mockSummary, NullLogger.Instance);
+                    Assert.Equal(2, exporter.ExportCount);
+                }
+                var savedFiles = Directory.EnumerateFiles(Path.GetDirectoryName(filePath), Path.GetFileNameWithoutExtension(filePath) + "*");
+                Assert.Equal(2, savedFiles.Count());
+            }
+            finally
+            {
+                if (File.Exists(filePath))
+                    File.Delete(filePath);
+                var otherFiles = Directory.EnumerateFiles(Path.GetDirectoryName(filePath), Path.GetFileNameWithoutExtension(filePath) + "*");
+                foreach (var file in otherFiles)
+                    File.Delete(file);
+            }
+        }
+
+        private Summary GetMockSummary(string resultsDirectoryPath)
+        {
+            return new Summary(
+                title: "bdn-test",
+                reports: new List<BenchmarkReport>(),
+                hostEnvironmentInfo: Environments.HostEnvironmentInfo.GetCurrent(),
+                config: Configs.DefaultConfig.Instance,
+                resultsDirectoryPath: resultsDirectoryPath,
+                totalTime: System.TimeSpan.Zero,
+                validationErrors: new Validators.ValidationError[0]
+            );
+        }
+
+        private class MockExporter : ExporterBase
+        {
+            public int ExportCount = 0;
+
+            public override void ExportToLog(Summary summary, ILogger logger)
+            {
+                ExportCount++;
+            }
+        }
+    }
+}
