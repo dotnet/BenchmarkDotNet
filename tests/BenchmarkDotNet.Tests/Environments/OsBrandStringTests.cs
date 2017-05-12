626e3a64ba78a66afde9cf82a312c7a2fe4e6e1a
Andrey Akinshin <andrey.akinshin@gmail.com>
2017-05-12 10:05:12 +0300

    Show Windows brand versions in summary

---
+++ tests/BenchmarkDotNet.Tests/Environments/OsBrandStringTests.cs
@@ -0,0 +1,14 @@
+using BenchmarkDotNet.Environments;
+using Xunit;
+
+namespace BenchmarkDotNet.Tests.Environments
+{
+    public class OsBrandStringTests
+    {
+        [Theory]
+        [InlineData("6.3.9600", "Windows 8.1 (6.3.9600)")]
+        [InlineData("10.0.14393", "Windows 10 Redstone 1 (10.0.14393)")]
+        public void WindowsIsPrettified(string originalVersion, string prettifiedName) =>
+            Assert.Equal(prettifiedName, OsBrandStringHelper.Prettify("Windows", originalVersion));
+    }
+}
