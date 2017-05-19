53fd6983b4de3e7fb7ec670a3566bcc8d1a01c98
Teknikaali <teknikaali@outlook.com>
2017-05-19 18:18:11 +0300

    Add XmlExporter's class files. Add constructors and static methods for export options.

---
+++ src/BenchmarkDotNet.Core/Exporters/Xml/XmlExporter.cs
@@ -0,0 +1,27 @@
+﻿namespace BenchmarkDotNet.Exporters.Xml
+{
+    public class XmlExporter : XmlExporterBase
+    {
+        public static readonly IExporter Brief = new XmlExporter("-brief", indentXml: true, excludeMeasurements: true);
+        public static readonly IExporter Full = new XmlExporter("-full", indentXml: true, excludeMeasurements: false);
+        public static readonly IExporter BriefCompressed = new XmlExporter("-brief-compressed", indentXml: false, excludeMeasurements: true);
+        public static readonly IExporter FullCompressed = new XmlExporter("-full-compressed", indentXml: false, excludeMeasurements: false);
+
+        public static readonly IExporter Default = Brief;
+
+        protected override string FileNameSuffix { get; } = string.Empty;
+
+        public XmlExporter(string fileNameSuffix = "",
+                           bool indentXml = false,
+                           bool excludeMeasurements = false)
+            : base(indentXml, excludeMeasurements)
+        {
+            FileNameSuffix = fileNameSuffix;
+        }
+
+        public static IExporter Custom(string fileNameSuffix = "",
+                                       bool indentXml = false,
+                                       bool excludeMeasurements = false) =>
+            new XmlExporter(fileNameSuffix, indentXml, excludeMeasurements);
+    }
+}
---
+++ src/BenchmarkDotNet.Core/Exporters/Xml/XmlExporterBase.cs
@@ -0,0 +1,29 @@
+﻿using System.Xml;
+using BenchmarkDotNet.Loggers;
+using BenchmarkDotNet.Reports;
+
+namespace BenchmarkDotNet.Exporters.Xml
+{
+    public abstract class XmlExporterBase : ExporterBase
+    {
+        protected override string FileExtension => "xml";
+
+        private readonly XmlWriterSettings settings;
+        private readonly bool excludeMeasurements;
+
+        public XmlExporterBase(bool indentXml = false, bool excludeMeasurements = false)
+        {
+            settings = new XmlWriterSettings
+            {
+                Indent = indentXml,
+            };
+
+            this.excludeMeasurements = excludeMeasurements;
+        }
+
+        public override void ExportToLog(Summary summary, ILogger logger)
+        {
+            
+        }
+    }
+}
