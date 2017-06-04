3cceb6135e376c284d2640b7653c37cb63a0c8f4
Andrey Akinshin <andrey.akinshin@gmail.com>
2017-06-04 19:27:22 +0300

    runCoreTests.sh: fix output file for integration-tests

--- tests/runCoreTests.sh
+++ tests/runCoreTests.sh
@@ -1,2 +1,2 @@
 dotnet test BenchmarkDotNet.Tests/BenchmarkDotNet.Tests.csproj --configuration Release --framework netcoreapp1.1 |& tee tests.log
-dotnet test BenchmarkDotNet.IntegrationTests/BenchmarkDotNet.IntegrationTests.csproj --configuration Release --framework netcoreapp1.1 |& tee tests.log
+dotnet test BenchmarkDotNet.IntegrationTests/BenchmarkDotNet.IntegrationTests.csproj --configuration Release --framework netcoreapp1.1 |& tee integration-tests.log
