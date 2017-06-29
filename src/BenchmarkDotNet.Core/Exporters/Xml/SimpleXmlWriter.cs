7d8eae0ee1c6d699a10fe2497b25b9ea6123147c
Teknikaali <teknikaali@outlook.com>
2017-06-29 19:45:50 +0300

    Add SimpleXmlWriter as an implementation for IXmlWriter to write the actual xml data.

---
+++ src/BenchmarkDotNet.Core/Exporters/Xml/SimpleXmlWriter.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.IO;
+using System.Xml;
+
+namespace BenchmarkDotNet.Exporters.Xml
+{
+    public class SimpleXmlWriter : IXmlWriter, IDisposable
+    {
+        private XmlWriter writer;
+
+        public SimpleXmlWriter(TextWriter writer, bool indent)
+        {
+            if (writer == null)
+                throw new ArgumentNullException(nameof(writer));
+
+            this.writer = XmlWriter.Create(writer, new XmlWriterSettings() { Indent = indent });
+        }
+
+        public void WriteElementString(string localName, string value)
+        {
+            writer.WriteElementString(localName, value);
+        }
+
+        public void WriteEndDocument()
+        {
+            writer.WriteEndDocument();
+        }
+
+        public void WriteEndElement()
+        {
+            writer.WriteEndElement();
+        }
+
+        public void WriteStartDocument()
+        {
+            writer.WriteStartDocument();
+        }
+
+        public void WriteStartElement(string localName)
+        {
+            writer.WriteStartElement(localName);
+        }
+
+        // Dispose
+        private bool disposedValue = false;
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    writer.Dispose();
+                }
+
+                disposedValue = true;
+            }
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+        }
+    }
+}
