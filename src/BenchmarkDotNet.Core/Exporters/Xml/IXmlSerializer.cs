69d8ddd4076fa473af3c5c9033d0133f2fdd5380
Teknikaali <teknikaali@outlook.com>
2017-06-29 19:39:30 +0300

    Add IXmlSerializer interface to make the custom XmlSerializer more mock friendly and testable.

---
+++ src/BenchmarkDotNet.Core/Exporters/Xml/IXmlSerializer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace BenchmarkDotNet.Exporters.Xml
+{
+    public interface IXmlSerializer : IFluentXmlSerializer
+    {
+        void Serialize(IXmlWriter writer, object source);
+    }
+
+    public interface IFluentXmlSerializer
+    {
+        IXmlSerializer WithRootName(string rootName);
+        IXmlSerializer WithCollectionItemName(Type type, string name);
+        IXmlSerializer WithExcludedProperty(string propertyName);
+    }
+
+    public interface IXmlWriter
+    {
+        void WriteStartDocument();
+        void WriteEndDocument();
+        void WriteStartElement(string localName);
+        void WriteEndElement();
+        void WriteElementString(string localName, string value);
+    }
+}
