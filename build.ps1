c6a35ea2744b88f36cdfda32569df42e6b8ac15b
Igor Fesenko <igor.aka.ky7m@gmail.com>
2017-07-16 00:33:12 +0300

    Update CakeBuild to 0.21.1

--- build.ps1
+++ build.ps1
@@ -31,7 +31,7 @@ Param(
     [string[]]$ScriptArgs
 )
 
-$CakeVersion = "0.21.0"
+$CakeVersion = "0.21.1"
 $DotNetChannel = "preview";
 $DotNetVersion = "1.0.4";
 $DotNetInstallerUri = "https://dot.net/v1/dotnet-install.ps1";
