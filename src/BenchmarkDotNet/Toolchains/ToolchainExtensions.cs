93dc6e638aab62dddd6bba50aa13b04645663cfd
Adam Sitnik <adam.sitnik@gmail.com>
2017-08-13 09:30:56 +0200

    Remove project.json support, fixes #526

--- src/BenchmarkDotNet/Toolchains/ToolchainExtensions.cs
+++ src/BenchmarkDotNet/Toolchains/ToolchainExtensions.cs
@@ -1,24 +1,16 @@
 ﻿using System;
-using System.IO;
-using System.Linq;
 using BenchmarkDotNet.Environments;
 using BenchmarkDotNet.Jobs;
 using BenchmarkDotNet.Toolchains.CsProj;
-using BenchmarkDotNet.Toolchains.DotNetCli;
-using BenchmarkDotNet.Toolchains.ProjectJson;
 
 namespace BenchmarkDotNet.Toolchains
 {
     internal static class ToolchainExtensions
     {
-        private static readonly Lazy<bool> isUsingProjectJson = new Lazy<bool>(IsUsingProjectJson);
-
         internal static IToolchain GetToolchain(this Job job)
-        {
-            return job.HasValue(InfrastructureMode.ToolchainCharacteristic)
+            => job.HasValue(InfrastructureMode.ToolchainCharacteristic)
                 ? job.Infrastructure.Toolchain
                 : GetToolchain(job.ResolveValue(EnvMode.RuntimeCharacteristic, EnvResolver.Instance));
-        }
 
         internal static IToolchain GetToolchain(this Runtime runtime)
         {
@@ -29,27 +21,13 @@ namespace BenchmarkDotNet.Toolchains
 #if CLASSIC
                     return new Roslyn.RoslynToolchain();
 #else
-                    return isUsingProjectJson.Value ? ProjectJsonNet46Toolchain.Instance : CsProjClassicNetToolchain.Current.Value;
+                    return CsProjClassicNetToolchain.Current.Value;
 #endif
                 case CoreRuntime core:
-                    return isUsingProjectJson.Value ? ProjectJsonCoreToolchain.Current.Value : CsProjCoreToolchain.Current.Value;
+                    return CsProjCoreToolchain.Current.Value;
                 default:
                     throw new ArgumentOutOfRangeException(nameof(runtime), runtime, "Runtime not supported");
             }
-        }
-
-        private static bool IsUsingProjectJson() => 
-            HostEnvironmentInfo.GetCurrent().DotNetSdkVersion.Value.Contains("preview") 
-            && SolutionDirectoryContainsProjectJsonFiles();
-
-        private static bool SolutionDirectoryContainsProjectJsonFiles()
-        {
-            if (!DotNetCliGenerator.GetSolutionRootDirectory(out var solutionRootDirectory))
-            {
-                solutionRootDirectory = new DirectoryInfo(Directory.GetCurrentDirectory());
-            }
-
-            return solutionRootDirectory.EnumerateFiles("project.json", SearchOption.AllDirectories).Any();
         }
     }
 }
