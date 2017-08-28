8e692728b8f1ffac51aa330a162c9e08cd272faa
Łukasz Pyrzyk <lukasz.pyrzyk@gmail.com>
2017-08-28 09:54:52 +0200

    Running VmIdentifier on the Appveyor

---
+++ tests/BenchmarkDotNet.Tests/XUnit/AppveyorOnlyFactAttribute.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using BenchmarkDotNet.Portability;
+using Xunit;
+
+namespace BenchmarkDotNet.Tests.XUnit
+{
+    public class AppveyorOnlyFactAttribute : FactAttribute
+    {
+        private static readonly string message = "Test is available only on AppVeyor";
+
+        static AppveyorOnlyFactAttribute()
+        {
+            string value = Environment.GetEnvironmentVariable("APPVEYOR");
+            if (!string.IsNullOrEmpty(value) && value.EqualsWithIgnoreCase("true"))
+            {
+                message = null;
+            }
+        }
+
+        public override string Skip => message;
+    }
+}
