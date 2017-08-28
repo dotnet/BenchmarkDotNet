28e89461afe32bec63a398798f9e840b563d73c6
Łukasz Pyrzyk <lukasz.pyrzyk@gmail.com>
2017-08-28 09:17:49 +0200

    Removed redundant #IF in stringextensions

--- src/BenchmarkDotNet.Core/Portability/StringExtensions.cs
+++ src/BenchmarkDotNet.Core/Portability/StringExtensions.cs
@@ -6,12 +6,7 @@ namespace BenchmarkDotNet.Portability
     {
         internal static bool EqualsWithIgnoreCase(this string left, string right)
         {
-#if !CORE
             return left.Equals(right, IgnoreCaseStringComparison);
-#else
-            // http://stackoverflow.com/questions/14600694/where-has-stringcomparison-invariantcultureignorecase-gone
-            return left.Equals(right, StringComparison.OrdinalIgnoreCase);
-#endif
         }
 
         internal static bool ContainsWithIgnoreCase(this string text, string word)
