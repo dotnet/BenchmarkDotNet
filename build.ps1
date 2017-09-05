feabd17a535493cdf108dcab772f98f36f5da653
Adam Sitnik <adam.sitnik@gmail.com>
2017-09-05 11:50:32 +0200

    bump the .NET Core version, #539

--- build.ps1
+++ build.ps1
@@ -33,7 +33,7 @@ Param(
 
 $CakeVersion = "0.21.1"
 $DotNetChannel = "preview";
-$DotNetVersion = "1.0.4";
+$DotNetVersion = "2.0.0";
 $DotNetInstallerUri = "https://dot.net/v1/dotnet-install.ps1";
 $NugetUrl = "https://dist.nuget.org/win-x86-commandline/latest/nuget.exe"
 
