6003316e433074daed203a37655f913cb35703cf
Christopher Gozdziewski <Chrisgozd92@gmail.com>
2017-11-22 20:04:59 +0000

    Fix more formatting

--- samples/BenchmarkDotNet.Samples/Program.cs
+++ samples/BenchmarkDotNet.Samples/Program.cs
@@ -1,12 +1,5 @@
 ﻿using System.Reflection;
 using BenchmarkDotNet.Running;
-using BenchmarkDotNet.Attributes;
-using System.Threading;
-using BenchmarkDotNet.Attributes.Jobs;
-using BenchmarkDotNet.Configs;
-using BenchmarkDotNet.Validators;
-using System.Linq;
-using System;
 
 namespace BenchmarkDotNet.Samples
 {
