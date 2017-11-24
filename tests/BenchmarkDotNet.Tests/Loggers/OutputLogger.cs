3f17763e2467cfcf74f82b7f69c95fce79aa2e79
Aleksei Kudelia <dekko.ru@yandex.ru>
2017-11-24 15:12:26 +1100

    Removed a xunit workaround because an issue has been fixed

--- tests/BenchmarkDotNet.Tests/Loggers/OutputLogger.cs
+++ tests/BenchmarkDotNet.Tests/Loggers/OutputLogger.cs
@@ -16,7 +16,7 @@ namespace BenchmarkDotNet.Tests.Loggers
 
         public override void Write(LogKind logKind, string text)
         {
-            currentLine += RemoveInvalidChars(text);
+            currentLine += text;
             base.Write(logKind, text);
         }
 
@@ -29,24 +29,9 @@ namespace BenchmarkDotNet.Tests.Loggers
 
         public override void WriteLine(LogKind logKind, string text)
         {
-            testOutputHelper.WriteLine(currentLine + RemoveInvalidChars(text));
+            testOutputHelper.WriteLine(currentLine + text);
             currentLine = "";
             base.WriteLine(logKind, text);
         }
-
-        #region Xunit bug workaround
-
-        /// <summary>
-        /// Workaround for xunit bug: https://github.com/xunit/xunit/issues/876
-        /// </summary>
-        private static string RemoveInvalidChars(string text)
-        {
-            if (string.IsNullOrEmpty(text))
-                return string.Empty;
-
-            return text.Replace((char) 0x1B, ' ');
-        }
-
-        #endregion
     }
 }
