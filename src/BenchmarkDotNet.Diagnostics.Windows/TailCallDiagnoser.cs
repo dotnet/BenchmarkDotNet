c1a4b20b11165e696f198e0e68a0a5c2b991b65e
George Plotnikov <accembler@gmail.com>
2017-12-04 23:31:48 +0300

    Сreate a tail call diagnoser (#573)
    * create a tail call diagnoser
    
    * added newline at the end of file
    
    * updated to support F#
    
    * fix proj references
    
    * rename local variables for unification
    
    * fix typo
    
    * added intergration test for the TailCallDiagnoser
    
    * chaged directives regarding the project
    
    * fixed warnings
    
    * refactored
    
    * applied the review comments

---
+++ src/BenchmarkDotNet.Diagnostics.Windows/TailCallDiagnoser.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections.Generic;
+using BenchmarkDotNet.Running;
+using Microsoft.Diagnostics.Tracing.Session;
+using BenchmarkDotNet.Loggers;
+using System;
+
+namespace BenchmarkDotNet.Diagnostics.Windows
+{
+    //See https://blogs.msdn.microsoft.com/clrcodegeneration/2009/05/11/jit-etw-tracing-in-net-framework-4/
+    public class TailCallDiagnoser : JitDiagnoser
+    {
+        private static readonly string LogSeparator = new string('-', 20);
+        public const string DiagnoserId = nameof(TailCallDiagnoser);
+        public override IEnumerable<string> Ids => new[] { DiagnoserId };
+
+        private readonly bool logFailuresOnly = true;
+        private readonly bool filterByNamespace = true;
+        private string expectedNamespace;
+
+        public TailCallDiagnoser()
+        {
+
+        }
+
+        /// <summary>
+        /// creates the new TailCallDiagnoser instance
+        /// </summary>
+        /// <param name="logFailuresOnly">only the methods that failed to get tail called. True by default.</param>
+        /// <param name="filterByNamespace">only the methods from declaring type's namespace. Set to false if you want to see all Jit tail events. True by default.</param>
+        public TailCallDiagnoser(bool logFailuresOnly, bool filterByNamespace)
+        {
+            this.logFailuresOnly = logFailuresOnly;
+            this.filterByNamespace = filterByNamespace;
+        }
+
+        protected override void AttachToEvents(TraceEventSession traceEventSession, Benchmark benchmark)
+        {
+            expectedNamespace = benchmark.Target.Method.DeclaringType.Namespace ?? benchmark.Target.Method.DeclaringType.FullName;
+
+            Logger.WriteLine();
+            Logger.WriteLineHeader(LogSeparator);
+            Logger.WriteLineInfo($"{benchmark.DisplayInfo}");
+            Logger.WriteLineHeader(LogSeparator);
+
+            traceEventSession.Source.Clr.MethodTailCallSucceeded += jitData =>
+            {
+                if (!logFailuresOnly && ShouldPrintEventInfo(jitData.CallerNamespace, jitData.CalleeNamespace))
+                {
+                    if (StatsPerProcess.TryGetValue(jitData.ProcessID, out object ignored))
+                    {
+                        Logger.WriteLineHelp($"Caller: {jitData.CallerNamespace}.{jitData.CallerName} - {jitData.CallerNameSignature}");
+                        Logger.WriteLineHelp($"Callee: {jitData.CalleeNamespace}.{jitData.CalleeName} - {jitData.CalleeNameSignature}");
+                        Logger.WriteLineHelp($"Tail prefix: {jitData.TailPrefix}");
+                        Logger.WriteLineHelp($"Tail call type: {jitData.TailCallType}");
+                        Logger.WriteLineHeader(LogSeparator);
+                    }
+                }
+            };
+            traceEventSession.Source.Clr.MethodTailCallFailed += jitData =>
+            {
+                if (ShouldPrintEventInfo(jitData.CallerNamespace, jitData.CalleeNamespace))
+                {
+                    if (StatsPerProcess.TryGetValue(jitData.ProcessID, out object ignored))
+                    {
+                        Logger.WriteLineHelp($"Caller: {jitData.CallerNamespace}.{jitData.CallerName} - {jitData.CallerNameSignature}");
+                        Logger.WriteLineHelp($"Callee: {jitData.CalleeNamespace}.{jitData.CalleeName} - {jitData.CalleeNameSignature}");
+                        Logger.WriteLineError($"Fail Reason: {jitData.FailReason}");
+                        Logger.WriteLineHeader(LogSeparator);
+                    }
+                }
+            };
+        }
+
+        private bool ShouldPrintEventInfo(string callerNamespace, string calleeNamespace) => 
+            !filterByNamespace
+                    || (!string.IsNullOrEmpty(callerNamespace) && callerNamespace.StartsWith(expectedNamespace))
+                    || (!string.IsNullOrEmpty(calleeNamespace) && calleeNamespace.StartsWith(expectedNamespace));
+    }
+}
---
+++ tests/BenchmarkDotNet.IntegrationTests/TailCallDiagnoserTests.cs
@@ -0,0 +1,106 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Columns;
+using BenchmarkDotNet.Configs;
+#if NET46
+using BenchmarkDotNet.Diagnostics.Windows;
+using BenchmarkDotNet.Diagnostics.Windows.Configs;
+using BenchmarkDotNet.Environments;
+#endif
+using BenchmarkDotNet.IntegrationTests.Xunit;
+using BenchmarkDotNet.Jobs;
+using BenchmarkDotNet.Running;
+using BenchmarkDotNet.Tests.Loggers;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace BenchmarkDotNet.IntegrationTests
+{
+#if NET46
+    public class TailCallDiagnoserTests : BenchmarkTestExecutor
+    {
+        private const string WindowsOnly = "Use JIT ETW Tail Call Event (Windows only)";
+        private const string TAIL_CALL_MARK = "Tail call type";
+
+        public TailCallDiagnoserTests(ITestOutputHelper outputHelper) : base(outputHelper)
+        {
+
+        }
+
+        public static IEnumerable<object[]> GetJits()
+            => new[]
+            {
+                new object[] { Jit.LegacyJit, Platform.X64, Runtime.Clr }, // 64bit LegacyJit for desktop .NET
+                new object[] { Jit.RyuJit, Platform.X64, Runtime.Clr }, // RyuJit for desktop .NET
+            };
+
+        public class TailCallBenchmarks
+        {
+            private long FactorialWithTailing(int pos, int depth)
+            {
+                return pos == 0 ? depth : FactorialWithTailing(pos - 1, depth * pos);
+            }
+
+            private long FactorialWithTailing(int depth)
+            {
+                return FactorialWithTailing(1, depth);
+            }
+            
+            [Benchmark]
+            public long Factorial()
+            {
+                return FactorialWithTailing(7);
+            }
+        }
+        
+        public class NonTailCallBenchmarks
+        {
+            private long FactorialWithoutTailing(int depth)
+            {
+                return depth == 0 ? 1 : depth * FactorialWithoutTailing(depth - 1);
+            }
+
+            [Benchmark]
+            public long Factorial()
+            {
+                return FactorialWithoutTailing(7);
+            }
+        }
+
+        [TheoryWindowsOnly(WindowsOnly)]
+        [MemberData(nameof(GetJits))]
+        [Trait(Constants.Category, Constants.BackwardCompatibilityCategory)]
+        public void TailCallDiagnoserCatchesTailCallEvents(Jit jit, Platform platform, Runtime runtime)
+        {
+            var output = Execute<TailCallBenchmarks>(jit, platform, runtime);
+            Assert.True(output.CapturedOutput.Where(x=>x.Text.Contains(TAIL_CALL_MARK)).Count() > 0);
+        }
+
+        [TheoryWindowsOnly(WindowsOnly)]
+        [MemberData(nameof(GetJits))]
+        [Trait(Constants.Category, Constants.BackwardCompatibilityCategory)]
+        public void TailCallDiagnoserNotCatchesTailCallEvents(Jit jit, Platform platform, Runtime runtime)
+        {
+            var output = Execute<NonTailCallBenchmarks>(jit, platform, runtime);
+            Assert.True(output.CapturedOutput.Where(x => x.Text.Contains(TAIL_CALL_MARK)).Count() == 0);
+        }
+
+        private LogCapture Execute<T>(Jit jit, Platform platform, Runtime runtime)
+        {
+            var tailCallDiagnoser = new TailCallDiagnoser(false, true);
+            CanExecute<T>(CreateConfig(jit, platform, runtime, tailCallDiagnoser));
+            var output = (LogCapture)(tailCallDiagnoser as EtwDiagnoser<object>).GetType().GetField("Logger", BindingFlags.Instance | BindingFlags.NonPublic).GetValue(tailCallDiagnoser);
+            return output;
+        }
+
+        private IConfig CreateConfig(Jit jit, Platform platform, Runtime runtime, TailCallDiagnoser diagnoser) => ManualConfig.CreateEmpty()
+            .With(Job.Dry.With(jit).With(platform).With(runtime))
+            .With(DefaultConfig.Instance.GetLoggers().ToArray())
+            .With(DefaultColumnProviders.Instance)
+            .With(diagnoser)
+            .With(new OutputLogger(Output));
+    }
+#endif
+}
