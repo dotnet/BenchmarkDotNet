688295b491cb8c5fee4040c9e9dba4291310e864
Andrey Akinshin <andrey.akinshin@gmail.com>
2018-01-13 21:41:44 +0300

    Revert Samples/Program.cs

--- samples/BenchmarkDotNet.Samples/Program.cs
+++ samples/BenchmarkDotNet.Samples/Program.cs
@@ -1,22 +1,13 @@
 ﻿using System.Reflection;
-using BenchmarkDotNet.Attributes;
-using BenchmarkDotNet.Attributes.Jobs;
 using BenchmarkDotNet.Running;
 
 namespace BenchmarkDotNet.Samples
 {
-    [DryJob]
-    public class Bench
-    {
-        [Benchmark(Description = "Benchmarking Container.Resolve<SomeType>()")]
-        public void Foo() {}
-    }
     class Program
     {
         static void Main(string[] args)
         {
-            BenchmarkRunner.Run<Bench>();
-//            BenchmarkSwitcher.FromAssembly(typeof(Program).GetTypeInfo().Assembly).Run(args);
+            BenchmarkSwitcher.FromAssembly(typeof(Program).GetTypeInfo().Assembly).Run(args);
         }
     }
 }
