c2c4e8cc5ff71c8974b528d692096c6de0f0eba4
Adam Sitnik <adam.sitnik@gmail.com>
2018-04-07 16:24:42 +0200

    set COMPLUS_Version env var for private Clr builds even if no env vars were defined, #706

--- src/BenchmarkDotNet.Core/Extensions/ProcessExtensions.cs
+++ src/BenchmarkDotNet.Core/Extensions/ProcessExtensions.cs
@@ -102,18 +102,14 @@ namespace BenchmarkDotNet.Extensions
         internal static void SetEnvironmentVariables(this ProcessStartInfo start, Benchmark benchmark, IResolver resolver)
         {
 #if !NETCOREAPP1_1 // ProcessStartInfo.EnvironmentVariables is avaialable for .NET Core 2.0+
+            if (benchmark.Job.Env.Runtime is ClrRuntime clrRuntime && !string.IsNullOrEmpty(clrRuntime.Version))
+                start.EnvironmentVariables["COMPLUS_Version"] = clrRuntime.Version;
+
             if (!benchmark.Job.HasValue(InfrastructureMode.EnvironmentVariablesCharacteristic))
                 return;
 
             foreach (var environmentVariable in benchmark.Job.Infrastructure.EnvironmentVariables)
-            {
                 start.EnvironmentVariables[environmentVariable.Key] = environmentVariable.Value;
-            }
-
-            if (benchmark.Job.Env.Runtime is ClrRuntime clrRuntime && !string.IsNullOrEmpty(clrRuntime.Version))
-            {
-                start.EnvironmentVariables["COMPLUS_Version"] = clrRuntime.Version;
-            }
 #endif
         }
     }
