927d18e3d68b373e79164f9af07a134ca64754f0
Lucas Trzesniewski <lucas.trzesniewski@gmail.com>
2018-05-03 00:10:49 +0200

    Add ReturnValueValidator

---
+++ src/BenchmarkDotNet/Validators/ReturnValueValidator.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using BenchmarkDotNet.Parameters;
+using BenchmarkDotNet.Running;
+using BenchmarkDotNet.Toolchains.InProcess;
+
+namespace BenchmarkDotNet.Validators
+{
+    public class ReturnValueValidator : ExecutionValidatorBase
+    {
+        public static ReturnValueValidator DontFailOnError { get; } = new ReturnValueValidator(false);
+        public static ReturnValueValidator FailOnError { get; } = new ReturnValueValidator(true);
+
+        private ReturnValueValidator(bool failOnError)
+            : base(failOnError) { }
+
+        protected override void ExecuteBenchmarks(object benchmarkTypeInstance, IEnumerable<Benchmark> benchmarks, List<ValidationError> errors)
+        {
+            foreach (var parameterGroup in benchmarks.GroupBy(i => i.Parameters, ParameterInstancesEqualityComparer.Instance))
+            {
+                try
+                {
+                    InProcessRunner.FillMembers(benchmarkTypeInstance, parameterGroup.First());
+                }
+                catch (Exception ex)
+                {
+                    errors.Add(new ValidationError(
+                        TreatsWarningsAsErrors,
+                        $"Failed to set benchmark parameters: '{parameterGroup.First().Parameters.DisplayInfo}', exception was: '{ex.Message}'"));
+                }
+
+                var results = new List<(Benchmark benchmark, object returnValue)>();
+                bool hasErrorsInGroup = false;
+
+                foreach (var benchmark in parameterGroup)
+                {
+                    try
+                    {
+                        var result = benchmark.Target.Method.Invoke(benchmarkTypeInstance, null);
+
+                        if (benchmark.Target.Method.ReturnType != typeof(void))
+                            results.Add((benchmark, result));
+                    }
+                    catch (Exception ex)
+                    {
+                        hasErrorsInGroup = true;
+
+                        errors.Add(new ValidationError(
+                            TreatsWarningsAsErrors,
+                            $"Failed to execute benchmark '{benchmark.DisplayInfo}', exception was: '{ex.Message}'",
+                            benchmark));
+                    }
+                }
+
+                if (hasErrorsInGroup || results.Count == 0)
+                    continue;
+
+                if (results.Any(result => !Equals(result.returnValue, results[0].returnValue)))
+                {
+                    errors.Add(new ValidationError(
+                        TreatsWarningsAsErrors,
+                        $"Inconsistent benchmark return values in {results[0].benchmark.Target.TypeInfo}: {string.Join(", ", results.Select(r => $"{r.benchmark.Target.MethodDisplayInfo}: {r.returnValue}"))} {parameterGroup.Key.DisplayInfo}"));
+                }
+            }
+        }
+
+        private class ParameterInstancesEqualityComparer : IEqualityComparer<ParameterInstances>
+        {
+            public static ParameterInstancesEqualityComparer Instance { get; } = new ParameterInstancesEqualityComparer();
+
+            public bool Equals(ParameterInstances x, ParameterInstances y)
+            {
+                if (ReferenceEquals(x, y))
+                    return true;
+
+                if (x == null || y == null)
+                    return false;
+
+                if (x.Count != y.Count)
+                    return false;
+
+                return x.Items.OrderBy(i => i.Name).Zip(y.Items.OrderBy(i => i.Name), (a, b) => a.Name == b.Name && Equals(a.Value, b.Value)).All(i => i);
+            }
+
+            public int GetHashCode(ParameterInstances obj)
+            {
+                if (obj.Count == 0)
+                    return 0;
+
+                unchecked
+                {
+                    int result = 0;
+
+                    foreach (var paramInstance in obj.Items.OrderBy(i => i.Name))
+                    {
+                        result = (result * 397) ^ paramInstance.Name.GetHashCode();
+                        result = (result * 397) ^ (paramInstance.Value?.GetHashCode() ?? 0);
+                    }
+
+                    return result;
+                }
+            }
+        }
+    }
+}
---
+++ tests/BenchmarkDotNet.Tests/Validators/ReturnValueValidatorTests.cs
@@ -0,0 +1,126 @@
+﻿using System.Linq;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+using BenchmarkDotNet.Validators;
+using Xunit;
+
+namespace BenchmarkDotNet.Tests.Validators
+{
+    public class ReturnValueValidatorTests
+    {
+        private const string ErrorMessagePrefix = "Inconsistent benchmark return values";
+
+        [Fact]
+        public void InconsistentReturnValuesAreDiscovered()
+        {
+            var validationErrors = ReturnValueValidator.FailOnError.Validate(BenchmarkConverter.TypeToBenchmarks(typeof(InconsistentResults))).ToList();
+
+            Assert.NotEmpty(validationErrors);
+            Assert.StartsWith(ErrorMessagePrefix, validationErrors.Single().Message);
+        }
+
+        public class InconsistentResults
+        {
+            [Benchmark]
+            public int Foo() => 42;
+
+            [Benchmark]
+            public int Bar() => 41;
+        }
+
+        [Fact]
+        public void ConsistentReturnValuesAreOmitted()
+        {
+            var validationErrors = ReturnValueValidator.FailOnError.Validate(BenchmarkConverter.TypeToBenchmarks(typeof(ConsistentResults))).ToList();
+
+            Assert.Empty(validationErrors);
+        }
+
+        public class ConsistentResults
+        {
+            [Benchmark]
+            public int Foo() => 42;
+
+            [Benchmark]
+            public int Bar() => 42;
+        }
+
+        [Fact]
+        public void BenchmarksWithOnlyVoidMethodsAreOmitted()
+        {
+            var validationErrors = ReturnValueValidator.FailOnError.Validate(BenchmarkConverter.TypeToBenchmarks(typeof(VoidMethods))).ToList();
+
+            Assert.Empty(validationErrors);
+        }
+
+        public class VoidMethods
+        {
+            [Benchmark]
+            public void Foo() { }
+
+            [Benchmark]
+            public void Bar() { }
+        }
+
+        [Fact]
+        public void VoidMethodsAreIgnored()
+        {
+            var validationErrors = ReturnValueValidator.FailOnError.Validate(BenchmarkConverter.TypeToBenchmarks(typeof(ConsistentResultsWithVoidMethod))).ToList();
+
+            Assert.Empty(validationErrors);
+        }
+
+        public class ConsistentResultsWithVoidMethod
+        {
+            [Benchmark]
+            public int Foo() => 42;
+
+            [Benchmark]
+            public int Bar() => 42;
+
+            [Benchmark]
+            public void Baz() { }
+        }
+
+        [Fact]
+        public void ConsistentReturnValuesInParameterGroupAreOmitted()
+        {
+            var validationErrors = ReturnValueValidator.FailOnError.Validate(BenchmarkConverter.TypeToBenchmarks(typeof(ConsistentResultsPerParameterGroup))).ToList();
+
+            Assert.Empty(validationErrors);
+        }
+
+        public class ConsistentResultsPerParameterGroup
+        {
+            [Params(1, 2, 3)]
+            public int Value { get; set; }
+
+            [Benchmark]
+            public int Foo() => Value;
+
+            [Benchmark]
+            public int Bar() => Value;
+        }
+
+        [Fact]
+        public void InconsistentReturnValuesInParameterGroupAreDetected()
+        {
+            var validationErrors = ReturnValueValidator.FailOnError.Validate(BenchmarkConverter.TypeToBenchmarks(typeof(InconsistentResultsPerParameterGroup))).ToList();
+
+            Assert.Equal(2, validationErrors.Count);
+            Assert.All(validationErrors, error => Assert.StartsWith(ErrorMessagePrefix, error.Message));
+        }
+
+        public class InconsistentResultsPerParameterGroup
+        {
+            [Params(1, 2, 3)]
+            public int Value { get; set; }
+
+            [Benchmark]
+            public int Foo() => Value;
+
+            [Benchmark]
+            public int Bar() => 2;
+        }
+    }
+}
