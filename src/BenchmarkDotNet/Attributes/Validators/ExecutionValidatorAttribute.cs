30f0873ed9d2cbef34894d362bf533e15996923a
Lucas Trzesniewski <lucas.trzesniewski@gmail.com>
2018-05-03 00:20:19 +0200

    Add validator attributes

---
+++ src/BenchmarkDotNet/Attributes/Validators/ExecutionValidatorAttribute.cs
@@ -0,0 +1,13 @@
+﻿using BenchmarkDotNet.Validators;
+
+namespace BenchmarkDotNet.Attributes.Validators
+{
+    public class ExecutionValidatorAttribute : ValidatorConfigBaseAttribute
+    {
+        public ExecutionValidatorAttribute()
+            : this(true) { }
+
+        public ExecutionValidatorAttribute(bool failOnError)
+            : base(failOnError ? ExecutionValidator.FailOnError : ExecutionValidator.DontFailOnError) { }
+    }
+}
