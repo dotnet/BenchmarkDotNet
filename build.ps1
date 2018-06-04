7b9c313962b68aff6cc1d9575644bc852172c72a
Adam Sitnik <adam.sitnik@gmail.com>
2018-06-04 23:56:37 +0200

    trying to fix the build

--- build.ps1
+++ build.ps1
@@ -78,6 +78,7 @@ $env:PATH = "$InstallPath;$env:PATH"
 
 $env:DOTNET_SKIP_FIRST_TIME_EXPERIENCE=1
 $env:DOTNET_CLI_TELEMETRY_OPTOUT=1
+$env:DOTNET_MULTILEVEL_LOOKUP=0
 
 ###########################################################################
 # INSTALL NUGET
