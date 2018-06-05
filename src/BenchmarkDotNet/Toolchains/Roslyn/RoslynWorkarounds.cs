d9959b847e7a1d06fe677e02dac65b1857ab895c
Adam Sitnik <adam.sitnik@gmail.com>
2018-06-05 11:18:39 +0200

    use ugly reflection to set IgnoreCorLibraryDuplicatedTypes for Roslyn, gets rid of error about duplicate types in mscorlib and ValueTuple (or sth else)

---
+++ src/BenchmarkDotNet/Toolchains/Roslyn/RoslynWorkarounds.cs
@@ -0,0 +1,27 @@
+﻿using System.Reflection;
+using Microsoft.CodeAnalysis.CSharp;
+
+namespace BenchmarkDotNet.Toolchains.Roslyn
+{
+    internal static class RoslynWorkarounds
+    {
+        internal static CSharpCompilationOptions WithIgnoreCorLibraryDuplicatedTypes(this CSharpCompilationOptions compilationOptions)
+        {
+            // this prevents from
+            // The type 'ValueTuple<T1>' exists in both 'System.ValueTuple, Version=4.0.3.0, Culture=neutral, PublicKeyToken=cc7b13ffcd2ddd51' and 'mscorlib, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089'
+            // idea taken from: https://github.com/OmniSharp/omnisharp-roslyn/pull/785
+            
+            var topLevelBinderFlagsProperty = typeof(CSharpCompilationOptions).GetProperty("TopLevelBinderFlags", BindingFlags.Instance | BindingFlags.NonPublic);
+            var binderFlagsType = typeof(CSharpCompilationOptions).GetTypeInfo().Assembly.GetType("Microsoft.CodeAnalysis.CSharp.BinderFlags");
+
+            var ignoreCorLibraryDuplicatedTypesMember = binderFlagsType?.GetField("IgnoreCorLibraryDuplicatedTypes", BindingFlags.Static | BindingFlags.Public);
+            var ignoreCorLibraryDuplicatedTypesValue = ignoreCorLibraryDuplicatedTypesMember?.GetValue(null);
+            if (ignoreCorLibraryDuplicatedTypesValue != null)
+            {
+                topLevelBinderFlagsProperty?.SetValue(compilationOptions, ignoreCorLibraryDuplicatedTypesValue);
+            }
+
+            return compilationOptions;
+        }
+    }
+}
