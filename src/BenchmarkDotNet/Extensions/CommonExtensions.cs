7c43da0a9a8f7f7de77a1af71b436094034214c3
Andrey Akinshin <andrey.akinshin@gmail.com>
2018-06-10 10:21:57 +0300

    CommonExtensions cleanup
    * Make it internal (hide it in docs)
    * Remove obsolete methods
    * Fix formatting

--- src/BenchmarkDotNet/Extensions/CommonExtensions.cs
+++ src/BenchmarkDotNet/Extensions/CommonExtensions.cs
@@ -10,33 +10,27 @@ using System.Text;
 
 namespace BenchmarkDotNet.Extensions
 {
-    public static class CommonExtensions
+    internal static class CommonExtensions
     {
-        public static List<T> ToSortedList<T>(this IEnumerable<T> values)
-        {
-            var list = new List<T>();
-            list.AddRange(values);
-            list.Sort();
-            return list;
-        }
-
-        public static string ToTimeStr(this double value, TimeUnit unit = null, int unitNameWidth = 1, bool showUnit = true, string format = "N4", Encoding encoding = null)
+        public static string ToTimeStr(this double value, TimeUnit unit = null, int unitNameWidth = 1, bool showUnit = true, string format = "N4",
+            Encoding encoding = null)
         {
             unit = unit ?? TimeUnit.GetBestTimeUnit(value);
-            var unitValue = TimeUnit.Convert(value, TimeUnit.Nanosecond, unit);
+            double unitValue = TimeUnit.Convert(value, TimeUnit.Nanosecond, unit);
             if (showUnit)
             {
-                var unitName = unit.Name.ToString(encoding ?? Encoding.ASCII).PadLeft(unitNameWidth);
+                string unitName = unit.Name.ToString(encoding ?? Encoding.ASCII).PadLeft(unitNameWidth);
                 return $"{unitValue.ToStr(format)} {unitName}";
             }
 
             return $"{unitValue.ToStr(format)}";
         }
-        
+
         public static string ToTimeStr(this double value, TimeUnit unit, Encoding encoding, string format = "N4", int unitNameWidth = 1, bool showUnit = true)
             => value.ToTimeStr(unit, unitNameWidth, showUnit, format, encoding);
-        
-        public static string ToTimeStr(this double value, Encoding encoding, TimeUnit unit = null, string format = "N4", int unitNameWidth = 1, bool showUnit = true)
+
+        public static string ToTimeStr(this double value, Encoding encoding, TimeUnit unit = null, string format = "N4", int unitNameWidth = 1,
+            bool showUnit = true)
             => value.ToTimeStr(unit, unitNameWidth, showUnit, format, encoding);
 
         public static string ToSizeStr(this long value, SizeUnit unit = null, int unitNameWidth = 1, bool showUnit = true)
@@ -45,13 +39,11 @@ namespace BenchmarkDotNet.Extensions
             var unitValue = SizeUnit.Convert(value, SizeUnit.B, unit);
             if (showUnit)
             {
-                var unitName = unit.Name.PadLeft(unitNameWidth);
+                string unitName = unit.Name.PadLeft(unitNameWidth);
                 return string.Format(HostEnvironmentInfo.MainCultureInfo, "{0:0.##} {1}", unitValue, unitName);
             }
-            else
-            {
-                return string.Format(HostEnvironmentInfo.MainCultureInfo, "{0:0.##}", unitValue);
-            }
+
+            return string.Format(HostEnvironmentInfo.MainCultureInfo, "{0:0.##}", unitValue);
         }
 
         public static string ToStr(this double value, string format = "0.##")
@@ -81,6 +73,8 @@ namespace BenchmarkDotNet.Extensions
                     return $"{column.ColumnName} [{style.SizeUnit.Name}]";
                 case UnitType.Time:
                     return $"{column.ColumnName} [{style.TimeUnit.Name}]";
+                case UnitType.Dimensionless:
+                    return column.ColumnName;
                 default:
                     return column.ColumnName;
             }
@@ -90,40 +84,30 @@ namespace BenchmarkDotNet.Extensions
         public static bool IsEmpty<T>(this IReadOnlyCollection<T> value) => value.Count == 0;
         public static bool IsEmpty<T>(this IEnumerable<T> value) => !value.Any();
 
-        public static T Penult<T>(this IList<T> list) => list[list.Count - 2];
-
-        public static bool IsOneOf<T>(this T value, params T[] values) => values.Contains(value);
-
 #if !NETCOREAPP2_1 // method with the same name was added to Dictionary in .NET Core 2.1, so we need this ulgy hack to get compiler happy
         public static TValue GetValueOrDefault<TKey, TValue>(this IDictionary<TKey, TValue> dictionary, TKey key)
-            => dictionary.TryGetValue(key, out TValue value) ? value : default;
+            => dictionary.TryGetValue(key, out var value) ? value : default;
 #endif
         public static double Sqr(this double x) => x * x;
         public static double Pow(this double x, double k) => Math.Pow(x, k);
 
-        public static int RoundToInt(this double x) => (int) Math.Round(x);
-        public static long RoundToLong(this double x) => (long) Math.Round(x);
-
         internal static IEnumerable<TItem> DistinctBy<TItem, TValue>(this IEnumerable<TItem> items, Func<TItem, TValue> selector)
             => DistinctBy(items, selector, EqualityComparer<TValue>.Default);
 
-        internal static IEnumerable<TItem> DistinctBy<TItem, TValue>(this IEnumerable<TItem> items, Func<TItem, TValue> selector, IEqualityComparer<TValue> equalityComparer)
+        private static IEnumerable<TItem> DistinctBy<TItem, TValue>(this IEnumerable<TItem> items, Func<TItem, TValue> selector,
+            IEqualityComparer<TValue> equalityComparer)
         {
             var seen = new HashSet<TValue>(equalityComparer);
 
             foreach (var item in items)
-            {
                 if (seen.Add(selector(item)))
                     yield return item;
-            }
         }
 
         internal static void ForEach<T>(this IList<T> source, Action<T> command)
         {
             foreach (var item in source)
-            {
                 command(item);
-            }
         }
 
         internal static string CreateIfNotExists(this string directoryPath)
