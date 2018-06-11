44ea0f54ea6a9bfdc16efcfeb804a33dc60c5f09
Andrey Akinshin <andrey.akinshin@gmail.com>
2018-06-11 15:09:19 +0300

    docs: add changelog (#789)

---
+++ docs/changelog-generator/download.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+start=`date +%s`
+echo "$1: download started"
+
+cd ChangeLogBuilder
+dotnet run -- "$@"
+cd ..
+
+end=`date +%s`
+echo "$1: download fininshed ($((end-start)) seconds)"
+
+DETAILS=_details
+
+if [ ! -d "$DETAILS" ]; then
+  mkdir "$DETAILS"
+fi
+
+cp ChangeLogBuilder/$1.md $DETAILS/$1.md
--- docs/docfx-serve.sh
+++ docs/docfx-serve.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
 
+cd ./changelog-generator/
+./generate-all.sh
+cd ..
 mono ./docfx-bin/docfx.exe docfx.json --serve
