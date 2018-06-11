b16b83fa5437b637fbd230777f218f12cf771aae
Andrey Akinshin <andrey.akinshin@gmail.com>
2018-06-11 13:09:07 +0300

    docs: samples for setup and cleanup

---
+++ samples/BenchmarkDotNet.Samples/IntroSetupCleanupGlobal.cs
@@ -0,0 +1,33 @@
+﻿using BenchmarkDotNet.Attributes;
+
+namespace BenchmarkDotNet.Samples
+{
+    public class IntroSetupCleanupGlobal
+    {
+        [Params(10, 100, 1000)]
+        public int N;
+
+        private int[] data;
+
+        [GlobalSetup]
+        public void GlobalSetup()
+        {
+            data = new int[N]; // executed once per each N value
+        }
+        
+        [Benchmark]
+        public int Logic()
+        {
+            int res = 0;
+            for (int i = 0; i < N; i++)
+                res += data[i];
+            return res;
+        }
+        
+        [GlobalCleanup]
+        public void GlobalCleanup()
+        {
+            // Disposing logic
+        }
+    }
+}
