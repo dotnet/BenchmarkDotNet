dotnet/BenchmarkDotNet

commit edf7f65d825ecd206b3dbec903a2492220dbb1e7

Improve filtering from console args (#785)
* improved command line parsing support

* re-added --joined support

* remove dead code from Summary

* print run time and number of executed benchmarks in total

* docs ;)

* post code review fixes

* fix

src/BenchmarkDotNet/Running/BenchmarkRunner.cs
@@ -102,7 +102,29 @@ namespace BenchmarkDotNet.Running
                         .ToDictionary(info => info.Benchmark, info => (info.Id, info.Value));
 
                     foreach (var benchmarkRunInfo in supportedBenchmarks) // we run them in the old order now using the new build artifacts
-                        results.Add(Run(benchmarkRunInfo, benchmarkToBuildResult, resolver, logger, artifactsToCleanup, rootArtifactsFolderPath, ref globalChronometer));
+                    {
+                        var runChronometer = Chronometer.Start();
+                        
+                        var summary = Run(benchmarkRunInfo, benchmarkToBuildResult, resolver, logger, artifactsToCleanup, rootArtifactsFolderPath, ref runChronometer);
+                        
+                        if (summaryPerType)
+                            PrintSummary(logger, commonSettingsConfig, summary);
+                        
+                        LogTotalTime(logger, runChronometer.GetElapsed().GetTimeSpan(), summary.GetNumberOfExecutedBenchmarks(), message: "Run time");
+                        logger.WriteLine();
+                        
+                        results.Add(summary);
+                    }
+
+                    if (!summaryPerType)
+                    {
+                        var joinedSummary = Summary.Join(results, commonSettingsConfig, globalChronometer.GetElapsed());
+                        
+                        PrintSummary(logger, commonSettingsConfig, joinedSummary);
+                        
+                        results.Clear();
+                        results.Add(joinedSummary);
+                    }
 
                     return results.ToArray();
                 }
@@ -134,7 +156,7 @@ namespace BenchmarkDotNet.Running
                                    ILogger logger, 
                                    List<string> artifactsToCleanup, 
                                    string rootArtifactsFolderPath,
-                                   ref StartedClock globalChronometer)
+                                   ref StartedClock runChronometer)
         {
             var benchmarks = benchmarkRunInfo.Benchmarks;
             var config = benchmarkRunInfo.Config;
@@ -175,16 +197,20 @@ namespace BenchmarkDotNet.Running
 
                 logger.WriteLine();
             }
-            var clockSpan = globalChronometer.GetElapsed();
+            
+            var clockSpan = runChronometer.GetElapsed();
 
-            var summary = new Summary(title,
-                                      reports,
-                                      HostEnvironmentInfo.GetCurrent(),
-                                      config,
-                                      GetResultsFolderPath(rootArtifactsFolderPath),
-                                      clockSpan.GetTimeSpan(), 
-                                      Validate(new[] {benchmarkRunInfo }, NullLogger.Instance)); // validate them once again, but don't print the output
+            return new Summary(title,
+                reports,
+                HostEnvironmentInfo.GetCurrent(),
+                config,
+                GetResultsFolderPath(rootArtifactsFolderPath),
+                clockSpan.GetTimeSpan(), 
+                Validate(new[] {benchmarkRunInfo }, NullLogger.Instance)); // validate them once again, but don't print the output
+        }
 
+        private static void PrintSummary(ILogger logger, IConfig config, Summary summary)
+        {
             logger.WriteLineHeader("// ***** BenchmarkRunner: Finish  *****");
             logger.WriteLine();
 
@@ -194,14 +220,12 @@ namespace BenchmarkDotNet.Running
             {
                 logger.WriteLineInfo($"  {file.Replace(currentDirectory, string.Empty).Trim('/', '\\')}");
             }
+
             logger.WriteLine();
 
             logger.WriteLineHeader("// * Detailed results *");
 
             BenchmarkReportExporter.Default.ExportToLog(summary, logger);
-
-            LogTotalTime(logger, clockSpan.GetTimeSpan());
-            logger.WriteLine();
 
             logger.WriteLineHeader("// * Summary *");
             MarkdownExporter.Console.ExportToLog(summary, logger);
@@ -238,7 +262,6 @@ namespace BenchmarkDotNet.Running
 
             logger.WriteLine();
             logger.WriteLineHeader("// ***** BenchmarkRunner: End *****");
-            return summary;
         }
 
         private static ValidationError[] Validate(BenchmarkRunInfo[] benchmarks, ILogger logger)
@@ -445,8 +468,8 @@ namespace BenchmarkDotNet.Running
             return (executeResults, gcStats);
         }
 
-        internal static void LogTotalTime(ILogger logger, TimeSpan time, string message = "Total time")
-            => logger.WriteLineStatistic($"{message}: {time.ToFormattedTotalTime()}");
+        internal static void LogTotalTime(ILogger logger, TimeSpan time, int executedBenchmarksCount, string message = "Total time")
+            => logger.WriteLineStatistic($"{message}: {time.ToFormattedTotalTime()}, executed benchmarks: {executedBenchmarksCount}");
 
         private static BenchmarkRunInfo[] GetSupportedBenchmarks(BenchmarkRunInfo[] benchmarkRunInfos, CompositeLogger logger, IResolver resolver)
             => benchmarkRunInfos.Select(info => new BenchmarkRunInfo(

docs/articles/samples/toc.yml
@@ -28,3 +28,9 @@
   href: IntroCustomMono.md
 - name: IntroCustomMonoArguments
   href: IntroCustomMonoArguments.md
+- name: IntroCategories
+  href: IntroCategories.md
+- name: IntroFilters
+  href: IntroFilters.md
+- name: IntroJoin
+  href: IntroJoin.md  

samples/BenchmarkDotNet.Samples/IntroJoin.cs
@@ -3,7 +3,7 @@ using BenchmarkDotNet.Attributes;
 
 namespace BenchmarkDotNet.Samples
 {
-    // Run BenchmarkSwither with arguments: "* --join --category=IntroJoinA"
+    // Run BenchmarkSwither with arguments: "--join --category=IntroJoinA"
     
     [DryJob]
     public class IntroJoin1

src/BenchmarkDotNet/Attributes/Filters/CategoryFilterAttribute.cs
@@ -1,12 +1,0 @@
-using BenchmarkDotNet.Filters;
-
-namespace BenchmarkDotNet.Attributes
-{
-    public class CategoryFilterAttribute : FilterConfigBaseAttribute
-    {
-        // CLS-Compliant Code requires a constuctor without an array in the argument list
-        public CategoryFilterAttribute() { }
-        
-        public CategoryFilterAttribute(string targetCategory) : base(new CategoryFilter(targetCategory)) { }
-    }
-}

src/BenchmarkDotNet/Configs/ConfigParser.cs
@@ -137,6 +137,7 @@ namespace BenchmarkDotNet.Configs
         public IConfig Parse(string[] args)
         {
             var config = new ManualConfig();
+            var filters = new List<IFilter>();
 
             for (int i = 0; i < args.Length; i++)
             {
@@ -168,10 +169,26 @@ namespace BenchmarkDotNet.Configs
                 {
                     case "categorys": // for now all the argument names at the place end with "s"
                     case "allcategories":
-                        config.Add(new AllCategoriesFilter(values));
+                        filters.Add(new AllCategoriesFilter(values));
                         break;
                     case "anycategories":
-                        config.Add(new AnyCategoriesFilter(values));
+                        filters.Add(new AnyCategoriesFilter(values));
+                        break;
+                    case "method":
+                    case "methods":
+                        filters.Add(new MethodNamesFilter(values));
+                        break;
+                    case "class":
+                    case "classes":
+                        filters.Add(new TypeNamesFilter(values));
+                        break;
+                    case "namespace":
+                    case "namespaces":
+                        filters.Add(new NamespacesFilter(values));
+                        break;
+                    case "attribute":
+                    case "attributes":
+                        filters.Add(new AttributesFilter(values));
                         break;
                 }
 
@@ -189,6 +206,12 @@ namespace BenchmarkDotNet.Configs
                         processOption(config, value);
                 }
             }
+
+            if (filters.Count > 1) // &&
+                config.Add(new UnionFilter(filters.ToArray()));
+            else if (filters.Count == 1)
+                config.Add(filters[0]);
+
             return config;
         }
 

src/BenchmarkDotNet/Configs/ManualConfig.cs
@@ -151,9 +151,11 @@ namespace BenchmarkDotNet.Configs
             {
                 case ConfigUnionRule.AlwaysUseLocal:
                     manualConfig.Add(localConfig);
+                    manualConfig.Add(globalConfig.GetFilters().ToArray()); // the filters should be merged anyway
                     break;
                 case ConfigUnionRule.AlwaysUseGlobal:
                     manualConfig.Add(globalConfig);
+                    manualConfig.Add(localConfig.GetFilters().ToArray()); // the filters should be merged anyway
                     break;
                 case ConfigUnionRule.Union:
                     manualConfig.Add(globalConfig);

commit 3155305e890ee61fecf6a43c3878a7788f0f3814

make sure DisassemblyDiagnoser output is exported, fixes bug introduced in #785

src/BenchmarkDotNet/Diagnosers/DisassemblyDiagnoser.cs
@@ -18,12 +18,20 @@ namespace BenchmarkDotNet.Diagnosers
     {
         private readonly WindowsDisassembler windowsDisassembler;
         private readonly MonoDisassembler monoDisassembler;
-        private readonly Dictionary<Benchmark, DisassemblyResult> results = new Dictionary<Benchmark, DisassemblyResult>();
+        private readonly Dictionary<Benchmark, DisassemblyResult> results;
 
         internal DisassemblyDiagnoser(WindowsDisassembler windowsDisassembler, MonoDisassembler monoDisassembler)
         {
             this.windowsDisassembler = windowsDisassembler;
             this.monoDisassembler = monoDisassembler;
+
+            results = new Dictionary<Benchmark, DisassemblyResult>();
+            Exporters = new IExporter[]
+            {
+                new CombinedDisassemblyExporter(results),
+                new RawDisassemblyExporter(results),
+                new PrettyDisassemblyExporter(results)
+            };
         }
 
         public static IConfigurableDiagnoser<DisassemblyDiagnoserConfig> Create(DisassemblyDiagnoserConfig config)
@@ -35,13 +43,7 @@ namespace BenchmarkDotNet.Diagnosers
         public IReadOnlyDictionary<Benchmark, DisassemblyResult> Results => results;
         public IEnumerable<string> Ids => new[] { nameof(DisassemblyDiagnoser) };
 
-        public IEnumerable<IExporter> Exporters 
-            => new IExporter[]
-            {
-                new CombinedDisassemblyExporter(Results),
-                new RawDisassemblyExporter(Results),
-                new PrettyDisassemblyExporter(Results)
-            };
+        public IEnumerable<IExporter> Exporters { get; }
 
         public IEnumerable<IAnalyser> Analysers => new IAnalyser[] { new DisassemblyAnalyzer(Results) };
 

src/BenchmarkDotNet/Running/BenchmarkRunner.cs
@@ -108,7 +108,7 @@ namespace BenchmarkDotNet.Running
                         var summary = Run(benchmarkRunInfo, benchmarkToBuildResult, resolver, logger, artifactsToCleanup, rootArtifactsFolderPath, ref runChronometer);
                         
                         if (summaryPerType)
-                            PrintSummary(logger, commonSettingsConfig, summary);
+                            PrintSummary(logger, benchmarkRunInfo.Config, summary);
                         
                         LogTotalTime(logger, runChronometer.GetElapsed().GetTimeSpan(), summary.GetNumberOfExecutedBenchmarks(), message: "Run time");
                         logger.WriteLine();