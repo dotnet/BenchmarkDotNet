000b329ef8b0dbb2389a8326df6a6c08d8a27898
Adam Sitnik <adam.sitnik@gmail.com>
2018-06-18 13:00:19 +0200

    make sure that every mutator can be applied only once per type

--- src/BenchmarkDotNet/Attributes/Mutators/JobMutatorConfigBaseAttribute.cs
+++ src/BenchmarkDotNet/Attributes/Mutators/JobMutatorConfigBaseAttribute.cs
@@ -1,14 +1,17 @@
-﻿using BenchmarkDotNet.Jobs;
+﻿using System;
+using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Jobs;
 
 namespace BenchmarkDotNet.Attributes
 {
-    public class JobMutatorConfigBaseAttribute : JobConfigBaseAttribute
+    [AttributeUsage(AttributeTargets.Class, AllowMultiple = false)] // users must not be able to define given mutator attribute more than once per type
+    public class JobMutatorConfigBaseAttribute : Attribute, IConfigSource
     {
         // CLS-Compliant Code requires a constuctor which use only CLS-compliant types
-        public JobMutatorConfigBaseAttribute() : base() { }
+        public JobMutatorConfigBaseAttribute() => Config = ManualConfig.CreateEmpty();
         
-        protected JobMutatorConfigBaseAttribute(Job job) : base(job.AsMutator())
-        {
-        }
+        protected JobMutatorConfigBaseAttribute(Job job) => Config = ManualConfig.CreateEmpty().With(job.AsMutator());
+        
+        public IConfig Config { get; }
     }
 }
---
+++ tests/BenchmarkDotNet.Tests/Attributes/MutatorAttributesTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using BenchmarkDotNet.Attributes;
+using Xunit;
+
+namespace BenchmarkDotNet.Tests.Attributes
+{
+    public class MutatorAttributesTests
+    {
+        [Fact]
+        public void AllMutatorsAttributesCanBeAppliedOnlyOncePerType()
+        {
+            var attributeTypes = typeof(JobMutatorConfigBaseAttribute)
+                .Assembly
+                .GetExportedTypes()
+                .Where(type => type.IsSubclassOf(typeof(JobMutatorConfigBaseAttribute)));
+
+            foreach (var attributeType in attributeTypes)
+            {
+                Assert.Single(
+                    attributeType.GetCustomAttributes().OfType<AttributeUsageAttribute>(),
+                    usageAttribute => usageAttribute.AllowMultiple == false);
+            }
+        }
+    }
+}
