e45f698429952316038500134894d2806737b81c
Andrey Akinshin <andrey.akinshin@gmail.com>
2018-07-23 14:22:24 +0300

    docs: update main.js

--- docs/template/styles/main.js
+++ docs/template/styles/main.js
@@ -5,10 +5,9 @@ $(function () {
             $('#navbar ul').append('<li id="navbar-versions" class="dropdown">\n' +
                 '  <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Version: master <span class="caret"></span></a>\n' +
                 '  <ul class="dropdown-menu">\n' +
-                '    <li><a href="/">master</a></li>\n' +
+                '    <li><a href="/master/">master</a></li>\n' +
+                '    <li><a href="/v0.11.0/">v0.11.0</a></li>\n' +
                 '    <li><a href="/v0.10.14/">v0.10.14</a></li>\n' +
-                '    <li><a href="/v0.10.13/">v0.10.13</a></li>\n' +
-                '    <li><a href="/v0.10.12/">v0.10.12</a></li>\n' +
                 '  </ul>\n' +
                 '</li>');
         }
