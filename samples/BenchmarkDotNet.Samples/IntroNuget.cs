8936840d391bc33191c44c91665bbd66b27fd59d
Shannon <sdeminick@gmail.com>
2018-10-25 11:26:58 +1100

    Updates IntroNuget, adds comments, adds ref to newtonsoft

--- samples/BenchmarkDotNet.Samples/IntroNuget.cs
+++ samples/BenchmarkDotNet.Samples/IntroNuget.cs
@@ -11,9 +11,16 @@ using BenchmarkDotNet.Exporters.Csv;
 using BenchmarkDotNet.Jobs;
 using BenchmarkDotNet.Loggers;
 using BenchmarkDotNet.Toolchains.CsProj;
+using Newtonsoft.Json;
 
 namespace BenchmarkDotNet.Samples
 {
+    /// <summary>
+    /// Benchmarks between various versions of a Nuget package
+    /// </summary>
+    /// <remarks>
+    /// Only supported with the DotNetCliBuilder toolchain
+    /// </remarks>
     [Config(typeof(Config))]
     public class IntroNuget
     {
@@ -21,32 +28,23 @@ namespace BenchmarkDotNet.Samples
         {
             public Config()
             {
-                //TODO: So far only implemented with any toolchain using DotNetCliBuilder
+                //Specify jobs with different versions of the same Nuget package to benchmark.
+                //The Nuget versions referenced on these jobs must be greater or equal to the 
+                //same Nuget version referenced in this benchmark project.
+                //Example: This benchmark project references Newtonsoft.Json 9.0.1
                 Add(Job.MediumRun.With(CsProjCoreToolchain.Current.Value).WithNuget("Newtonsoft.Json", "11.0.2").WithId("11.0.2"));
+                Add(Job.MediumRun.With(CsProjCoreToolchain.Current.Value).WithNuget("Newtonsoft.Json", "11.0.1").WithId("11.0.1"));
                 Add(Job.MediumRun.With(CsProjCoreToolchain.Current.Value).WithNuget("Newtonsoft.Json", "10.0.3").WithId("10.0.3"));
+                Add(Job.MediumRun.With(CsProjCoreToolchain.Current.Value).WithNuget("Newtonsoft.Json", "10.0.2").WithId("10.0.2"));
+                Add(Job.MediumRun.With(CsProjCoreToolchain.Current.Value).WithNuget("Newtonsoft.Json", "10.0.1").WithId("10.0.1"));
                 Add(Job.MediumRun.With(CsProjCoreToolchain.Current.Value).WithNuget("Newtonsoft.Json", "9.0.1").WithId("9.0.1"));
-                Add(Job.MediumRun.With(CsProjCoreToolchain.Current.Value).WithNuget("Newtonsoft.Json", "8.0.1").WithId("8.0.1"));
-                Add(Job.MediumRun.With(CsProjCoreToolchain.Current.Value).WithNuget("Newtonsoft.Json", "7.0.1").WithId("7.0.1"));
-                Add(Job.MediumRun.With(CsProjCoreToolchain.Current.Value).WithNuget("Newtonsoft.Json", "6.0.1").WithId("6.0.1"));
                 Add(DefaultConfig.Instance.GetColumnProviders().ToArray());
                 Add(DefaultConfig.Instance.GetLoggers().ToArray());
                 Add(CsvExporter.Default);
             }
         }
 
-        [GlobalSetup]
-        public void Setup()
-        {
-            var jsonConvertMethod = Assembly.Load("Newtonsoft.Json").GetType("Newtonsoft.Json.JsonConvert")
-                .GetMethods().Where(x => x.Name == "SerializeObject" && x.GetParameters().Length == 1)
-                .First();
-
-            Serializer = s => (string)jsonConvertMethod.Invoke(null, new object[] { s });
-        }
-
-        public Func<object, string> Serializer { get; private set; }
-
         [Benchmark]
-        public void SerializeAnonymousObject() => Serializer(new { hello = "world", price = 1.99, now = DateTime.UtcNow });
+        public void SerializeAnonymousObject() => JsonConvert.SerializeObject(new { hello = "world", price = 1.99, now = DateTime.UtcNow });
     }
 }
