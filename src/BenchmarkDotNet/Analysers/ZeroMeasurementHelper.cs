8b2015ba3872b6db4a019de0c4544223ebfe4e7e
Andrey Akinshin <andrey.akinshin@gmail.com>
2018-11-01 20:06:07 +0300

    Fix ZeroMeasurementHelper for dry mode case, fixes #943

--- src/BenchmarkDotNet/Analysers/ZeroMeasurementHelper.cs
+++ src/BenchmarkDotNet/Analysers/ZeroMeasurementHelper.cs
@@ -8,14 +8,22 @@ namespace BenchmarkDotNet.Analysers
         /// Checks distribution against Zero Measurement hypothesis in case of known threshold
         /// </summary>
         /// <returns>True if measurement is ZeroMeasurement</returns>
-        public static bool CheckZeroMeasurementOneSample(double[] results, double threshold) 
-            => !StudentTest.Instance.IsGreater(results, threshold).NullHypothesisIsRejected;
+        public static bool CheckZeroMeasurementOneSample(double[] results, double threshold)
+        {
+            if (results.Length < 3)
+                return false;
+            return !StudentTest.Instance.IsGreater(results, threshold).NullHypothesisIsRejected;
+        }
 
         /// <summary>
         /// Checks distribution against Zero Measurement hypothesis in case of two samples
         /// </summary>
         /// <returns>True if measurement is ZeroMeasurement</returns>
-        public static bool CheckZeroMeasurementTwoSamples(double[] workload, double[] overhead) 
-            => !WelchTest.Instance.IsGreater(workload, overhead).NullHypothesisIsRejected;
+        public static bool CheckZeroMeasurementTwoSamples(double[] workload, double[] overhead)
+        {
+            if (workload.Length < 3 || overhead.Length < 3)
+                return false;
+            return !WelchTest.Instance.IsGreater(workload, overhead).NullHypothesisIsRejected;
+        }
     }
 }
