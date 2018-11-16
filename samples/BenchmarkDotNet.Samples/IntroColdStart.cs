e0d6d23464f1267ba286152d9fd5ef79309b0431
coding4fun <korshunovartem@mail.ru>
2018-11-16 22:30:56 +0300

    Incorrect field name 'firstCall'?

--- samples/BenchmarkDotNet.Samples/IntroColdStart.cs
+++ samples/BenchmarkDotNet.Samples/IntroColdStart.cs
@@ -9,14 +9,14 @@ namespace BenchmarkDotNet.Samples
     [MinColumn, MaxColumn, MeanColumn, MedianColumn]
     public class IntroColdStart
     {
-        private bool firstCall;
+        private bool firstCall = true;
 
         [Benchmark]
         public void Foo()
         {
-            if (firstCall == false)
+            if (firstCall == true)
             {
-                firstCall = true;
+                firstCall = false;
                 Console.WriteLine("// First call");
                 Thread.Sleep(1000);
             }
