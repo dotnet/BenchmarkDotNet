13832524c29053d7eaccb862cbe507bbeb8a2fff
Adam Sitnik <adam.sitnik@gmail.com>
2018-12-03 09:58:09 +0100

    File names should be consistent across all OSes, fixes #981

---
+++ tests/BenchmarkDotNet.Tests/FolderNameTests.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using BenchmarkDotNet.Helpers;
+using Xunit;
+
+namespace BenchmarkDotNet.Tests
+{
+    public class FolderNameTests
+    {
+        [Theory]
+        [InlineData(typeof(FolderNameTests), "BenchmarkDotNet.Tests.FolderNameTests")]
+        [InlineData(typeof(List<int>), "System.Collections.Generic.List_Int32_")]
+        public void FileNamesAreConsistentAcrossOSes(object value, string expectedName)
+        {
+            Assert.Equal(expectedName, FolderNameHelper.ToFolderName(value));
+        }
+    }
+}
