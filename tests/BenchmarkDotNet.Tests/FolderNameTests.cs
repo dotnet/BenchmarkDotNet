c0aac17c323850b2519da7df7f0175ca5a0ce13a
Andrey Akinshin <andrey.akinshin@gmail.com>
2018-12-03 20:54:54 +0300

    More tests for FolderNameHelper

--- tests/BenchmarkDotNet.Tests/FolderNameTests.cs
+++ tests/BenchmarkDotNet.Tests/FolderNameTests.cs
@@ -1,11 +1,35 @@
 ﻿using System.Collections.Generic;
+using BenchmarkDotNet.Environments;
 using BenchmarkDotNet.Helpers;
+using BenchmarkDotNet.Horology;
 using Xunit;
 
 namespace BenchmarkDotNet.Tests
 {
     public class FolderNameTests
     {
+        [Theory]
+        [InlineData(false, "false")]
+        [InlineData(true, "true")]
+        [InlineData("<MyString>", "_MyString_")]
+        [InlineData('a', "97")]
+        [InlineData(0.42f, "0-42")]
+        [InlineData(0.42d, "0-42")]
+        [InlineData(Jit.RyuJit, "RyuJit")]
+        [InlineData(typeof(int), "System.Int32")]
+        public void ToFolderNameTest(object value, string expectedName)
+        {
+            Assert.Equal(expectedName, FolderNameHelper.ToFolderName(value));
+        }
+
+        // Value types can't be used as attribute arguments
+        [Fact]
+        public void ToFolderNameStructTest()
+        {
+            Assert.Equal("0-42", FolderNameHelper.ToFolderName(0.42m));
+            Assert.Equal("1234000000ns", FolderNameHelper.ToFolderName(TimeInterval.FromSeconds(1.234)));
+        }
+
         [Theory]
         [InlineData(typeof(FolderNameTests), "BenchmarkDotNet.Tests.FolderNameTests")]
         [InlineData(typeof(List<int>), "System.Collections.Generic.List_Int32_")]
