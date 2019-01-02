27dd870e196b1a9586cceb62bdd958f35a4d4dc5
Adam Sitnik <adam.sitnik@gmail.com>
2019-01-03 00:38:36 +0100

    CoreRunToolchain: when the file exists, overwrite it

--- src/BenchmarkDotNet/Toolchains/CoreRun/CoreRunGenerator.cs
+++ src/BenchmarkDotNet/Toolchains/CoreRun/CoreRunGenerator.cs
@@ -46,8 +46,9 @@ namespace BenchmarkDotNet.Toolchains.CoreRun
             
             foreach (DirectoryInfo dir in source.GetDirectories())
                 CopyFilesRecursively(dir, target.CreateSubdirectory(dir.Name));
+
             foreach (FileInfo file in source.GetFiles())
-                file.CopyTo(Path.Combine(target.FullName, file.Name));
+                file.CopyTo(Path.Combine(target.FullName, file.Name), overwrite: true);
         }
     }
 }
