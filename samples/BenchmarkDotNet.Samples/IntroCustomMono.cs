9bcaddcdcd0e8b97557802d370adcce51a860931
Alexander Kyte <alkyte@microsoft.com>
2019-01-15 17:39:42 -0500

    Add documentation for using mono aot args

--- samples/BenchmarkDotNet.Samples/IntroCustomMono.cs
+++ samples/BenchmarkDotNet.Samples/IntroCustomMono.cs
@@ -3,6 +3,7 @@ using BenchmarkDotNet.Configs;
 using BenchmarkDotNet.Environments;
 using BenchmarkDotNet.Jobs;
 using BenchmarkDotNet.Running;
+using System;
 
 namespace BenchmarkDotNet.Samples
 {
@@ -32,6 +33,36 @@ namespace BenchmarkDotNet.Samples
                     "Mono x64", @"C:\Program Files\Mono\bin\mono.exe")));
                 Add(Job.ShortRun.With(new MonoRuntime(
                     "Mono x86", @"C:\Program Files (x86)\Mono\bin\mono.exe")));
+            }
+        }
+
+        [Benchmark]
+        public void Foo()
+        {
+            // Benchmark body
+        }
+    }
+
+    // ** Object Style, Using AOT **
+
+    [Config(typeof(Config))]
+    public class IntroCustomMonoObjectStyleAot
+    {
+        private class Config : ManualConfig
+        {
+            public void AddMono (string name, string mono_top_dir)
+            {
+                var aot_compile_args  = "--aot=llvm";
+                var mono_bcl = String.Format(@"{0}\lib\mono\4.5", mono_top_dir);
+                var mono_bin = String.Format(@"{0}\bin\mono", mono_top_dir);
+                Add(Job.ShortRun.With(new MonoRuntime(
+                    name, mono_bin, aot_compile_args, mono_bcl)));
+            }
+
+            public Config()
+            {
+                AddMono("Mono x64", @"C:\Program Files\Mono");
+                AddMono("Mono x86", @"C:\Program Files (x86)");
             }
         }
 
