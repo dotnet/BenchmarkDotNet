e23ed18db4107d883ba22b6ce22473b51eb37327
wojtpl2 <wojtpl2@gmail.com>
2019-01-15 13:30:42 +0100

    Update sample

--- samples/BenchmarkDotNet.Samples/IntroColdCpuCache.cs
+++ samples/BenchmarkDotNet.Samples/IntroColdCpuCache.cs
@@ -3,43 +3,79 @@ using System.Linq;
 using BenchmarkDotNet.Attributes;
 using BenchmarkDotNet.Columns;
 using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Diagnosers;
 using BenchmarkDotNet.Engines;
 using BenchmarkDotNet.Jobs;
+using BenchmarkDotNet.Mathematics.StatisticalTesting;
 
 namespace BenchmarkDotNet.Samples
 {
-    [Config(typeof(CompereCacheClearingStrategies))]
+    [StatisticalTestColumn(StatisticalTestKind.MannWhitney, ThresholdUnit.Microseconds, 0.2, true)]
+    [Config(typeof(CompareCacheClearingStrategies))]
     public class IntroColdCpuCache
     {
-        private class CompereCacheClearingStrategies : ManualConfig
+        private class CompareCacheClearingStrategies : ManualConfig
         {
-            public CompereCacheClearingStrategies()
+            public CompareCacheClearingStrategies()
             {
+                Add(Job.Default.WithCacheClearingStrategy(CacheClearingStrategy.None).AsBaseline());
                 Add(Job.Default.WithCacheClearingStrategy(CacheClearingStrategy.Allocations));
-                Add(Job.Default.WithCacheClearingStrategy(CacheClearingStrategy.None));
-                Add(Job.Default.WithAffinity((IntPtr) 1).WithCacheClearingStrategy(CacheClearingStrategy.Allocations));
-                Add(Job.Default.WithAffinity((IntPtr) 1).WithCacheClearingStrategy(CacheClearingStrategy.None));
 
-                Add(StatisticColumn.Q1,
-                    StatisticColumn.Median,
-                    StatisticColumn.Q3,
-                    StatisticColumn.Skewness,
-                    StatisticColumn.Kurtosis);
+                Add(Job.Default.WithAffinity((IntPtr) 1).WithCacheClearingStrategy(CacheClearingStrategy.None));
+                Add(Job.Default.WithAffinity((IntPtr) 1).WithCacheClearingStrategy(CacheClearingStrategy.Allocations));
+                
+                Add(HardwareCounter.CacheMisses);
             }
         }
 
-        private readonly int[] array = Enumerable.Range(1, 14 * 1024 * 1024).ToArray();
+        private const int arrayCount = 14 * 1024 * 1024; 
+        private readonly int[] array = Enumerable.Repeat(1, arrayCount).ToArray(); 
 
         [Benchmark]
         public int ArraySum()
         {
             int result = 0;
-            for (int i = 0; i < array.Length; i += 16) // 16 because sizeof(int) = 4, 4 * 16 = 64 bytes.
+            for (int i = 0; i < arrayCount; i += 16) // 16 because sizeof(int) = 4, 4 * 16 = 64 bytes.
             {
                 result += array[i];
             }
 
             return result;
+        }
+
+        [Benchmark]
+        public void Accessing()
+        {
+            for (int i = 0; i < arrayCount; i += 16) // 16 because sizeof(int) = 4, 4 * 16 = 64 bytes.
+            {
+                array[i] = 1;
+            }
+        }
+
+        private readonly int[,] tweDimensionalArray = new int[5000, 5000];
+
+        [Benchmark]
+        public void ArrayOfArrayAccessingByRows()
+        {
+            for (int i = 0; i < 5000; i++)
+            {
+                for (int j = 0; j < 5000; j++)
+                {
+                    tweDimensionalArray[i, j] = 1;
+                }
+            }
+        }
+
+        [Benchmark]
+        public void ArrayOfArrayAccessingByColumns()
+        {
+            for (int i = 0; i < 5000; i++)
+            {
+                for (int j = 0; j < 5000; j++)
+                {
+                    tweDimensionalArray[j, i] = 1;
+                }
+            }
         }
     }
 }
