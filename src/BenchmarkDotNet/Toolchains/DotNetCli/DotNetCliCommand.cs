6ee21bb6356f7199425edb788a6acd85aec95b7b
Adam Sitnik <adam.sitnik@microsoft.com>
2019-01-15 17:03:34 -0800

    if we fail to do the full build, we try with --no-dependencies

--- src/BenchmarkDotNet/Toolchains/DotNetCli/DotNetCliCommand.cs
+++ src/BenchmarkDotNet/Toolchains/DotNetCli/DotNetCliCommand.cs
@@ -57,9 +57,12 @@ namespace BenchmarkDotNet.Toolchains.DotNetCli
             if (!restoreResult.IsSuccess)
                 return BuildResult.Failure(GenerateResult, restoreResult.AllInformation);
 
-            var buildResult = BuildNoRestore().ToBuildResult(GenerateResult);
+            var buildResult = BuildNoRestore();
 
-            return buildResult;
+            if (!buildResult.IsSuccess) // if we fail to do the full build, we try with --no-dependencies
+                buildResult = BuildNoRestoreNoDependencies();
+
+            return buildResult.ToBuildResult(GenerateResult);
         }
 
         [PublicAPI]
@@ -106,7 +109,10 @@ namespace BenchmarkDotNet.Toolchains.DotNetCli
         public DotNetCliCommandResult BuildNoRestore()
             => DotNetCliCommandExecutor.Execute(WithArguments(
                 GetBuildCommand(BuildPartition, $"{Arguments} --no-restore")));
-                
+
+        public DotNetCliCommandResult BuildNoRestoreNoDependencies()
+            => DotNetCliCommandExecutor.Execute(WithArguments(
+                GetBuildCommand(BuildPartition, $"{Arguments} --no-restore --no-dependencies")));
 
         public DotNetCliCommandResult PublishNoBuildAndNoRestore()
             => DotNetCliCommandExecutor.Execute(WithArguments(
