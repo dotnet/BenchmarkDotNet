ca368527f92c48a1d946e2a53be70eea36747f8e
CodeTherapist <codetherapist0@gmail.com>
2019-02-03 18:10:21 +0100

    Add "dotnet new" benchmark project template

---
+++ templates/BenchmarkDotNet.BenchmarkPorjectTemplate.CSharp/content/_BenchmarkName_.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using BenchmarkDotNet;
+using BenchmarkDotNet.Attributes;
+
+namespace _BenchmarkProjectName_
+{
+#if(config)
+    [Config(typeof(BenchmarkConfig))]
+#endif
+    public class $(BenchmarkName)
+    {
+
+        [Benchmark(Description = "Your description here")]
+        public void YourBenchmark()
+        {
+            // Implement your benchmark here
+
+            
+        }
+    }
+}
