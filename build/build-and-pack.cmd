1a8826ba7ce30910cb7c6f527054957dc96b8284
Andrey Akinshin <andrey.akinshin@gmail.com>
2019-02-15 12:36:17 +0300

    Update build-and-pack.cmd

--- build/build-and-pack.cmd
+++ build/build-and-pack.cmd
@@ -4,8 +4,10 @@ git clean -xfd
 dotnet restore
 dotnet build .\src\BenchmarkDotNet\BenchmarkDotNet.csproj -c Release
 dotnet build .\src\BenchmarkDotNet.Diagnostics.Windows\BenchmarkDotNet.Diagnostics.Windows.csproj  -c Release
-dotnet pack .\src\BenchmarkDotNet\BenchmarkDotNet.csproj -c Release
-dotnet pack .\src\BenchmarkDotNet.Diagnostics.Windows\BenchmarkDotNet.Diagnostics.Windows.csproj  -c Release
+dotnet build .\src\BenchmarkDotNet.Tool\BenchmarkDotNet.Tool.csproj  -c Release
+dotnet pack .\src\BenchmarkDotNet\BenchmarkDotNet.csproj -c Release --include-symbols -p:SymbolPackageFormat=snupkg
+dotnet pack .\src\BenchmarkDotNet.Diagnostics.Windows\BenchmarkDotNet.Diagnostics.Windows.csproj  -c Release --include-symbols -p:SymbolPackageFormat=snupkg
+dotnet pack .\src\BenchmarkDotNet.Tool\BenchmarkDotNet.Tool.csproj -c Release --include-symbols -p:SymbolPackageFormat=snupkg
 rmdir artifacts /s /q
 mkdir artifacts
 for /R %%x in (BenchmarkDotNet*.*nupkg) do copy "%%x" "artifacts/" /Y
