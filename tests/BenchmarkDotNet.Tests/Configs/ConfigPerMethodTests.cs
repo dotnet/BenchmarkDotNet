75d7a9c6e27ea89a01ecfccfd84b31fa3e114dde
Adam Sitnik <adam.sitnik@gmail.com>
2019-03-10 13:40:21 +0100

    allow the FilterConfigBaseAttribute to be applied per method

---
+++ tests/BenchmarkDotNet.Tests/Configs/ConfigPerMethodTests.cs
@@ -0,0 +1,41 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Filters;
+using BenchmarkDotNet.Running;
+using Xunit;
+
+namespace BenchmarkDotNet.Tests.Configs
+{
+    public class ConfigPerMethodTests
+    {
+        [Fact]
+        public void PetMethodConfigsAreRespected()
+        {
+            var never = BenchmarkConverter.TypeToBenchmarks(typeof(WithBenchmarkThatShouldNeverRun));
+
+            Assert.Empty(never.BenchmarksCases);
+
+            var always = BenchmarkConverter.TypeToBenchmarks(typeof(WithBenchmarkThatShouldAlwaysRun));
+
+            Assert.NotEmpty(always.BenchmarksCases);
+        }
+
+        public class WithBenchmarkThatShouldNeverRun
+        {
+            [Benchmark]
+            [ConditionalRun(false)]
+            public void Method() { }
+        }
+
+        public class WithBenchmarkThatShouldAlwaysRun
+        {
+            [Benchmark]
+            [ConditionalRun(true)]
+            public void Method() { }
+        }
+
+        public class ConditionalRun : FilterConfigBaseAttribute
+        {
+            public ConditionalRun(bool value) : base(new SimpleFilter(_ => value)) { }
+        }
+    }
+}
