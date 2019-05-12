c19741c6e28419d759c8d6495f32d187f30897b3
Adam Sitnik <adam.sitnik@microsoft.com>
2019-05-12 16:23:38 -0700

    define new attribute types

---
+++ src/BenchmarkDotNet.Annotations/Attributes/InvocationCleanupAttribute.cs
@@ -0,0 +1,14 @@
+﻿using JetBrains.Annotations;
+using System;
+
+namespace BenchmarkDotNet.Attributes
+{
+    /// <summary>
+    /// Marks method to be executed after every benchmark invocation.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Method)]
+    [MeansImplicitUse]
+    public class InvocationCleanupAttribute : TargetedAttribute
+    {
+    }
+}
---
+++ src/BenchmarkDotNet.Annotations/Attributes/InvocationSetupAttribute.cs
@@ -0,0 +1,14 @@
+﻿using JetBrains.Annotations;
+using System;
+
+namespace BenchmarkDotNet.Attributes
+{
+    /// <summary>
+    /// Marks method to be executed before every benchmark invocation.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Method)]
+    [MeansImplicitUse]
+    public class InvocationSetupAttribute : TargetedAttribute
+    {
+    }
+}
