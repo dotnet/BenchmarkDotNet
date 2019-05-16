d6f0415f872135577eb1f461c4f5f115b1cf5a71
Adam Sitnik <adam.sitnik@microsoft.com>
2019-05-16 16:19:39 -0700

    add tests

---
+++ tests/BenchmarkDotNet.Tests/XUnit/TheoryNetCore30Attribute.cs
@@ -0,0 +1,15 @@
+﻿using BenchmarkDotNet.Toolchains.DotNetCli;
+using Xunit;
+
+namespace BenchmarkDotNet.Tests.XUnit
+{
+    public class TheoryNetCore30Attribute : TheoryAttribute
+    {
+        // ReSharper disable once VirtualMemberCallInConstructor
+        public TheoryNetCore30Attribute(string skipReason)
+        {
+            if (NetCoreAppSettings.GetCurrentVersion() != NetCoreAppSettings.NetCoreApp30)
+                Skip = skipReason;
+        }
+    }
+}
