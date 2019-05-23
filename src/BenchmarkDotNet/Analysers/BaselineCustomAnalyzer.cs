eeb6dc2e1799a03d173271656ab4e06a0fabb4d2
Muhammad Azeez <Muhammad-Azeez@outlook.com>
2019-05-23 11:22:11 +0300

    refactor the code so that ResultsAreInvalid is reused

--- src/BenchmarkDotNet/Analysers/BaselineCustomAnalyzer.cs
+++ src/BenchmarkDotNet/Analysers/BaselineCustomAnalyzer.cs
@@ -19,26 +19,20 @@ namespace BenchmarkDotNet.Analysers
                                               .Distinct()
                                               .ToArray();
 
-            var columNames = string.Join(",", columns);
-            foreach (var benchmarkCase in summary.BenchmarksCases.Where(c => ResultsAreInvalid(summary, c)))
+            var columNames = string.Join(", ", columns);
+
+            foreach (var benchmarkCase in summary.BenchmarksCases)
             {
-                string message = "A question mark '?' symbol indicates that it was not possible " +
+                string logicalGroupKey = summary.GetLogicalGroupKey(benchmarkCase);
+                var baseline = summary.GetBaseline(logicalGroupKey);
+                if (BaselineCustomColumn.ResultsAreInvalid(summary, benchmarkCase, baseline) == false)
+                    continue;
+
+                var message = "A question mark '?' symbol indicates that it was not possible " +
                                 $"to compute ({columNames}) because the baseline value is too close to zero.";
+
                 yield return Conclusion.CreateWarning(Id, message);
             }
-        }
-
-        private static bool ResultsAreInvalid(Summary summary, BenchmarkCase benchmarkCase)
-        {
-            var logicalGroupKey = summary.GetLogicalGroupKey(benchmarkCase);
-            var baseline = summary.GetBaseline(logicalGroupKey);
-
-            return baseline == null ||
-                   summary[baseline] == null ||
-                   summary[baseline].ResultStatistics == null ||
-                   !summary[baseline].ResultStatistics.CanBeInverted() ||
-                   summary[benchmarkCase] == null ||
-                   summary[benchmarkCase].ResultStatistics == null;
         }
     }
 }
