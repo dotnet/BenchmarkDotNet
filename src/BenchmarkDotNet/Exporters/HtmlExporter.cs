dfa074b024cfa8bbe4fe175d3d65a3d9f85127ff
Ilya Chudin <chudin.ilya@gmail.com>
2019-07-04 14:00:18 +0400

    Update HtmlExporter.cs (#1189)
    HTML report looks closer to MD report now.

--- src/BenchmarkDotNet/Exporters/HtmlExporter.cs
+++ src/BenchmarkDotNet/Exporters/HtmlExporter.cs
@@ -9,7 +9,7 @@ namespace BenchmarkDotNet.Exporters
         internal const string CssDefinition = @"
 <style type=""text/css"">
 	table { border-collapse: collapse; display: block; width: 100%; overflow: auto; }
-	td, th { padding: 6px 13px; border: 1px solid #ddd; }
+	td, th { padding: 6px 13px; border: 1px solid #ddd; text-align: right; }
 	tr { background-color: #fff; border-top: 1px solid #ccc; }
 	tr:nth-child(even) { background: #f8f8f8; }
 </style>";
