0229868aed045d94f237e891fb75befb05604257
Andrey Akinshin <andrey.akinshin@gmail.com>
2019-10-29 19:11:15 +0300

    Improve BenchmarkDotNet.Templates
    * Migrate from nuspec to csproj
    * Include template project in the solution
    * Introduce additional parameter for BenchmarkDotNet version
    * Add bat script for local template installation from sources

--- build/build-and-pack.cmd
+++ build/build-and-pack.cmd
@@ -6,11 +6,12 @@ dotnet build .\src\BenchmarkDotNet\BenchmarkDotNet.csproj -c Release
 dotnet build .\src\BenchmarkDotNet.Diagnostics.Windows\BenchmarkDotNet.Diagnostics.Windows.csproj  -c Release
 dotnet build .\src\BenchmarkDotNet.Tool\BenchmarkDotNet.Tool.csproj  -c Release
 dotnet build .\src\BenchmarkDotNet.Annotations\BenchmarkDotNet.Annotations.csproj  -c Release
+dotnet build .\templates\BenchmarkDotNet.Templates.csproj -c Release
 dotnet pack .\src\BenchmarkDotNet\BenchmarkDotNet.csproj -c Release --include-symbols -p:SymbolPackageFormat=snupkg
 dotnet pack .\src\BenchmarkDotNet.Diagnostics.Windows\BenchmarkDotNet.Diagnostics.Windows.csproj  -c Release --include-symbols -p:SymbolPackageFormat=snupkg
 dotnet pack .\src\BenchmarkDotNet.Tool\BenchmarkDotNet.Tool.csproj -c Release --include-symbols -p:SymbolPackageFormat=snupkg
 dotnet pack .\src\BenchmarkDotNet.Annotations\BenchmarkDotNet.Annotations.csproj -c Release --include-symbols -p:SymbolPackageFormat=snupkg
+dotnet pack .\templates\BenchmarkDotNet.Templates.csproj -c Release
 rmdir artifacts /s /q
 mkdir artifacts
 for /R %%x in (BenchmarkDotNet*.*nupkg) do copy "%%x" "artifacts/" /Y
-nuget pack .\templates\BenchmarkDotNet.Templates.nuspec -OutputDirectory artifacts
