1a211492b50153211ef448e6f9c88560ea2e3193
Wojciech Nagórski <wojtpl2@gmail.com>
2019-12-19 14:05:09 +0100

    Improvement of attributes

--- src/BenchmarkDotNet/Attributes/Jobs/InProcessAttribute.cs
+++ src/BenchmarkDotNet/Attributes/Jobs/InProcessAttribute.cs
@@ -1,7 +1,9 @@
+using System;
 using BenchmarkDotNet.Jobs;
 
 namespace BenchmarkDotNet.Attributes
 {
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Assembly, AllowMultiple = true)]
     public class InProcessAttribute : JobConfigBaseAttribute
     {
         public InProcessAttribute(bool dontLogOutput = false) : base(dontLogOutput ? Job.InProcessDontLogOutput : Job.InProcess)
--- src/BenchmarkDotNet/Attributes/Jobs/MediumRunJobAttribute.cs
+++ src/BenchmarkDotNet/Attributes/Jobs/MediumRunJobAttribute.cs
@@ -1,10 +1,33 @@
-﻿using BenchmarkDotNet.Jobs;
+﻿using System;
+using BenchmarkDotNet.Environments;
+using BenchmarkDotNet.Jobs;
 
 namespace BenchmarkDotNet.Attributes
 {
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Assembly, AllowMultiple = true)]
     public class MediumRunJobAttribute : JobConfigBaseAttribute
     {
         public MediumRunJobAttribute() : base(Job.MediumRun)
+        {
+        }
+
+        /// <summary>
+        /// defines a new MediumRun Job that targets specified Framework
+        /// </summary>
+        /// <param name="runtimeMoniker">Target Framework to test.</param>
+        public MediumRunJobAttribute(RuntimeMoniker runtimeMoniker)
+            : base(GetJob(Job.MediumRun, runtimeMoniker, null, null))
+        {
+        }
+
+        /// <summary>
+        /// defines a new MediumRun Job that targets specified Framework, JIT and Platform
+        /// </summary>
+        /// <param name="runtimeMoniker">Target Framework to test.</param>
+        /// <param name="jit">Jit to test.</param>
+        /// <param name="platform">Platform to test.</param>
+        public MediumRunJobAttribute(RuntimeMoniker runtimeMoniker, Jit jit, Platform platform)
+            : base(GetJob(Job.MediumRun, runtimeMoniker, jit, platform))
         {
         }
     }
