179cc5dfb07d422b7752bd482382d35e58146f1b
Adam Sitnik <adam.sitnik@gmail.com>
2020-03-03 11:53:06 +0100

    move SpeedScope logic to a separate type

--- src/BenchmarkDotNet/Diagnosers/EventPipeProfiler.cs
+++ src/BenchmarkDotNet/Diagnosers/EventPipeProfiler.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Diagnostics.Tracing;
 using System.IO;
 using System.Linq;
@@ -17,11 +16,6 @@ using BenchmarkDotNet.Reports;
 using BenchmarkDotNet.Running;
 using BenchmarkDotNet.Validators;
 using Microsoft.Diagnostics.NETCore.Client;
-using Microsoft.Diagnostics.Symbols;
-using Microsoft.Diagnostics.Tracing;
-using Microsoft.Diagnostics.Tracing.Etlx;
-using Microsoft.Diagnostics.Tracing.Stacks;
-using Microsoft.Diagnostics.Tracing.Stacks.Formats;
 
 namespace BenchmarkDotNet.Diagnosers
 {
@@ -150,7 +144,7 @@ namespace BenchmarkDotNet.Diagnosers
             Task.WaitAll(collectingTask);
 
             if (benchmarkToTraceFile.TryGetValue(results.BenchmarkCase, out var traceFilePath))
-                ConvertToSpeedscope(results.BenchmarkCase, traceFilePath);
+                benchmarkToTraceFile[results.BenchmarkCase] = SpeedScopeExporter.Convert(traceFilePath, logger);
 
             return Array.Empty<Metric>();
         }
@@ -176,61 +170,6 @@ namespace BenchmarkDotNet.Diagnosers
             resultLogger.WriteLineInfo(benchmarkToTraceFile.Values.First());
 
             resultLogger.WriteLineHeader(logSeparator);
-        }
-
-        private void ConvertToSpeedscope(BenchmarkCase benchmarkCase, string traceFilePath)
-        {
-            var speedscopeFileName = Path.ChangeExtension(traceFilePath, "speedscope.json");
-
-            try
-            {
-                ConvertToSpeedscope(traceFilePath, speedscopeFileName);
-                benchmarkToTraceFile[benchmarkCase] = speedscopeFileName;
-            }
-            // Below comment come from https://github.com/dotnet/diagnostics/blob/2c23d3265dd8f642a8d6cf4bb8a135a5ff8b00c2/src/Tools/dotnet-trace/TraceFileFormatConverter.cs#L42
-            // TODO: On a broken/truncated trace, the exception we get from TraceEvent is a plain System.Exception type because it gets caught and rethrown inside TraceEvent.
-            // We should probably modify TraceEvent to throw a better exception.
-            catch (Exception ex)
-            {
-                if (ex.ToString().Contains("Read past end of stream."))
-                {
-                    logger.WriteLine(LogKind.Info,
-                        "Detected a potentially broken trace. Continuing with best-efforts to convert the trace, but resulting speedscope file may contain broken stacks as a result.");
-                    ConvertToSpeedscope(traceFilePath, speedscopeFileName, true);
-                    benchmarkToTraceFile[benchmarkCase] = speedscopeFileName;
-                }
-                else
-                {
-                    logger.WriteLine(LogKind.Error, $"An exception occurred during converting {traceFilePath} file to speedscope format: {ex}");
-                }
-            }
-        }
-
-        // Method copied from https://github.com/dotnet/diagnostics/blob/2c23d3265dd8f642a8d6cf4bb8a135a5ff8b00c2/src/Tools/dotnet-trace/TraceFileFormatConverter.cs#L64
-        private static void ConvertToSpeedscope(string fileToConvert, string outputFilename, bool continueOnError = false)
-        {
-            var etlxFilePath = TraceLog.CreateFromEventPipeDataFile(fileToConvert, null, new TraceLogOptions() { ContinueOnError = continueOnError });
-            using (var symbolReader = new SymbolReader(System.IO.TextWriter.Null) { SymbolPath = SymbolPath.MicrosoftSymbolServerPath })
-            using (var eventLog = new TraceLog(etlxFilePath))
-            {
-                var stackSource = new MutableTraceEventStackSource(eventLog)
-                {
-                    OnlyManagedCodeStacks = true // EventPipe currently only has managed code stacks.
-                };
-
-                var computer = new SampleProfilerThreadTimeComputer(eventLog, symbolReader)
-                {
-                    IncludeEventSourceEvents = false // SpeedScope handles only CPU samples, events are not supported
-                };
-                computer.GenerateThreadTimeStacks(stackSource);
-
-                SpeedScopeStackSourceWriter.WriteStackViewAsJson(stackSource, outputFilename);
-            }
-
-            if (File.Exists(etlxFilePath))
-            {
-                File.Delete(etlxFilePath);
-            }
         }
     }
 }
