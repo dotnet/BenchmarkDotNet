19169a288bd91069a925962059c2fea3a13b6ace
Andrey Akinshin <andrey.akinshin@gmail.com>
2020-03-03 13:01:12 +0300

    Add brand strings for latest Windows versions

--- src/BenchmarkDotNet/Environments/OsBrandStringHelper.cs
+++ src/BenchmarkDotNet/Environments/OsBrandStringHelper.cs
@@ -94,7 +94,8 @@ namespace BenchmarkDotNet.Environments
             { "10.0.16299", "10 Redstone 3 [1709, Fall Creators Update]" },
             { "10.0.17134", "10 Redstone 4 [1803, April 2018 Update]" },
             { "10.0.17763", "10 Redstone 5 [1809, October 2018 Update]" },
-            { "10.0.18362", "10 19H1 [1903, May 2019 Update]" }
+            { "10.0.18362", "10 19H1 [1903, May 2019 Update]" },
+            { "10.0.18363", "10 19H2 [1909, November 2019 Update]" }
         };
 
         private class Windows10Version
@@ -133,7 +134,10 @@ namespace BenchmarkDotNet.Environments
                 new Windows10Version(1703, "Redstone 2", "Creators Update", 15063),
                 new Windows10Version(1709, "Redstone 3", "Fall Creators Update", 16299),
                 new Windows10Version(1803, "Redstone 4", "April 2018 Update", 17134),
-                new Windows10Version(1809, "Redstone 5", "October 2018 Update", 17763)
+                new Windows10Version(1809, "Redstone 5", "October 2018 Update", 17763),
+                new Windows10Version(1903, "19H1", "May 2019 Update", 18362),
+                new Windows10Version(1909, "19H2", "November 2018 Update", 18363),
+                new Windows10Version(2004, "20H1", "?", 19041)
             };
 
             [CanBeNull]
