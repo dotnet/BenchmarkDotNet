1faf14a4e8fb70d9b05455addd7e1964643342ca
Wojciech Nagórski <wojtpl2@gmail.com>
2020-03-03 12:25:04 +0100

    Small improvements of EventPipeProfiler

--- src/BenchmarkDotNet/Diagnosers/EventPipeProfile.cs
+++ src/BenchmarkDotNet/Diagnosers/EventPipeProfile.cs
@@ -1,9 +1,23 @@
-﻿namespace BenchmarkDotNet.Diagnosers 
+﻿namespace BenchmarkDotNet.Diagnosers
 {
+    /// <summary>
+    /// Contains defined profiles for <see cref="EventPipeProfiler"/>.
+    /// </summary>
     public enum EventPipeProfile
     {
+        /// <summary>
+        /// Useful for tracking CPU usage and general .NET runtime information. This is the default option if no profile or providers are specified.
+        /// </summary>
         CpuSampling,
+
+        /// <summary>
+        /// Tracks GC collections and samples object allocations.
+        /// </summary>
         GcVerbose,
-        GcCollect
+
+        /// <summary>
+        /// Tracks GC collections only at very low overhead.
+        /// </summary>
+        GcCollect,
     }
 }
--- src/BenchmarkDotNet/Diagnosers/EventPipeProfileMapper.cs
+++ src/BenchmarkDotNet/Diagnosers/EventPipeProfileMapper.cs
@@ -3,42 +3,46 @@ using System.Diagnostics.Tracing;
 using Microsoft.Diagnostics.NETCore.Client;
 using Microsoft.Diagnostics.Tracing.Parsers;
 
-namespace BenchmarkDotNet.Diagnosers 
+namespace BenchmarkDotNet.Diagnosers
 {
     internal sealed class EventPipeProfileMapper
     {
-        internal static IReadOnlyDictionary<EventPipeProfile, EventPipeProvider[]> DotNetRuntimeProfiles { get; } = new Dictionary<EventPipeProfile, EventPipeProvider[]>
-        {
-            //Useful for tracking CPU usage and general .NET runtime information. This is the default option if no profile or providers are specified.
-            { EventPipeProfile.CpuSampling,
-                new[]
+        internal static IReadOnlyDictionary<EventPipeProfile, EventPipeProvider[]> DotNetRuntimeProfiles { get; } =
+            new Dictionary<EventPipeProfile, EventPipeProvider[]>
+            {
                 {
-                    new EventPipeProvider("Microsoft-DotNETCore-SampleProfiler", EventLevel.Informational),
-                    new EventPipeProvider("Microsoft-Windows-DotNETRuntime", EventLevel.Informational, (long) ClrTraceEventParser.Keywords.Default)
-                }},
-            //Tracks GC collections and samples object allocations.
-            {EventPipeProfile.GcVerbose,
-                new[]
+                    EventPipeProfile.CpuSampling,
+                    new[]
+                    {
+                        new EventPipeProvider("Microsoft-DotNETCore-SampleProfiler", EventLevel.Informational),
+                        new EventPipeProvider("Microsoft-Windows-DotNETRuntime", EventLevel.Informational, (long) ClrTraceEventParser.Keywords.Default)
+                    }
+                },
                 {
-                    new EventPipeProvider(
-                        name: "Microsoft-Windows-DotNETRuntime",
-                        eventLevel: EventLevel.Verbose,
-                        keywords: (long) ClrTraceEventParser.Keywords.GC |
-                                  (long) ClrTraceEventParser.Keywords.GCHandle |
-                                  (long) ClrTraceEventParser.Keywords.Exception
-                    ),
-                }},
-            //Tracks GC collections only at very low overhead.
-            {EventPipeProfile.GcCollect,
-                new[]
+                    EventPipeProfile.GcVerbose,
+                    new[]
+                    {
+                        new EventPipeProvider(
+                            name: "Microsoft-Windows-DotNETRuntime",
+                            eventLevel: EventLevel.Verbose,
+                            keywords: (long) ClrTraceEventParser.Keywords.GC |
+                                      (long) ClrTraceEventParser.Keywords.GCHandle |
+                                      (long) ClrTraceEventParser.Keywords.Exception
+                        ),
+                    }
+                },
                 {
-                    new EventPipeProvider(
-                        name: "Microsoft-Windows-DotNETRuntime",
-                        eventLevel: EventLevel.Informational,
-                        keywords: (long) ClrTraceEventParser.Keywords.GC |
-                                  (long) ClrTraceEventParser.Keywords.Exception
-                    )
-                }},
-        };
+                    EventPipeProfile.GcCollect,
+                    new[]
+                    {
+                        new EventPipeProvider(
+                            name: "Microsoft-Windows-DotNETRuntime",
+                            eventLevel: EventLevel.Informational,
+                            keywords: (long) ClrTraceEventParser.Keywords.GC |
+                                      (long) ClrTraceEventParser.Keywords.Exception
+                        )
+                    }
+                },
+            };
     }
 }
