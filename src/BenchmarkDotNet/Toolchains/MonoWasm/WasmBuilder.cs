d0187693769d76834d09a34f1c052481813e816e
Nathan Ricci <naricc@microsoft.com>
2020-07-07 15:31:31 -0400

    Added WasmBuilder.

--- src/BenchmarkDotNet/Toolchains/MonoWasm/WasmBuilder.cs
+++ src/BenchmarkDotNet/Toolchains/MonoWasm/WasmBuilder.cs
@@ -1,10 +1,35 @@
 ﻿using System;
+using System.Collections.Generic;
+using BenchmarkDotNet.Jobs;
+using BenchmarkDotNet.Loggers;
+using BenchmarkDotNet.Running;
+using BenchmarkDotNet.Toolchains.Results;
+using BenchmarkDotNet.Toolchains.DotNetCli;
+using System.IO;
+
 namespace BenchmarkDotNet.Toolchains.MonoWasm
 {
-    public class EmptyClass
+    public class WasmBuilder : DotNetCliBuilder
     {
-        public EmptyClass()
+        private WasmAppBuilder WasmAppBuilder;
+
+        public WasmBuilder(string targetFrameworkMoniker, WasmSettings wasmSettings, string customDotNetCliPath = null, TimeSpan? timeout = null)
+            :base (targetFrameworkMoniker, customDotNetCliPath, timeout)
         {
+            List<string> extraAssemblies = new List<string> { };
+
+            WasmAppBuilder = new WasmAppBuilder(wasmSettings);
         }
+
+
+        public override BuildResult Build(GenerateResult generateResult, BuildPartition buildPartition, ILogger logger)
+        {
+            BuildResult buildResult = base.Build(generateResult, buildPartition, logger);
+            WasmAppBuilder.BuildApp(buildPartition.ProgramName, generateResult.ArtifactsPaths.BuildArtifactsDirectoryPath);
+
+            return buildResult;
+        }
+
+
     }
 }
