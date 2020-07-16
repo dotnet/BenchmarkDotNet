be6c675f10534a4e8e34213243122c451fa799b8
Nathan Ricci <naricc@microsoft.com>
2020-07-16 17:18:52 -0400

    Reverted unneeded chagne to BenchmarkSwitcher.

--- src/BenchmarkDotNet/Running/BenchmarkSwitcher.cs
+++ src/BenchmarkDotNet/Running/BenchmarkSwitcher.cs
@@ -63,8 +63,6 @@ namespace BenchmarkDotNet.Running
             // VS generates bad assembly binding redirects for ValueTuple for Full .NET Framework
             // we need to keep the logic that uses it in a separate method and create DirtyAssemblyResolveHelper first
             // so it can ignore the version mismatch ;)
-
-            Console.WriteLine("!!! naricc modified BenchmarkDotNet");
             using (DirtyAssemblyResolveHelper.Create())
                 return RunWithDirtyAssemblyResolveHelper(args, config);
         }
