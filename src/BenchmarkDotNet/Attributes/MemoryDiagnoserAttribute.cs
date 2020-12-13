c918e150dff07e5009cd845cf35ef2a2941423e2
Tim <cassell.timothy@gmail.com>
2020-12-13 18:20:52 -0500

    MemoryDiagnoserAttribute constructor docs (mostly to rerun CI tests).

--- src/BenchmarkDotNet/Attributes/MemoryDiagnoserAttribute.cs
+++ src/BenchmarkDotNet/Attributes/MemoryDiagnoserAttribute.cs
@@ -11,6 +11,9 @@ namespace BenchmarkDotNet.Attributes
 
         public MemoryDiagnoserAttribute() : this(false) { }
 
+        /// <summary>
+        /// If <paramref name="includeSurvived"/> is true, monitoring will be enabled and survived memory will be measured on the first benchmark run.
+        /// </summary>
         public MemoryDiagnoserAttribute(bool includeSurvived)
         {
             Config = ManualConfig.CreateEmpty().AddDiagnoser(includeSurvived ? MemoryDiagnoser.WithSurvived : MemoryDiagnoser.Default);
