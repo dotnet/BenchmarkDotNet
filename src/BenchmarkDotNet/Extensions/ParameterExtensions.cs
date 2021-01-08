d727c3fdca0cdf954b26caf474efbb5adf6ac6b2
IdanZel <idan.zel.harari@gmail.com>
2021-01-08 16:10:27 +0200

    Added ParameterExtensions and NamedParameter

---
+++ src/BenchmarkDotNet/Extensions/ParameterExtensions.cs
@@ -0,0 +1,26 @@
+﻿using JetBrains.Annotations;
+
+namespace BenchmarkDotNet.Extensions
+{
+    public static class ParameterExtensions
+    {
+        [PublicAPI]
+        public static NamedParameter WithName(this object parameter, [NotNull] string name) => new NamedParameter(parameter, name);
+
+        [PublicAPI]
+        public static NamedParameter WithDefaultName(this object parameter) => new NamedParameter(parameter, null);
+    }
+
+    public class NamedParameter
+    {
+        public object Value { get; }
+
+        public string Name { get; }
+
+        internal NamedParameter(object value, string name)
+        {
+            Value = value;
+            Name = name;
+        }
+    }
+}
