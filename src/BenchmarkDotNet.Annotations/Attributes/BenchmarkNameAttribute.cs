3113f70e4aa7c9abececc22ad605a4b13c41d165
Christiano Donke <cdonke@outlook.com>
2021-02-19 14:24:37 -0300

    Adding new Attribute

---
+++ src/BenchmarkDotNet.Annotations/Attributes/BenchmarkNameAttribute.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BenchmarkDotNet.Attributes
+{
+    [AttributeUsage(AttributeTargets.Class)]
+    public class BenchmarkNameAttribute : Attribute
+    {
+        public BenchmarkNameAttribute(){        }
+        public BenchmarkNameAttribute(string name)
+            => Name = name;
+
+        public string Name { get; set; }
+    }
+}
