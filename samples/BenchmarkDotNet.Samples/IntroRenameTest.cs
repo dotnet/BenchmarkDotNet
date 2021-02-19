f96a5a41359a58a997a03a0ccf6a681fccede928
Christiano Donke <cdonke@outlook.com>
2021-02-19 14:24:50 -0300

    Sample applying new attribute

---
+++ samples/BenchmarkDotNet.Samples/IntroRenameTest.cs
@@ -0,0 +1,22 @@
+﻿using BenchmarkDotNet.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace BenchmarkDotNet.Samples
+{
+    [BenchmarkName("Used to be 'IntroRenameTest', now is 'My Renamed Test'")]
+    public class IntroRenameTest
+    {
+        // And define a method with the Benchmark attribute
+        [Benchmark]
+        public void Sleep() => Thread.Sleep(10);
+
+        // You can write a description for your method.
+        [Benchmark(Description = "Thread.Sleep(10)")]
+        public void SleepWithDescription() => Thread.Sleep(10);
+    }
+}
