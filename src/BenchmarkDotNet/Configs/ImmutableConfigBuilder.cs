ffa58883fd21c71a60ece0a066b1ddfe3f40b4cc
Giuseppe Lippolis <workgroupengineering@outlook.com>
2021-05-15 12:52:27 +0200

    feat(configuration): Print warning when dependency of export is already present in the configuration.

--- src/BenchmarkDotNet/Configs/ImmutableConfigBuilder.cs
+++ src/BenchmarkDotNet/Configs/ImmutableConfigBuilder.cs
@@ -38,7 +38,7 @@ namespace BenchmarkDotNet.Configs
 
             var uniqueHardwareCounters = source.GetHardwareCounters().ToImmutableHashSet();
             var uniqueDiagnosers = GetDiagnosers(source.GetDiagnosers(), uniqueHardwareCounters);
-            var uniqueExporters = GetExporters(source.GetExporters(), uniqueDiagnosers);
+            var uniqueExporters = GetExporters(source.GetExporters(), uniqueDiagnosers, uniqueLoggers);
             var uniqueAnalyzers = GetAnalysers(source.GetAnalysers(), uniqueDiagnosers);
 
             var uniqueValidators = GetValidators(source.GetValidators(), MandatoryValidators, source.Options);
@@ -88,7 +88,9 @@ namespace BenchmarkDotNet.Configs
             return builder.ToImmutable();
         }
 
-        private static ImmutableArray<IExporter> GetExporters(IEnumerable<IExporter> exporters, ImmutableHashSet<IDiagnoser> uniqueDiagnosers)
+        private static ImmutableArray<IExporter> GetExporters(IEnumerable<IExporter> exporters,
+            ImmutableHashSet<IDiagnoser> uniqueDiagnosers,
+            IEnumerable<Loggers.ILogger> loggers = null)
         {
 
             var mergeDictionary = new Dictionary<System.Type, IExporter>();
@@ -114,28 +116,42 @@ namespace BenchmarkDotNet.Configs
                 if (result[i] is IExporterDependencies exporterDependencies)
                     foreach (var dependency in exporterDependencies.Dependencies)
                         /*
-                         * I don't know how to correct
-                         * the problem is:
-                         * you have banchmark like this:
+                         *  When exporter that depends on an other already present in the configuration print warning.
                          *
-                         *  // Global Current Culture separetor is Semicolon;
+                         *  Example:
+                         *
+                         *  // Global Current Culture separator is Semicolon;
                          *  [CsvMeasurementsExporter(CsvSeparator.Comma)] // force use Comma
                          *  [RPlotExporter]
-                         *  public class MYBanch
+                         *  public class MyBanch
                          *  {
                          *
                          *  }
                          *
                          *  RPlotExporter is depend from CsvMeasurementsExporter.Default
                          *
-                         *  If I check for the type of the exporter RPlotExporter will fail
-                         *  because it will find an unexpected delimiter,
-                         *  if I stay the check as it is
-                         *  RPlotExporter it will work but the generated CSV
-                         *  will not have the required delimiter.
-                        */
-                        if (!result.Contains(dependency))
+                         *  On active logger will by print:
+                         *  "The CsvMeasurementsExporter is already present in the configuration. There may be unexpected results of RPlotExporter.
+                         *
+                         */
+                        if (!result.Any(exporter=> exporter.GetType() == dependency.GetType()))
                             result.Insert(i, dependency); // All the exporter dependencies should be added before the exporter
+                        else
+                        {
+                            if (loggers?.Any() == true)
+                            {
+                                var warning = $"The {dependency.Name} is already present in the configuration. There may be unexpected results of {result[i].GetType().Name}.";
+                                foreach (var logger in loggers)
+                                {
+                                    logger.WriteLine(Loggers.LogKind.Warning,
+                                         warning);
+                                }
+                            }
+                            else
+                            {
+                                // If there are no loggers should I throw an exception?
+                            }
+                        }
 
             result.Sort((left, right) => (left is IExporterDependencies).CompareTo(right is IExporterDependencies)); // the case when they were defined by user in wrong order ;)
 
