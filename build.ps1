d97285236327de3cdc74f87fa6eb896b4a643630
Andrii Kurdiumov <kant2002@gmail.com>
2021-06-14 18:18:31 +0600

    Add ValidateExecutableReferencesMatchSelfContained (#1725)
    * Add ValidateExecutableReferencesMatchSelfContained
    This is required to make CoreRT work on 5.0.300 SDK
    
    * Update SDK to 5.0.301

--- build.ps1
+++ build.ps1
@@ -32,7 +32,7 @@ Param(
 )
 
 $CakeVersion = "1.1.0"
-$DotNetVersion = "5.0.101";
+$DotNetVersion = "5.0.301";
 $DotNetInstallerUri = "https://dot.net/v1/dotnet-install.ps1";
 $NugetUrl = "https://dist.nuget.org/win-x86-commandline/latest/nuget.exe"
 
