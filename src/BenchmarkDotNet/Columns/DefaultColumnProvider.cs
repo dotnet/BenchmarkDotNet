95605152698de1452f95b042e6105c3809562dec
Yegor Stepanov <yegor.stepanov@outlook.com>
2021-12-07 14:41:39 +0300

    Add Allocation Ratio column (#1859)
    Co-authored-by: Adam Sitnik <adam.sitnik@gmail.com>

--- src/BenchmarkDotNet/Columns/DefaultColumnProvider.cs
+++ src/BenchmarkDotNet/Columns/DefaultColumnProvider.cs
@@ -63,12 +63,22 @@ namespace BenchmarkDotNet.Columns
                     bool hide = stdDevColumnValues.All(value => value == "0.00" || value == "0.01");
                     if (!hide)
                         yield return BaselineRatioColumn.RatioStdDev;
+
+                    if (HasMemoryDiagnoser(summary))
+                    {
+                        yield return new BaselineAllocationRatioColumn();
+                    }
                 }
             }
 
             private static bool NeedToShow(Summary summary, Func<Statistics, bool> check)
             {
                 return summary.Reports != null && summary.Reports.Any(r => r.ResultStatistics != null && check(r.ResultStatistics));
+            }
+
+            private static bool HasMemoryDiagnoser(Summary summary)
+            {
+                return summary.BenchmarksCases.Any(c => c.Config.HasMemoryDiagnoser());
             }
         }
 
--- src/BenchmarkDotNet/Diagnosers/MemoryDiagnoser.cs
+++ src/BenchmarkDotNet/Diagnosers/MemoryDiagnoser.cs
@@ -45,20 +45,6 @@ namespace BenchmarkDotNet.Diagnosers
             yield return new Metric(AllocatedMemoryMetricDescriptor.Instance, diagnoserResults.GcStats.GetBytesAllocatedPerOperation(diagnoserResults.BenchmarkCase));
         }
 
-        private class AllocatedMemoryMetricDescriptor : IMetricDescriptor
-        {
-            internal static readonly IMetricDescriptor Instance = new AllocatedMemoryMetricDescriptor();
-
-            public string Id => "Allocated Memory";
-            public string DisplayName => "Allocated";
-            public string Legend => "Allocated memory per single operation (managed only, inclusive, 1KB = 1024B)";
-            public string NumberFormat => "0.##";
-            public UnitType UnitType => UnitType.Size;
-            public string Unit => SizeUnit.B.Name;
-            public bool TheGreaterTheBetter => false;
-            public int PriorityInCategory => GC.MaxGeneration + 1;
-        }
-
         private class GarbageCollectionsMetricDescriptor : IMetricDescriptor
         {
             internal static readonly IMetricDescriptor Gen0 = new GarbageCollectionsMetricDescriptor(0);
