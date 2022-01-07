d946cf61dc891e9b081cdc38b3b7ee6234f1e4d5
Nathan Ricci <naricc@microsoft.com>
2022-01-07 18:31:45 -0500

    Removed old work arounds.

--- src/BenchmarkDotNet/Toolchains/MonoAotLLVM/MonoAotLLVMBuilder.cs
+++ src/BenchmarkDotNet/Toolchains/MonoAotLLVM/MonoAotLLVMBuilder.cs
@@ -21,48 +21,8 @@ namespace BenchmarkDotNet.Toolchains.MonoAotLLVM
         {
             BuildResult buildResult = dotnetCliBuilder.Build(generateResult, buildPartition, logger);
 
-            if (buildResult.IsBuildSuccess
-                || File.Exists(generateResult.ArtifactsPaths.ExecutablePath)) // the build has failed, but produced an exe
-            {
-                RenameSharedLibaries(generateResult, logger);
-            }
-
-            if (!buildResult.IsBuildSuccess && File.Exists(generateResult.ArtifactsPaths.ExecutablePath))
-            {
-                logger.WriteLineError($"The buid has failed with following error: {buildResult.ErrorMessage}");
-                logger.WriteLine("But it has produced the executable file, so we are going to try to use it.");
-
-                return BuildResult.Success(generateResult); // we lie on purpose
-            }
-
             return buildResult;
         }
 
-        // This method is as work around because the MonoAOTCompilerTask
-        // does not generate files with the right names.
-        // In the future it may have an option to write .so/.dylib files,
-        // and we can get rid of this method.
-        public void RenameSharedLibaries(GenerateResult generateResult, ILogger logger)
-        {
-            string[] publishDirFiles = Directory.GetFiles(generateResult.ArtifactsPaths.BuildArtifactsDirectoryPath, ".dll.o");
-            string sharedObjectExtension = "";
-
-            if (RuntimeInformation.IsMacOSX())
-            {
-                sharedObjectExtension = "dylib";
-            }
-            else if (RuntimeInformation.IsLinux())
-            {
-                sharedObjectExtension = "so";
-            }
-
-            foreach (string fileName in publishDirFiles)
-            {
-                string newFileName = Path.ChangeExtension(fileName, sharedObjectExtension);
-
-                logger.WriteLine($"RenameSharedLibraries: Moving ${fileName} to {newFileName}");
-                File.Move(fileName, newFileName);
-            }
-        }
     }
 }
