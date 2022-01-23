80ed095d565d3ae7baea38d8d6ec0f081e51219e
Adam Sitnik <adam.sitnik@gmail.com>
2022-01-23 08:40:02 +0100

    install packages required for source gen to work

---
+++ src/BenchmarkDotNet.SourceGenerators/Class1.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.CodeAnalysis;
+using System;
+
+namespace BenchmarkDotNet.SourceGenerators
+{
+    [Generator]
+    public class BenchmarkGenerator : ISourceGenerator
+    {
+        public void Execute(GeneratorExecutionContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Initialize(GeneratorInitializationContext context)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
