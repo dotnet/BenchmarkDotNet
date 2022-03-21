0754df4a87f7416242a7472c0fc47fb0a0815fc3
Adam Sitnik <adam.sitnik@gmail.com>
2022-03-21 12:34:24 +0100

    Apply suggestions from code review

--- src/BenchmarkDotNet/Extensions/StringAndTextExtensions.cs
+++ src/BenchmarkDotNet/Extensions/StringAndTextExtensions.cs
@@ -129,12 +129,6 @@ namespace BenchmarkDotNet.Extensions
         /// <param name="argument">The argument to append to this string builder</param>
         /// <returns>The string builder with the arguments added</returns>
         internal static StringBuilder AppendArgument(this StringBuilder stringBuilder, object argument)
-        {
-            if (argument == null)
-            {
-                return stringBuilder;
-            }
-            return AppendArgument(stringBuilder, argument.ToString());
-        }
+            => argument ?? stringBuilder : AppendArgument(stringBuilder, argument.ToString());
     }
 }
