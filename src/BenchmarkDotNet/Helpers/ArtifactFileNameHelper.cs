dotnet/BenchmarkDotNet

commit c6d6fb2b1c392990b12fd2267aa62e440bd7e976

Fix Merge operation failed for EtwProfiler (#1545)
* we should consider trace file extension as part of the name

* add failing test case

* don't use long paths for file names, even if the Windows feature is enabled, fixes #1544

* update TraceEvent to the latest version

* nit: user Assert.Range that gives a clear error message instead of a very general Assert.True

* handle one more edge case

src/BenchmarkDotNet/Helpers/ArtifactFileNameHelper.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using BenchmarkDotNet.Diagnosers;
 using BenchmarkDotNet.Exporters;
 using BenchmarkDotNet.Extensions;
+using BenchmarkDotNet.Portability;
 using BenchmarkDotNet.Running;
 using BenchmarkDotNet.Toolchains;
 
@@ -18,7 +19,11 @@ namespace BenchmarkDotNet.Helpers
         {
             string nameNoLimit = GetFilePathNoLimits(details, creationTime, fileExtension);
 
-            int limit = PathFeatures.AreAllLongPathsAvailable() ? CommonSenseLimit : WindowsOldPathLimit;
+            // long paths can be enabled on Windows but it does not mean that ETW is going to work fine..
+            // so we always use 260 as limit on Windows
+            int limit =  RuntimeInformation.IsWindows()
+                ? WindowsOldPathLimit - "userheap.etl".Length // the session files get merged, they need to have same name (without extension)
+                : CommonSenseLimit;
 
             if (nameNoLimit.Length <= limit)
             {
@@ -40,7 +45,7 @@ namespace BenchmarkDotNet.Helpers
             string shortTypeName = FolderNameHelper.ToFolderName(details.BenchmarkCase.Descriptor.Type, includeNamespace: false);
             string methodName = details.BenchmarkCase.Descriptor.WorkloadMethod.Name;
             string parameters = details.BenchmarkCase.HasParameters
-                ? $"-hash{Hashing.HashString(FullNameProvider.GetMethodName(details.BenchmarkCase)).ToString()}"
+                ? $"-hash{Hashing.HashString(FullNameProvider.GetMethodName(details.BenchmarkCase))}"
                 : string.Empty;
 
             string fileName = $@"{shortTypeName}.{methodName}{parameters}";

src/BenchmarkDotNet.Diagnostics.Windows/BenchmarkDotNet.Diagnostics.Windows.csproj
@@ -12,6 +12,6 @@
     <ProjectReference Include="..\BenchmarkDotNet\BenchmarkDotNet.csproj" />
   </ItemGroup>
   <ItemGroup>
-    <PackageReference Include="Microsoft.Diagnostics.Tracing.TraceEvent" Version="2.0.57" PrivateAssets="contentfiles;analyzers" />
+    <PackageReference Include="Microsoft.Diagnostics.Tracing.TraceEvent" Version="2.0.61" PrivateAssets="contentfiles;analyzers" />
   </ItemGroup>
 </Project>

src/BenchmarkDotNet/BenchmarkDotNet.csproj
@@ -27,7 +27,7 @@
     <PackageReference Include="System.Threading.Tasks.Extensions" Version="4.5.2" />
     <PackageReference Include="Microsoft.CodeAnalysis.CSharp" Version="2.10.0" />
     <PackageReference Include="Microsoft.Diagnostics.NETCore.Client" Version="0.2.61701" />
-    <PackageReference Include="Microsoft.Diagnostics.Tracing.TraceEvent" Version="2.0.57" PrivateAssets="contentfiles;analyzers" />
+    <PackageReference Include="Microsoft.Diagnostics.Tracing.TraceEvent" Version="2.0.61" PrivateAssets="contentfiles;analyzers" />
   </ItemGroup>
   <ItemGroup Condition="'$(OS)' == 'Windows_NT'">
     <ProjectReference Include="..\BenchmarkDotNet.Disassembler.x64\BenchmarkDotNet.Disassembler.x64.csproj">

tests/BenchmarkDotNet.Tests/ArtifactFileNameHelperTests.cs
@@ -1,0 +1,87 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Diagnosers;
+using BenchmarkDotNet.Helpers;
+using BenchmarkDotNet.Running;
+using BenchmarkDotNet.Tests.XUnit;
+using System.Buffers.Tests;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace BenchmarkDotNet.Tests
+{
+    public class ArtifactFileNameHelperTests
+    {
+        [FactWindowsOnly(nonWindowsSkipReason: "ETW Sessions can be created only on Windows")]
+        public void OnWindowsWeMustAlwaysUseOldLongPathsLimitForSessionFiles()
+        {
+            var config = DefaultConfig.Instance
+                .WithArtifactsPath(@"C:\Projects\performance\artifacts\bin\MicroBenchmarks\Release\netcoreapp5.0\BenchmarkDotNet.Artifacts");
+
+            var benchmarkCase = BenchmarkConverter.TypeToBenchmarks(typeof(RentReturnArrayPoolTests<byte>), config).BenchmarksCases.First();
+
+            var parameters = new DiagnoserActionParameters(
+                process: null,
+                benchmarkCase: benchmarkCase,
+                new BenchmarkId(0, benchmarkCase));
+
+            foreach (string fileExtension in new[] { "etl", "kernel.etl", "userheap.etl" })
+            {
+                var traceFilePath = ArtifactFileNameHelper.GetTraceFilePath(parameters, new System.DateTime(2020, 10, 1), fileExtension);
+
+                Assert.InRange(actual: traceFilePath.Length, low: 0, high: 260);
+            }
+        }
+    }
+}
+
+namespace System.Buffers.Tests
+{
+    [GenericTypeArguments(typeof(byte))] // value type
+    [GenericTypeArguments(typeof(object))] // reference type
+    public class RentReturnArrayPoolTests<T>
+    {
+        private readonly ArrayPool<T> _createdPool = ArrayPool<T>.Create();
+        private const int Iterations = 100_000;
+        [Params(4096)]
+        public int RentalSize;
+
+        [Params(false, true)]
+        public bool ManipulateArray { get; set; }
+
+        [Params(false, true)]
+        public bool Async { get; set; }
+
+        [Params(false, true)]
+        public bool UseSharedPool { get; set; }
+
+        private ArrayPool<T> Pool => UseSharedPool ? ArrayPool<T>.Shared : _createdPool;
+
+        private static void Clear(T[] arr) => arr.AsSpan().Clear();
+
+        private static T IterateAll(T[] arr)
+        {
+            T ret = default;
+            foreach (T item in arr)
+            {
+                ret = item;
+            }
+            return ret;
+        }
+
+        [Benchmark(OperationsPerInvoke = Iterations)]
+        public async Task SingleSerial()
+        {
+            ArrayPool<T> pool = Pool;
+            for (int i = 0; i < Iterations; i++)
+            {
+                T[] arr = pool.Rent(RentalSize);
+                if (ManipulateArray) Clear(arr);
+                if (Async) await Task.Yield();
+                if (ManipulateArray) IterateAll(arr);
+                pool.Return(arr);
+            }
+        }
+    }
+}

commit 757a1dc37132ad5d579eb02231d0d1a4087abddc

Update ArtifaceFileNameHelper error message
Update error message to reflect the change introduced by https://github.com/dotnet/BenchmarkDotNet/pull/1545

src/BenchmarkDotNet/Helpers/ArtifactFileNameHelper.cs
@@ -54,8 +54,8 @@ namespace BenchmarkDotNet.Helpers
 
             if (finalResult.Length > limit)
             {
-                throw new NotSupportedException($"The full benchmark name: \"{fileName}\" combined with artifiacts path: \"{details.Config.ArtifactsPath}\" is too long. " +
-                   $"Please enable long paths support, set the value of {nameof(details.Config)}.{nameof(details.Config.ArtifactsPath)} to shorter path or rename the type or method.");
+                throw new NotSupportedException($"The full benchmark name: \"{fileName}\" combined with artifacts path: \"{details.Config.ArtifactsPath}\" is too long. " +
+                   $"Please set the value of {nameof(details.Config)}.{nameof(details.Config.ArtifactsPath)} to shorter path or rename the type or method.");
             }
 
             return finalResult;