2bae23f3c1d0cc128b14c888e05d8cb5bbfe81fc
Adam Sitnik <adam.sitnik@gmail.com>
2022-04-06 21:55:05 +0200

    Fix AppVeyor build (#1987)
    * add Visual C++ toolset to $path so NativeAOT tests can build native executables
    
    * set Platform for Windows build
    
    * force the tool to not look for the .dll in platform-specific directory
    
    * don't run some of the very time consuming tests on AppVeyor as we hit 1h limit

---
+++ tests/BenchmarkDotNet.IntegrationTests/ContinuousIntegration.cs
@@ -0,0 +1,14 @@
+﻿using BenchmarkDotNet.Portability;
+using System;
+
+namespace BenchmarkDotNet.IntegrationTests
+{
+    internal static class ContinuousIntegration
+    {
+        internal static bool IsGitHubActionsOnWindows()
+            => RuntimeInformation.IsWindows() && !string.IsNullOrEmpty(Environment.GetEnvironmentVariable("GITHUB_ACTION"));
+
+        internal static bool IsAppVeyorOnWindows()
+            => RuntimeInformation.IsWindows() && !string.IsNullOrEmpty(Environment.GetEnvironmentVariable("APPVEYOR"));
+    }
+}
