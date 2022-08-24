e954c3269379cc99a678e8116fd828df22ee7e3b
Adam Sitnik <adam.sitnik@gmail.com>
2022-08-24 11:19:12 +0200

    we can't target net6.0 and use .NET 7 ILCompiler anymore (#2080)

--- tests/BenchmarkDotNet.IntegrationTests/NativeAotTests.cs
+++ tests/BenchmarkDotNet.IntegrationTests/NativeAotTests.cs
@@ -23,6 +23,8 @@ namespace BenchmarkDotNet.IntegrationTests
                 return;
             if (ContinuousIntegration.IsAppVeyorOnWindows()) // too time consuming for AppVeyor (1h limit)
                 return;
+            if (NativeAotRuntime.GetCurrentVersion().RuntimeMoniker < RuntimeMoniker.NativeAot70) // we can't target net6.0 and use .NET 7 ILCompiler anymore (#2080)
+                return;
 
             var toolchain = NativeAotToolchain.CreateBuilder().UseNuGet().IlcInstructionSet(IsAvx2Supported() ? "avx2" : "").ToToolchain();
 
