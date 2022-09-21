98a8223bbddf7b4c18617e5fb860eeeec3904047
Jan Vorlicek <janvorli@microsoft.com>
2022-09-21 17:58:16 +0200

    Resolve indirect addresses in disassembly

--- src/BenchmarkDotNet/Disassemblers/InstructionFormatter.cs
+++ src/BenchmarkDotNet/Disassemblers/InstructionFormatter.cs
@@ -12,7 +12,23 @@ namespace BenchmarkDotNet.Disassemblers
                 case Asm asm when asm.IntelInstruction.HasValue:
                     return InstructionFormatter.Format(asm.IntelInstruction.Value, formatter, printInstructionAddresses, pointerSize);
                 case Asm asm when asm.Arm64Instruction is not null:
-                    return $"{asm.Arm64Instruction.Mnemonic} {asm.Arm64Instruction.Operand}"; // TODO: implement proper formatting
+                {
+                    string operand = asm.Arm64Instruction.Operand;
+
+                    // Symbolize branch and call instructions target with immediate address argument
+                    if (asm.Arm64Instruction.Details.BelongsToGroup(Gee.External.Capstone.Arm64.Arm64InstructionGroupId.ARM64_GRP_BRANCH_RELATIVE) &&
+                        asm.ReferencedAddress != null &&
+                        !asm.IsReferencedAddressIndirect &&
+                        (asm.AddressToNameMapping.TryGetValue(asm.ReferencedAddress.Value, out string text) ||
+                        asm?.AddressToLabelMapping.TryGetValue(asm.ReferencedAddress.Value, out text) == true))
+                    {
+                        string partToReplace = $"#0x{asm.ReferencedAddress.Value:x}";
+                        operand = operand.Replace(partToReplace, text);
+                    }
+
+                    string instructionAddress = printInstructionAddresses ? $"{asm.Arm64Instruction.Address:X16} " : string.Empty;
+                    return $"{instructionAddress}{asm.Arm64Instruction.Mnemonic} {operand}";
+                }
                 case Sharp sharp:
                     return sharp.Text;
                 case MonoCode mono:
