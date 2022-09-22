a6086f850bfdce38db10144d67155d44ff4220f5
Adam Sitnik <adam.sitnik@gmail.com>
2022-09-22 20:17:32 +0200

    add an attribute and a sample

--- src/BenchmarkDotNet/Diagnosers/PerfCollectProfilerConfig.cs
+++ src/BenchmarkDotNet/Diagnosers/PerfCollectProfilerConfig.cs
@@ -4,9 +4,9 @@ namespace BenchmarkDotNet.Diagnosers
 {
     public class PerfCollectProfilerConfig
     {
-        /// <param name="performExtraBenchmarksRun">if set to true, benchmarks will be executed one more time with the profiler attached. If set to false, there will be no extra run but the results will contain overhead. True by default.</param>
-        /// <param name="timeoutInSeconds">how long should we wait for the perfcollect script to start collecting and/or finish processing the trace. 30s by default</param>
-        public PerfCollectProfilerConfig(bool performExtraBenchmarksRun = true, int timeoutInSeconds = 60)
+        /// <param name="performExtraBenchmarksRun">When set to true, benchmarks will be executed one more time with the profiler attached. If set to false, there will be no extra run but the results will contain overhead. False by default.</param>
+        /// <param name="timeoutInSeconds">How long should we wait for the perfcollect script to start collecting and/or finish processing the trace. 120s by default</param>
+        public PerfCollectProfilerConfig(bool performExtraBenchmarksRun = false, int timeoutInSeconds = 120)
         {
             RunMode = performExtraBenchmarksRun ? RunMode.ExtraRun : RunMode.NoOverhead;
             Timeout = TimeSpan.FromSeconds(timeoutInSeconds);
