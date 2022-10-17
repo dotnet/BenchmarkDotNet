eda1a412418ba59eda676cd909accbed6756eeee
Adam Sitnik <adam.sitnik@gmail.com>
2022-10-17 15:31:04 +0200

    remove dependency to Mono.Posix.NETStandard to unblock WASM benchmarks runs (#2154)
    * remove dependency to Mono.Posix.NETStandard to unblock WASM benchmark runs
    
    * Unix samples fix
    
    * increase the default timeout to 5 minutes

--- src/BenchmarkDotNet/Attributes/PerfCollectProfilerAttribute.cs
+++ src/BenchmarkDotNet/Attributes/PerfCollectProfilerAttribute.cs
@@ -8,8 +8,8 @@ namespace BenchmarkDotNet.Attributes
     public class PerfCollectProfilerAttribute : Attribute, IConfigSource
     {
         /// <param name="performExtraBenchmarksRun">When set to true, benchmarks will be executed one more time with the profiler attached. If set to false, there will be no extra run but the results will contain overhead. False by default.</param>
-        /// <param name="timeoutInSeconds">How long should we wait for the perfcollect script to finish processing the trace. 120s by default.</param>
-        public PerfCollectProfilerAttribute(bool performExtraBenchmarksRun = false, int timeoutInSeconds = 120)
+        /// <param name="timeoutInSeconds">How long should we wait for the perfcollect script to finish processing the trace. 300s by default.</param>
+        public PerfCollectProfilerAttribute(bool performExtraBenchmarksRun = false, int timeoutInSeconds = 300)
         {
             Config = ManualConfig.CreateEmpty().AddDiagnoser(new PerfCollectProfiler(new PerfCollectProfilerConfig(performExtraBenchmarksRun, timeoutInSeconds)));
         }
