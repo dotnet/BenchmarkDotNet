464a69fe07b23d9b7e68be4a16d6cc61f519f56b
Yegor Stepanov <yegor.stepanov@outlook.com>
2022-10-22 12:42:03 +0300

    Remove allowMultiple=true

--- src/BenchmarkDotNet/Attributes/Columns/BaselineColumnAttribute.cs
+++ src/BenchmarkDotNet/Attributes/Columns/BaselineColumnAttribute.cs
@@ -3,7 +3,7 @@ using BenchmarkDotNet.Columns;
 
 namespace BenchmarkDotNet.Attributes
 {
-    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Assembly, AllowMultiple = true)]
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Assembly)]
     public class BaselineColumnAttribute : ColumnConfigBaseAttribute
     {
         public BaselineColumnAttribute() : base(BaselineColumn.Default) { }
--- src/BenchmarkDotNet/Attributes/Columns/LogicalGroupColumnAttribute.cs
+++ src/BenchmarkDotNet/Attributes/Columns/LogicalGroupColumnAttribute.cs
@@ -3,7 +3,7 @@ using BenchmarkDotNet.Columns;
 
 namespace BenchmarkDotNet.Attributes
 {
-    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Assembly, AllowMultiple = true)]
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Assembly)]
     public class LogicalGroupColumnAttribute : ColumnConfigBaseAttribute
     {
         public LogicalGroupColumnAttribute() : base(LogicalGroupColumn.Default) { }
