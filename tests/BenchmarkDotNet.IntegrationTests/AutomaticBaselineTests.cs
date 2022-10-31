d8d99e2a42cb82ae299f4be60ac3c073671665c4
Serg046 <serg046@outlook.com>
2022-10-31 03:08:16 +0300

    Issue #1024: Make AutomaticBaselineSelectionIsCorrect test stable enough

--- tests/BenchmarkDotNet.IntegrationTests/AutomaticBaselineTests.cs
+++ tests/BenchmarkDotNet.IntegrationTests/AutomaticBaselineTests.cs
@@ -1,11 +1,9 @@
-﻿using BenchmarkDotNet.Attributes;
-using BenchmarkDotNet.Columns;
+﻿using BenchmarkDotNet.Columns;
 using BenchmarkDotNet.Configs;
-using BenchmarkDotNet.Jobs;
 using BenchmarkDotNet.Reports;
-using BenchmarkDotNet.Running;
+using BenchmarkDotNet.Tests.Mocks;
+using System;
 using System.Linq;
-using System.Threading;
 using Xunit;
 
 namespace BenchmarkDotNet.IntegrationTests
@@ -15,39 +13,20 @@ namespace BenchmarkDotNet.IntegrationTests
         [Fact]
         public void AutomaticBaselineSelectionIsCorrect()
         {
-            var config = CreateConfig();
+            var config = ManualConfig.CreateEmpty()
+                .AddColumnProvider(DefaultColumnProviders.Instance)
+                .WithAutomaticBaseline(AutomaticBaselineMode.Fastest);
 
-            var summary = BenchmarkRunner.Run<BaselineSample>();
-
+            var summary = MockFactory.CreateSummary(config, hugeSd: true, Array.Empty<Metric>());
             var table = summary.GetTable(SummaryStyle.Default);
-            var column = table.Columns.Single(c => c.Header == "Ratio");
-            Assert.Equal(2, column.Content.Length);
-            Assert.Equal(1.0, double.Parse(column.Content[1])); // Ratio of TwoMilliseconds
-            Assert.True(double.Parse(column.Content[0]) > 1.0); // Ratio of TwoHundredMilliseconds
+            var method = table.Columns.Single(c => c.Header == "Method");
+            var ratio = table.Columns.Single(c => c.Header == "Ratio");
+
+            Assert.Equal(2, method.Content.Length);
+            Assert.Equal(nameof(MockFactory.MockBenchmarkClass.Foo), method.Content[0]);
+            Assert.Equal(1.0, double.Parse(ratio.Content[0])); // A faster one, see measurements in MockFactory.cs
+            Assert.Equal(nameof(MockFactory.MockBenchmarkClass.Bar), method.Content[1]);
+            Assert.Equal(1.5, double.Parse(ratio.Content[1]));
         }
-
-        [AutomaticBaseline(AutomaticBaselineMode.Fastest)]
-        public class BaselineSample
-        {
-            [Benchmark]
-            public void TwoHundredMilliseconds()
-            {
-                Thread.Sleep(200);
-            }
-
-            [Benchmark]
-            public void TwoMilliseconds()
-            {
-                Thread.Sleep(2);
-            }
-        }
-
-        private IConfig CreateConfig()
-            => ManualConfig.CreateEmpty()
-                .AddJob(Job.ShortRun
-                    .WithEvaluateOverhead(false) // no need to run idle for this test
-                    .WithWarmupCount(0) // don't run warmup to save some time for our CI runs
-                    .WithIterationCount(1)) // single iteration is enough for us
-                .AddColumnProvider(DefaultColumnProviders.Instance);
     }
 }
