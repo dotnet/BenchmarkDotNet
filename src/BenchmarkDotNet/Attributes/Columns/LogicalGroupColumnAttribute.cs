5f8fb2d94c7f222c8ef3157dc070e7fb5259b836
Yegor Stepanov <yegor.stepanov@outlook.com>
2022-11-01 21:37:41 +0300

    Remove redundant attributes

--- src/BenchmarkDotNet/Attributes/Columns/LogicalGroupColumnAttribute.cs
+++ src/BenchmarkDotNet/Attributes/Columns/LogicalGroupColumnAttribute.cs
@@ -1,9 +1,7 @@
-﻿using System;
-using BenchmarkDotNet.Columns;
+﻿using BenchmarkDotNet.Columns;
 
 namespace BenchmarkDotNet.Attributes
 {
-    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Assembly)]
     public class LogicalGroupColumnAttribute : ColumnConfigBaseAttribute
     {
         public LogicalGroupColumnAttribute() : base(LogicalGroupColumn.Default) { }
