e2678fa511c39b7422cdd82b34f77ed4434bbfca
Adam Sitnik <adam.sitnik@gmail.com>
2022-12-01 16:58:47 +0100

    improve the tests (some of them require these settings)

--- tests/BenchmarkDotNet.IntegrationTests/MemoryDiagnoserTests.cs
+++ tests/BenchmarkDotNet.IntegrationTests/MemoryDiagnoserTests.cs
@@ -299,6 +299,8 @@ namespace BenchmarkDotNet.IntegrationTests
                     .WithWarmupCount(0) // don't run warmup to save some time for our CI runs
                     .WithIterationCount(1) // single iteration is enough for us
                     .WithGcForce(false)
+                    .WithGcServer(false)
+                    .WithGcConcurrent(false)
                     .WithEnvironmentVariable("COMPlus_TieredCompilation", "0") // Tiered JIT can allocate some memory on a background thread, let's disable it to make our tests less flaky (#1542)
                     .WithToolchain(toolchain))
                 .AddColumnProvider(DefaultColumnProviders.Instance)
