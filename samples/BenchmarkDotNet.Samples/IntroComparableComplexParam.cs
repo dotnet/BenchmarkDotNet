8f59c5b09727a5b210d72ee75de2db2f29c8052f
Mohammad Rahhal <m.r992@hotmail.com>
2023-05-10 15:44:05 +0200

    Simplify intro example

--- samples/BenchmarkDotNet.Samples/IntroComparableComplexParam.cs
+++ samples/BenchmarkDotNet.Samples/IntroComparableComplexParam.cs
@@ -6,17 +6,15 @@ namespace BenchmarkDotNet.Samples
 {
     public class IntroComparableComplexParam
     {
-        // property with public setter
         [ParamsSource(nameof(ValuesForA))]
         public ComplexParam A { get; set; }
 
-        // public property
         public IEnumerable<ComplexParam> ValuesForA => new[] { new ComplexParam(1, "First"), new ComplexParam(2, "Second") };
 
         [Benchmark]
         public object Benchmark() => A;
 
-        // Only non generic IComparable is required, but implementing IComparable<> too is customary.
+        // Only non generic IComparable is required to provide custom order behavior, but implementing IComparable<> too is customary.
         public class ComplexParam : IComparable<ComplexParam>, IComparable
         {
             public ComplexParam(int value, string name)
@@ -29,20 +27,9 @@ namespace BenchmarkDotNet.Samples
 
             public string Name { get; set; }
 
-            public override string ToString()
-            {
-                return Name;
-            }
+            public override string ToString() => Name;
 
-            public int CompareTo(ComplexParam other)
-            {
-                if (other == null)
-                {
-                    return 1;
-                }
-
-                return Value.CompareTo(other.Value);
-            }
+            public int CompareTo(ComplexParam other) => other == null ? 1 : Value.CompareTo(other.Value);
 
             public int CompareTo(object obj)
             {
