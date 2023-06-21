1b0b518adbde240c44dd047ab49afbf05fab827e
Yegor Stepanov <yegor.stepanov@outlook.com>
2023-06-21 10:48:52 +0300

    Update Program.cs

--- templates/templates/BenchmarkDotNet.BenchmarkProjectTemplate.CSharp/Program.cs
+++ templates/templates/BenchmarkDotNet.BenchmarkProjectTemplate.CSharp/Program.cs
@@ -1,3 +1,4 @@
+using BenchmarkDotNet.Configs;
 using BenchmarkDotNet.Running;
 
 namespace _BenchmarkProjectName_
@@ -6,7 +7,11 @@ namespace _BenchmarkProjectName_
     {
         public static void Main(string[] args)
         {
-            var summary = BenchmarkRunner.Run<$(BenchmarkName)>();
+            var config = DefaultConfig.Instance;
+            var summary = BenchmarkRunner.Run<$(BenchmarkName)>(config, args);
+
+            // Use this to select benchmarks from the console:
+            // var summaries = BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args, config);
         }
     }
 }
