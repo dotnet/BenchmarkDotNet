213549acf2ee05ea8402aeb4be2e4cdfeaeb04d1
Tim <cassell.timothy@gmail.com>
2023-06-21 02:18:29 -0400

    ﻿Removed `Consumer` from benchmark actions. Overhead always returns `void`.

---
+++ src/BenchmarkDotNet/Toolchains/InProcess/Emit/Implementation/Emitters/NonVoidConsumeEmitter.cs
@@ -0,0 +1,31 @@
+﻿using System.Reflection.Emit;
+using BenchmarkDotNet.Helpers.Reflection.Emit;
+
+namespace BenchmarkDotNet.Toolchains.InProcess.Emit.Implementation
+{
+    internal class NonVoidConsumeEmitter : ConsumeEmitter
+    {
+        private LocalBuilder disassemblyDiagnoserLocal;
+
+        public NonVoidConsumeEmitter(ConsumableTypeInfo consumableTypeInfo) : base(consumableTypeInfo)
+        {
+        }
+
+        protected override void DeclareDisassemblyDiagnoserLocalsOverride(ILGenerator ilBuilder)
+        {
+            // optional local if default(T) uses .initobj
+            disassemblyDiagnoserLocal = ilBuilder.DeclareOptionalLocalForReturnDefault(ConsumableInfo.WorkloadMethodReturnType);
+        }
+
+        protected override void EmitDisassemblyDiagnoserReturnDefaultOverride(ILGenerator ilBuilder)
+        {
+            ilBuilder.EmitReturnDefault(ConsumableInfo.WorkloadMethodReturnType, disassemblyDiagnoserLocal);
+        }
+
+        protected override void EmitActionAfterCallOverride(ILGenerator ilBuilder)
+        {
+            // IL_000a: pop
+            ilBuilder.Emit(OpCodes.Pop);
+        }
+    }
+}
