dc3cefde39c755339d61d813f5a4d24c768c26f6
Yegor Stepanov <yegor.stepanov@outlook.com>
2023-07-01 14:25:41 +0300

    Improve templates (#2338)
    * Fix batch file
    
    * Update Program.cs
    
    * Fix type of framework parameter
    
    * Fix docs
    
    * Set consoleApp=true by default
    
    * Remove outdated info. The parameter was changed in #1632
    
    * Update docs

--- templates/install-from-source.bat
+++ templates/install-from-source.bat
@@ -1,4 +1,14 @@
-dotnet build -c Release BenchmarkDotNet.Templates.csproj
-dotnet pack -c Release BenchmarkDotNet.Templates.csproj
-dotnet new -u BenchmarkDotNet.Templates
-dotnet new -i BenchmarkDotNet.Templates::0.0.0-* --nuget-source .\bin\Release\
+:: Run only from the folder where the batch file is located!
+
+dotnet build BenchmarkDotNet.Templates.csproj -c Release
+dotnet pack BenchmarkDotNet.Templates.csproj -c Release
+
+:: If we install the templates via a folder path, then it will have a different ID (ID=folder path).
+:: It will conflict with BDN templates from nuget.
+:: We need to install the templates via a FILE path in order to update the template from nuget.
+::
+:: https://stackoverflow.com/questions/47450531/batch-write-output-of-dir-to-a-variable
+for /f "delims=" %%a in ('dir /s /b BenchmarkDotNet.Templates*.nupkg') do set "nupkg_path=%%a"
+
+dotnet new --uninstall "BenchmarkDotNet.Templates"
+dotnet new --install "%nupkg_path%"
