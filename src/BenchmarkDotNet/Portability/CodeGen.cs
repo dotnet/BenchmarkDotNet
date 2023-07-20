bc59d48367b4f4c41305494f265e1f65f8b3ace1
Tim Cassell <35501420+timcassell@users.noreply.github.com>
2023-07-20 03:43:52 -0400

    `AggressiveOptimization` in `InProcess` toolchains (#2335)
    * Apply `AggressiveOptimization` to workload actions in `InProcess` toolchains.
    
    * Placed constants in `BenchmarkDotNet.Portability.CodeGenHelper`.

---
+++ src/BenchmarkDotNet/Portability/CodeGen.cs
@@ -0,0 +1,12 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+
+namespace BenchmarkDotNet.Portability
+{
+    public static class CodeGenHelper
+    {
+        // AggressiveOptimization is not available in netstandard2.0, so just use the value casted to enum.
+        public const MethodImplOptions AggressiveOptimizationOption = (MethodImplOptions) 512;
+        public const MethodImplAttributes AggressiveOptimizationOptionForEmit = (MethodImplAttributes) 512;
+    }
+}
