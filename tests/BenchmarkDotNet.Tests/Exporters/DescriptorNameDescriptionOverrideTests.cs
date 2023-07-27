c9b03625c6458986abd56a9db55f2de6f6ce8e6b
Nepp3r <plstas06@gmail.com>
2023-07-27 21:44:10 +0200

    Adding tests for situations with overridable description of benchmark

---
+++ tests/BenchmarkDotNet.Tests/Exporters/DescriptorNameDescriptionOverrideTests.cs
@@ -0,0 +1,41 @@
+﻿using BenchmarkDotNet.Attributes;
+
+namespace BenchmarkDotNet.Tests.Exporters
+{
+    internal class DescriptorNameDescriptionOverrideTests
+    {
+        public class MethodDescriptionOverrideTests
+        {
+            [Benchmark]
+            public void VoidTest() { }
+
+            [Benchmark(Description = "from Benchmark")]
+            public void BenchmarkAttributeOverride() { }
+
+            [Benchmark]
+            [BenchmarkDescription("OverrideFromAttribute")]
+            public void BenchmarkDescriptionAttributeOverride() { }
+
+            [Benchmark(Description = "Who are the winner?")]
+            [BenchmarkDescription("OverrideFromAttribute")]
+            public void BothAttributeOverride() { }
+        }
+        [BenchmarkDescription("FromClassDescription")]
+        public class ClassDescriptionOverrideTests
+        {
+            [Benchmark]
+            public void VoidTest() { }
+
+            [Benchmark(Description = "from Benchmark")]
+            public void ClassBenchmarkAttributeOverride() { }
+
+            [Benchmark]
+            [BenchmarkDescription("OverrideFromAttribute")]
+            public void ClassBenchmarkDescriptionAttributeOverride() { }
+
+            [Benchmark(Description = "Who are the winner?")]
+            [BenchmarkDescription("OverrideFromAttribute")]
+            public void ClassBothAttributeOverride() { }
+        }
+    }
+}
