a5eacc38d1fec82a6b500f1e60d08a55567a8ea3
Nepp3r <plstas06@gmail.com>
2023-07-28 09:14:00 +0200

    Fixing DescriptorNameDescriptionTests

--- tests/BenchmarkDotNet.Tests/Exporters/DescriptorNameDescriptionOverrideTests.cs
+++ tests/BenchmarkDotNet.Tests/Exporters/DescriptorNameDescriptionOverrideTests.cs
@@ -1,4 +1,6 @@
 ﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+using Xunit;
 
 namespace BenchmarkDotNet.Tests.Exporters
 {
@@ -10,15 +12,30 @@ namespace BenchmarkDotNet.Tests.Exporters
             public void VoidTest() { }
 
             [Benchmark(Description = "from Benchmark")]
-            public void BenchmarkAttributeOverride() { }
+            public void BenchmarkAttributeOverride()
+            {
+                var description = BenchmarkConverter.TypeToBenchmarks(typeof(MethodDescriptionOverrideTests));
+
+                Assert.Equal("OverrideFromBenchmarkAttribute", description.BenchmarksCases[0].Descriptor.DisplayInfo);
+            }
 
             [Benchmark]
             [BenchmarkDescription("OverrideFromAttribute")]
-            public void BenchmarkDescriptionAttributeOverride() { }
+            public void BenchmarkDescriptionAttributeOverride()
+            {
+                var description = BenchmarkConverter.TypeToBenchmarks(typeof(MethodDescriptionOverrideTests));
+
+                Assert.Equal("OverrideFromBenchmarkDescriptionMethod", description.BenchmarksCases[0].Descriptor.DisplayInfo);
+            }
 
             [Benchmark(Description = "Who are the winner?")]
             [BenchmarkDescription("OverrideFromAttribute")]
-            public void BothAttributeOverride() { }
+            public void BothAttributeOverride()
+            {
+                var description = BenchmarkConverter.TypeToBenchmarks(typeof(MethodDescriptionOverrideTests));
+
+                Assert.Equal("OverrideFromBothAttribute", description.BenchmarksCases[0].Descriptor.DisplayInfo);
+            }
         }
         [BenchmarkDescription("FromClassDescription")]
         public class ClassDescriptionOverrideTests
@@ -27,15 +44,27 @@ namespace BenchmarkDotNet.Tests.Exporters
             public void VoidTest() { }
 
             [Benchmark(Description = "from Benchmark")]
-            public void ClassBenchmarkAttributeOverride() { }
+            public void ClassBenchmarkAttributeOverride(){
+                var description = BenchmarkConverter.TypeToBenchmarks(typeof(MethodDescriptionOverrideTests));
+
+                Assert.Equal("ClassOverrideFromBenchmarkAttribute", description.BenchmarksCases[0].Descriptor.DisplayInfo);
+            }
 
             [Benchmark]
             [BenchmarkDescription("OverrideFromAttribute")]
-            public void ClassBenchmarkDescriptionAttributeOverride() { }
+            public void ClassBenchmarkDescriptionAttributeOverride(){
+                var description = BenchmarkConverter.TypeToBenchmarks(typeof(MethodDescriptionOverrideTests));
+
+                Assert.Equal("ClassOverrideFromBenchmarkDescriptionMethod", description.BenchmarksCases[0].Descriptor.DisplayInfo);
+            }
 
             [Benchmark(Description = "Who are the winner?")]
             [BenchmarkDescription("OverrideFromAttribute")]
-            public void ClassBothAttributeOverride() { }
+            public void ClassBothAttributeOverride(){
+                var description = BenchmarkConverter.TypeToBenchmarks(typeof(MethodDescriptionOverrideTests));
+
+                Assert.Equal("ClassOverrideFromBothAttribute", description.BenchmarksCases[0].Descriptor.DisplayInfo);
+            }
         }
     }
 }
