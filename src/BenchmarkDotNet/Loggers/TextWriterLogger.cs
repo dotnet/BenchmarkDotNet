16530793e5d5a9d0ced60b1c2422b06e5c11b4b9
ntr <ntr@ihfood.dk>
2023-08-17 13:46:26 +0200

    Refactor out base TextWriterLogger from StreamLogger

---
+++ src/BenchmarkDotNet/Loggers/TextWriterLogger.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+
+namespace BenchmarkDotNet.Loggers
+{
+    public class TextWriterLogger : ILogger, IDisposable
+    {
+        private readonly TextWriter writer;
+
+        public TextWriterLogger(TextWriter writer) => this.writer = writer;
+
+        public void Dispose() => writer.Dispose();
+
+        public string Id => nameof(TextWriterLogger);
+        public int Priority => 0;
+        public void Write(LogKind logKind, string text) => writer.Write(text);
+
+        public void WriteLine() => writer.WriteLine();
+
+        public void WriteLine(LogKind logKind, string text) => writer.WriteLine(text);
+
+        public void Flush() => writer.Flush();
+    }
+}
