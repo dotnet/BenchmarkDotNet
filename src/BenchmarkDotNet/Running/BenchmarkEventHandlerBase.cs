3f0936da42fc47d6b1e616a129fa569e9c3607fa
Cameron Aavik <caaavik@microsoft.com>
2023-08-24 20:01:49 +1000

    Add base event handler with virtual methods

---
+++ src/BenchmarkDotNet/Running/BenchmarkEventHandlerBase.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using BenchmarkDotNet.Reports;
+using BenchmarkDotNet.Toolchains.Results;
+using BenchmarkDotNet.Validators;
+
+namespace BenchmarkDotNet.Running
+{
+    public class BenchmarkEventHandlerBase : IBenchmarkEventHandler
+    {
+        public virtual void HandleBuildFailed(BenchmarkCase benchmarkCase, BuildResult buildResult)
+        {
+        }
+
+        public virtual void HandleCompletedBenchmark(BenchmarkCase benchmarkCase, BenchmarkReport report)
+        {
+        }
+
+        public virtual void HandleCompletedBenchmarksInType(Type type, Summary summary)
+        {
+        }
+
+        public virtual void HandleRunBenchmark(BenchmarkCase benchmarkCase)
+        {
+        }
+
+        public virtual void HandleRunBenchmarksInType(Type type, IReadOnlyList<BenchmarkCase> benchmarks)
+        {
+        }
+
+        public virtual void HandleStartBuildStage()
+        {
+        }
+
+        public virtual void HandleStartRunStage()
+        {
+        }
+
+        public virtual void HandleStartValidationStage()
+        {
+        }
+
+        public virtual void HandleUnsupportedBenchmark(BenchmarkCase benchmarkCase)
+        {
+        }
+
+        public virtual void HandleValidationError(ValidationError validationError)
+        {
+        }
+    }
+}
