2d763cf1dfabd90bb47d169981aca2f0269f6a02
Alina Smirnova <BlueOstrich@yandex.ru>
2023-08-27 14:26:56 +0200

    Enable nullability for BenchmarkDotNet.Annotations

--- src/BenchmarkDotNet.Annotations/Attributes/ArgumentsAttribute.cs
+++ src/BenchmarkDotNet.Annotations/Attributes/ArgumentsAttribute.cs
@@ -6,13 +6,13 @@ namespace BenchmarkDotNet.Attributes
     [AttributeUsage(AttributeTargets.Method, AllowMultiple = true)]
     public class ArgumentsAttribute : PriorityAttribute
     {
-        public object[] Values { get; }
+        public object?[] Values { get; }
 
         // CLS-Compliant Code requires a constructor without an array in the argument list
         [PublicAPI]
         public ArgumentsAttribute() => Values = new object[0];
 
-        public ArgumentsAttribute(params object[] values)
-            => Values = values ?? new object[] { null }; // when users do Arguments(null) they mean one, null argument
+        public ArgumentsAttribute(params object?[]? values)
+            => Values = values ?? new object?[] { null }; // when users do Arguments(null) they mean one, null argument
     }
 }
