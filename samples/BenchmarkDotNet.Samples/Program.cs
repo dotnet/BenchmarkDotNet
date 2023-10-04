c0092678ab3d1253fd0417983ec4ce5c9b95b257
Cameron Aavik <caaavik@microsoft.com>
2023-10-05 09:53:40 +1000

    WIP removing entry point

--- samples/BenchmarkDotNet.Samples/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿using BenchmarkDotNet.Running;
-
-namespace BenchmarkDotNet.Samples
-{
-    public class Program
-    {
-        public static void Main(string[] args) => BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args);
-    }
-}
---
+++ src/BenchmarkDotNet/build/BenchmarkDotNet.cs
@@ -0,0 +1,7 @@
+﻿using BenchmarkDotNet.Running;
+using System.Reflection;
+
+public class Program
+{
+    public static void Main(string[] args) => BenchmarkSwitcher.FromAssembly(Assembly.GetEntryAssembly()).Run(args);
+}
