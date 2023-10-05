134b8edd09ad7dad0a17728eae4e9f50e16d3fe0
Andrey Akinshin <andrey.akinshin@gmail.com>
2023-10-05 14:19:37 +0200

    [build] Automatic NextVersion evaluation

--- build/BenchmarkDotNet.Build/Program.cs
+++ build/BenchmarkDotNet.Build/Program.cs
@@ -223,6 +223,9 @@ public class ReleaseTask : FrostingTask<BuildContext>, IHelpProvider
             new Example(Name)
                 .WithArgument(KnownOptions.Stable)
                 .WithArgument(KnownOptions.NextVersion, "0.1.1729")
+                .WithArgument(KnownOptions.Push),
+            new Example(Name)
+                .WithArgument(KnownOptions.Stable)
                 .WithArgument(KnownOptions.Push)
         }
     };
