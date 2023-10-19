e170684208103ca5ba4212ad8dc7c2aad5cf02d4
Andrey Akinshin <andrey.akinshin@gmail.com>
2023-10-19 18:18:03 +0200

    Adjust 'Failed to set up high priority' message
    https://github.com/dotnet/BenchmarkDotNet/issues/2436#issuecomment-1768620186

--- src/BenchmarkDotNet/Extensions/ProcessExtensions.cs
+++ src/BenchmarkDotNet/Extensions/ProcessExtensions.cs
@@ -32,7 +32,7 @@ namespace BenchmarkDotNet.Extensions
             }
             catch (Exception ex)
             {
-                logger.WriteLineError($"Failed to set up high priority. Make sure you have the right permissions. Message: {ex.Message}");
+                logger.WriteLineInfo($"// Failed to set up high priority ({ex.Message}). In order to run benchmarks with high priority, make sure you have the right permissions.");
             }
         }
 
