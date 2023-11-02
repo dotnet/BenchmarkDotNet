96a76a8bfd546af64dc5279b87992ede23b9a384
Michał Petryka <35800402+MichalPetryka@users.noreply.github.com>
2023-11-02 21:33:58 +0100

    Update GcStats.cs

--- src/BenchmarkDotNet/Engines/GcStats.cs
+++ src/BenchmarkDotNet/Engines/GcStats.cs
@@ -14,13 +14,6 @@ namespace BenchmarkDotNet.Engines
 
         public static readonly long AllocationQuantum = CalculateAllocationQuantumSize();
 
-#if !NET6_0_OR_GREATER
-        // do not reorder these, CheckMonitoringTotalAllocatedMemorySize relies on GetTotalAllocatedBytesDelegate being initialized first
-        private static readonly Func<bool, long> GetTotalAllocatedBytesDelegate = CreateGetTotalAllocatedBytesDelegate();
-        private static readonly Func<long> GetAllocatedBytesForCurrentThreadDelegate = CreateGetAllocatedBytesForCurrentThreadDelegate();
-        private static readonly bool CanUseMonitoringTotalAllocatedMemorySize = CheckMonitoringTotalAllocatedMemorySize();
-#endif
-
         public static readonly GcStats Empty = default;
 
         private GcStats(int gen0Collections, int gen1Collections, int gen2Collections, long? allocatedBytes, long totalOperations)
@@ -157,85 +150,18 @@ namespace BenchmarkDotNet.Engines
 #if NET6_0_OR_GREATER
             return GC.GetTotalAllocatedBytes(precise: true);
 #else
-            if (GetTotalAllocatedBytesDelegate != null) // it's .NET Core 3.0 with the new API available
-                return GetTotalAllocatedBytesDelegate.Invoke(true); // true for the "precise" argument
+            if (ReflectionHelper.GetTotalAllocatedBytesDelegate != null) // it's .NET Core 3.0 with the new API available
+                return ReflectionHelper.GetTotalAllocatedBytesDelegate.Invoke(true); // true for the "precise" argument
 
-            if (CanUseMonitoringTotalAllocatedMemorySize) // Monitoring is not available in Mono, see http://stackoverflow.com/questions/40234948/how-to-get-the-number-of-allocated-bytes-
+            if (ReflectionHelper.CanUseMonitoringTotalAllocatedMemorySize) // Monitoring is not available in Mono, see http://stackoverflow.com/questions/40234948/how-to-get-the-number-of-allocated-bytes-
                 return AppDomain.CurrentDomain.MonitoringTotalAllocatedMemorySize;
 
-            if (GetAllocatedBytesForCurrentThreadDelegate != null)
-                return GetAllocatedBytesForCurrentThreadDelegate.Invoke();
+            if (ReflectionHelper.GetAllocatedBytesForCurrentThreadDelegate != null)
+                return ReflectionHelper.GetAllocatedBytesForCurrentThreadDelegate.Invoke();
 
             return null;
 #endif
         }
-
-#if !NET6_0_OR_GREATER
-        private static Func<bool, long> CreateGetTotalAllocatedBytesDelegate()
-        {
-            try
-            {
-                // this method is not a part of .NET Standard so we need to use reflection
-                var method = typeof(GC).GetTypeInfo().GetMethod("GetTotalAllocatedBytes", BindingFlags.Public | BindingFlags.Static);
-
-                if (method == null)
-                    return null;
-
-                // we create delegate to avoid boxing, IMPORTANT!
-                var del = (Func<bool, long>)method.CreateDelegate(typeof(Func<bool, long>));
-
-                // verify the api works
-                return del(true) >= 0 ? del : null;
-            }
-            catch
-            {
-                return null;
-            }
-        }
-
-        private static Func<long> CreateGetAllocatedBytesForCurrentThreadDelegate()
-        {
-            try
-            {
-                // this method is not a part of .NET Standard so we need to use reflection
-                var method = typeof(GC).GetTypeInfo().GetMethod("GetAllocatedBytesForCurrentThread", BindingFlags.Public | BindingFlags.Static);
-
-                if (method == null)
-                    return null;
-
-                // we create delegate to avoid boxing, IMPORTANT!
-                var del = (Func<long>)method.CreateDelegate(typeof(Func<long>));
-
-                // verify the api works
-                return del() >= 0 ? del : null;
-            }
-            catch
-            {
-                return null;
-            }
-        }
-
-        private static bool CheckMonitoringTotalAllocatedMemorySize()
-        {
-            try
-            {
-                // we potentially don't want to enable monitoring if we don't need it
-                if (GetTotalAllocatedBytesDelegate != null)
-                    return false;
-
-                // check if monitoring is enabled
-                if (!AppDomain.MonitoringIsEnabled)
-                    AppDomain.MonitoringIsEnabled = true;
-
-                // verify the api works
-                return AppDomain.MonitoringIsEnabled && AppDomain.CurrentDomain.MonitoringTotalAllocatedMemorySize >= 0;
-            }
-            catch
-            {
-                return false;
-            }
-        }
-#endif
 
         public string ToOutputLine()
             => $"{ResultsLinePrefix} {Gen0Collections} {Gen1Collections} {Gen2Collections} {AllocatedBytes?.ToString() ?? MetricColumn.UnknownRepresentation} {TotalOperations}";
@@ -310,5 +236,80 @@ namespace BenchmarkDotNet.Engines
         public override bool Equals(object obj) => obj is GcStats other && Equals(other);
 
         public override int GetHashCode() => HashCode.Combine(Gen0Collections, Gen1Collections, Gen2Collections, AllocatedBytes, TotalOperations);
+
+        #if !NET6_0_OR_GREATER
+        private static class ReflectionHelper
+        {
+            // do not reorder these, CheckMonitoringTotalAllocatedMemorySize relies on GetTotalAllocatedBytesDelegate being initialized first
+            public static readonly Func<bool, long> GetTotalAllocatedBytesDelegate = CreateGetTotalAllocatedBytesDelegate();
+            public static readonly Func<long> GetAllocatedBytesForCurrentThreadDelegate = CreateGetAllocatedBytesForCurrentThreadDelegate();
+            public static readonly bool CanUseMonitoringTotalAllocatedMemorySize = CheckMonitoringTotalAllocatedMemorySize();
+
+            private static Func<bool, long> CreateGetTotalAllocatedBytesDelegate()
+            {
+                try
+                {
+                    // this method is not a part of .NET Standard so we need to use reflection
+                    var method = typeof(GC).GetTypeInfo().GetMethod("GetTotalAllocatedBytes", BindingFlags.Public | BindingFlags.Static);
+
+                    if (method == null)
+                        return null;
+
+                    // we create delegate to avoid boxing, IMPORTANT!
+                    var del = (Func<bool, long>)method.CreateDelegate(typeof(Func<bool, long>));
+
+                    // verify the api works
+                    return del.Invoke(true) >= 0 ? del : null;
+                }
+                catch
+                {
+                    return null;
+                }
+            }
+
+            private static Func<long> CreateGetAllocatedBytesForCurrentThreadDelegate()
+            {
+                try
+                {
+                    // this method is not a part of .NET Standard so we need to use reflection
+                    var method = typeof(GC).GetTypeInfo().GetMethod("GetAllocatedBytesForCurrentThread", BindingFlags.Public | BindingFlags.Static);
+
+                    if (method == null)
+                        return null;
+
+                    // we create delegate to avoid boxing, IMPORTANT!
+                    var del = (Func<long>)method.CreateDelegate(typeof(Func<long>));
+
+                    // verify the api works
+                    return del.Invoke() >= 0 ? del : null;
+                }
+                catch
+                {
+                    return null;
+                }
+            }
+
+            private static bool CheckMonitoringTotalAllocatedMemorySize()
+            {
+                try
+                {
+                    // we potentially don't want to enable monitoring if we don't need it
+                    if (GetTotalAllocatedBytesDelegate != null)
+                        return false;
+
+                    // check if monitoring is enabled
+                    if (!AppDomain.MonitoringIsEnabled)
+                        AppDomain.MonitoringIsEnabled = true;
+
+                    // verify the api works
+                    return AppDomain.MonitoringIsEnabled && AppDomain.CurrentDomain.MonitoringTotalAllocatedMemorySize >= 0;
+                }
+                catch
+                {
+                    return false;
+                }
+            }
+        }
+#endif
     }
 }
