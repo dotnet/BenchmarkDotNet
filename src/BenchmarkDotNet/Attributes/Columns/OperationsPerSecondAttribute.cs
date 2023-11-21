b88bbf073a23f53456855576d146f675a5fa9391
Oleg V. Kozlyuk <elquentaro@gmail.com>
2023-11-21 12:37:55 +0300

    Add OperationsPerSecondAttribute

---
+++ src/BenchmarkDotNet/Attributes/Columns/OperationsPerSecondAttribute.cs
@@ -0,0 +1,9 @@
+using BenchmarkDotNet.Columns;
+using JetBrains.Annotations;
+
+namespace BenchmarkDotNet.Attributes;
+
+public class OperationsPerSecondAttribute : ColumnConfigBaseAttribute
+{
+    public OperationsPerSecondAttribute() : base(StatisticColumn.OperationsPerSecond) { }
+}
