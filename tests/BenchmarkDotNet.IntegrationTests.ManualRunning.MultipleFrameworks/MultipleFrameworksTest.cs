ca70c52648ba4556bec78e4ac64d3b6e798c7292
Tim <cassell.timothy@gmail.com>
2023-12-03 19:44:19 -0500

    Fixed some tests.

--- tests/BenchmarkDotNet.IntegrationTests.ManualRunning.MultipleFrameworks/MultipleFrameworksTest.cs
+++ tests/BenchmarkDotNet.IntegrationTests.ManualRunning.MultipleFrameworks/MultipleFrameworksTest.cs
@@ -3,10 +3,14 @@ using BenchmarkDotNet.Attributes;
 using BenchmarkDotNet.Configs;
 using BenchmarkDotNet.Extensions;
 using BenchmarkDotNet.Jobs;
+using BenchmarkDotNet.Portability;
 using Xunit;
 
 namespace BenchmarkDotNet.IntegrationTests
 {
+    // Note: To properly test this locally, modify
+    // BenchmarkDotNet.IntegrationTests.ManualRunning.MultipleFrameworks.csproj,
+    // following the comments in that file.
     public class MultipleFrameworksTest : BenchmarkTestExecutor
     {
         private const string TfmEnvVarName = "TfmEnvVarName";
@@ -15,9 +19,23 @@ namespace BenchmarkDotNet.IntegrationTests
         [InlineData(RuntimeMoniker.Net461)]
         [InlineData(RuntimeMoniker.Net48)]
         [InlineData(RuntimeMoniker.NetCoreApp20)]
-        [InlineData(RuntimeMoniker.Net70)]
+        [InlineData(RuntimeMoniker.Net80)]
         public void EachFrameworkIsRebuilt(RuntimeMoniker runtime)
         {
+#if NET461
+            // We cannot detect what target framework version the host was compiled for on full Framework,
+            // which causes the RoslynToolchain to be used instead of CsProjClassicNetToolchain when the host is full Framework
+            // (because full Framework always uses the version that's installed on the machine, unlike Core),
+            // which means if the machine has net48 installed (not net481), the net461 host with net48 runtime moniker
+            // will not be recompiled, causing the test to fail.
+
+            // If we ever change the default toolchain to CsProjClassicNetToolchain instead of RoslynToolchain, we can remove this check.
+            if (runtime == RuntimeMoniker.Net48)
+            {
+                // XUnit doesn't provide Assert.Skip API yet.
+                return;
+            }
+#endif
             var config = ManualConfig.CreateEmpty().AddJob(Job.Dry.WithRuntime(runtime.GetRuntime()).WithEnvironmentVariable(TfmEnvVarName, runtime.ToString()));
             CanExecute<ValuePerTfm>(config);
         }
@@ -31,8 +49,8 @@ namespace BenchmarkDotNet.IntegrationTests
                 RuntimeMoniker.Net48;
 #elif NETCOREAPP2_0
                 RuntimeMoniker.NetCoreApp20;
-#elif NET7_0
-                RuntimeMoniker.Net70;
+#elif NET8_0
+                RuntimeMoniker.Net80;
 #else
                 RuntimeMoniker.NotRecognized;
 #endif
