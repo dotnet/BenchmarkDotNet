c2f40c16b66988867d219b6109b6f181aa8aa471
Matt Chaulklin <mchaulklin@gmail.com>
2024-01-23 15:34:48 -0500

    addking validator

---
+++ src/BenchmarkDotNet/Validators/SdkValidator.cs
@@ -0,0 +1,118 @@
+﻿using BenchmarkDotNet.Jobs;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BenchmarkDotNet.Validators
+{
+    public class SdkValidator : IValidator
+    {
+        public static readonly SdkValidator Instance = new SdkValidator();
+
+        private SdkValidator()
+        { }
+
+        public bool TreatsWarningsAsErrors => true;
+
+        public IEnumerable<ValidationError> Validate(ValidationParameters validationParameters)
+        {
+            foreach (var benchmark in validationParameters.Benchmarks)
+            {
+                if (benchmark == null || benchmark.Job == null || benchmark.Job.Environment?.Runtime?.RuntimeMoniker == null)
+                {
+                    continue;
+                }
+
+                var runtimeMoniker = benchmark.Job.Environment.Runtime.RuntimeMoniker;
+                if (!IsSdkInstalled(runtimeMoniker))
+                {
+                    yield return new ValidationError(
+                        TreatsWarningsAsErrors,
+                        $"The required SDK for {runtimeMoniker} is not installed",
+                        benchmark);
+                }
+            }
+        }
+
+        private bool IsSdkInstalled(RuntimeMoniker runtimeMoniker)
+        {
+            var startInfo = new System.Diagnostics.ProcessStartInfo
+            {
+                FileName = "dotnet",
+                Arguments = "--list-sdks",
+                RedirectStandardOutput = true,
+                UseShellExecute = false,
+                CreateNoWindow = true,
+            };
+
+            var process = new System.Diagnostics.Process { StartInfo = startInfo };
+            process.Start();
+
+            string output = process.StandardOutput.ReadToEnd();
+            process.WaitForExit();
+
+            string requiredSdkVersion = GetSdkVersionFromMoniker(runtimeMoniker);
+            return output.Contains(requiredSdkVersion);
+        }
+
+        private string GetSdkVersionFromMoniker(RuntimeMoniker runtimeMoniker)
+        {
+            switch (runtimeMoniker)
+            {
+                case RuntimeMoniker.Net461:
+                    return "4.6.1";
+
+                case RuntimeMoniker.Net462:
+                    return "4.6.2";
+
+                case RuntimeMoniker.Net47:
+                    return "4.7";
+
+                case RuntimeMoniker.Net471:
+                    return "4.7.1";
+
+                case RuntimeMoniker.Net472:
+                    return "4.7.2";
+
+                case RuntimeMoniker.Net48:
+                    return "4.8";
+
+                case RuntimeMoniker.Net481:
+                    return "4.8.1";
+
+                case RuntimeMoniker.NetCoreApp20:
+                    return "2.0";
+
+                case RuntimeMoniker.NetCoreApp21:
+                    return "2.1";
+
+                case RuntimeMoniker.NetCoreApp22:
+                    return "2.2";
+
+                case RuntimeMoniker.NetCoreApp30:
+                    return "3.0";
+
+                case RuntimeMoniker.NetCoreApp31:
+                    return "3.1";
+
+                case RuntimeMoniker.Net50:
+                    return "5.0";
+
+                case RuntimeMoniker.Net60:
+                    return "6.0";
+
+                case RuntimeMoniker.Net70:
+                    return "7.0";
+
+                case RuntimeMoniker.Net80:
+                    return "8.0";
+
+                case RuntimeMoniker.Net90:
+                    return "9.0";
+
+                default:
+                    throw new NotImplementedException($"SDK version check not implemented for {runtimeMoniker}");
+            }
+        }
+    }
+}
