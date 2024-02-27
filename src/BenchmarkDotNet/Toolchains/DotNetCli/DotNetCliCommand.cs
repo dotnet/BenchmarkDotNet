6d7e25ecdbde90444aaf8f2488c9f8ba492ee9f1
Cameron Aavik <99771732+caaavik-msft@users.noreply.github.com>
2024-02-27 17:21:48 +1000

    Attempt at adding WASM integration test

--- src/BenchmarkDotNet/Toolchains/DotNetCli/DotNetCliCommand.cs
+++ src/BenchmarkDotNet/Toolchains/DotNetCli/DotNetCliCommand.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.IO;
 using System.Text;
 using BenchmarkDotNet.Characteristics;
 using BenchmarkDotNet.Extensions;
--- tests/BenchmarkDotNet.IntegrationTests/WasmTests.cs
+++ tests/BenchmarkDotNet.IntegrationTests/WasmTests.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using BenchmarkDotNet.Attributes;
 using BenchmarkDotNet.Configs;
 using BenchmarkDotNet.Environments;
@@ -6,6 +7,8 @@ using BenchmarkDotNet.Jobs;
 using BenchmarkDotNet.Portability;
 using BenchmarkDotNet.Tests.Loggers;
 using BenchmarkDotNet.Tests.XUnit;
+using BenchmarkDotNet.Toolchains.DotNetCli;
+using BenchmarkDotNet.Toolchains.MonoWasm;
 using Xunit.Abstractions;
 
 namespace BenchmarkDotNet.IntegrationTests
@@ -17,10 +20,22 @@ namespace BenchmarkDotNet.IntegrationTests
         [FactEnvSpecific("WASM is only supported on Unix", EnvRequirement.NonWindows)]
         public void WasmIsSupported()
         {
+            var dotnetVersion = "net8.0";
             var logger = new OutputLogger(Output);
+            var netCoreAppSettings = new NetCoreAppSettings(dotnetVersion, null, "Wasm");
+            var mainJsPath = Path.Combine(AppContext.BaseDirectory, "AppBundle", "test-main.js");
+
+            // Since WasmExecutor uses ShellExecute=false, it is not searching the path and needs a full path
+            // Maybe ShellExecute should be true? Otherwise not sure of a good way to get the v8 path in the integration test.
+            var v8Path = "/home/caaavik/.jsvu/bin/v8";
             var config = ManualConfig.CreateEmpty()
                 .AddLogger(logger)
-                .AddJob(Job.Dry.WithRuntime(WasmRuntime.Default));
+                .AddJob(Job.Dry
+                    .WithArguments([new MsBuildArgument($"/p:WasmMainJSPath={mainJsPath}")])
+                    .WithRuntime(new WasmRuntime(dotnetVersion, javaScriptEngine: v8Path, moniker: RuntimeMoniker.WasmNet70, javaScriptEngineArguments: "--expose_wasm --module"))
+                    .WithToolchain(WasmToolchain.From(netCoreAppSettings)))
+                .WithOption(ConfigOptions.GenerateMSBuildBinLog, true);
+
             CanExecute<WasmBenchmark>(config);
         }
 
@@ -29,7 +44,9 @@ namespace BenchmarkDotNet.IntegrationTests
             [Benchmark]
             public void Check()
             {
-                if (RuntimeInformation.GetCurrentRuntime() != WasmRuntime.Default)
+                // See if there are any other checks to run to make sure the benchmark is running in a WASM environment.
+
+                if (RuntimeInformation.GetCurrentRuntime().RuntimeMoniker != RuntimeMoniker.WasmNet70)
                 {
                     throw new Exception("Incorrect runtime detection");
                 }
