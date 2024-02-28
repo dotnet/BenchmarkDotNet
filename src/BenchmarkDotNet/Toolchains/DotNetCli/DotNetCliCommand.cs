88bb8241bec832f6f9febadabdde89c0efdcc188
Cameron Aavik <caaavik@microsoft.com>
2024-02-29 06:46:24 +1000

    Remove unused import

--- src/BenchmarkDotNet/Toolchains/DotNetCli/DotNetCliCommand.cs
+++ src/BenchmarkDotNet/Toolchains/DotNetCli/DotNetCliCommand.cs
@@ -2,7 +2,6 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.IO;
 using System.Text;
 using BenchmarkDotNet.Characteristics;
 using BenchmarkDotNet.Extensions;
