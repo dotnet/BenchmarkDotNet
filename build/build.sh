b5fc0595587eda8574351177ca35deb7ec79face
Andrey Akinshin <andrey.akinshin@gmail.com>
2024-03-06 16:37:13 +0100

    [build] Make build.sh more portable

--- build/build.sh
+++ build/build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Define variables
-SCRIPT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )
+PROJECT_ROOT=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )
 
 ###########################################################################
 # INSTALL .NET CORE CLI
@@ -12,17 +12,17 @@ export DOTNET_CLI_TELEMETRY_OPTOUT=1
 export DOTNET_SYSTEM_NET_HTTP_USESOCKETSHTTPHANDLER=0
 export DOTNET_ROLL_FORWARD_ON_NO_CANDIDATE_FX=2
 
-if [ ! -d "$SCRIPT_DIR/.dotnet" ]; then
-    mkdir "$SCRIPT_DIR/.dotnet"
-    curl -Lsfo "$SCRIPT_DIR/.dotnet/dotnet-install.sh" https://dot.net/v1/dotnet-install.sh
-    bash "$SCRIPT_DIR/.dotnet/dotnet-install.sh" --jsonfile ./build/sdk/global.json --install-dir .dotnet --no-path
+if [ ! -d "$PROJECT_ROOT/.dotnet" ]; then
+    mkdir "$PROJECT_ROOT/.dotnet"
+    curl -Lsfo "$PROJECT_ROOT/.dotnet/dotnet-install.sh" https://dot.net/v1/dotnet-install.sh
+    bash "$PROJECT_ROOT/.dotnet/dotnet-install.sh" --jsonfile ./build/sdk/global.json --install-dir .dotnet --no-path
 fi
 
-export PATH="$SCRIPT_DIR/.dotnet":$PATH
-export DOTNET_ROOT="$SCRIPT_DIR/.dotnet"
+export PATH="$PROJECT_ROOT/.dotnet":$PATH
+export DOTNET_ROOT="$PROJECT_ROOT/.dotnet"
 
 ###########################################################################
 # RUN BUILD SCRIPT
 ###########################################################################
 
-dotnet run --configuration Release --project ./build/BenchmarkDotNet.Build/BenchmarkDotNet.Build.csproj -- "$@"
+dotnet run --configuration Release --project "$PROJECT_ROOT/build/BenchmarkDotNet.Build/BenchmarkDotNet.Build.csproj" -- "$@"
