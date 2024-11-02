64c2f4f52dbe4e325069db55fdb30e78f73772e8
Leon van der Meer <64834803+leonvandermeer@users.noreply.github.com>
2024-11-02 23:19:40 +0100

    Apply suggestions from code review and add documentation

--- src/BenchmarkDotNet/Helpers/DisposeAtProcessTermination.cs
+++ src/BenchmarkDotNet/Helpers/DisposeAtProcessTermination.cs
@@ -1,18 +1,38 @@
 ﻿using System;
+using System.Runtime.InteropServices;
 
 namespace BenchmarkDotNet.Helpers
 {
     /// <summary>
-    /// Ensures that Dispose is called at termination of the Process.
+    /// Ensures that explicit Dispose is called at termination of the Process.
     /// </summary>
-    public class DisposeAtProcessTermination : IDisposable
+    /// <remarks>
+    /// <para>
+    /// This class exists to help in reverting system state where C#'s using statement does not
+    /// suffice. I.e. when Benchmark's process is aborted via Ctrl-C, Ctrl-Break or via click on the
+    /// X in the upper right of Window.
+    /// </para>
+    /// <para>
+    /// Usage: Derive your clas that changes system state of this class. Revert system state in
+    /// override of <see cref="Dispose"/> implementation.
+    /// Use your class in C#'s using statement, to ensure system state is reverted in normal situations.
+    /// This class ensures your override is also called at process 'abort'.
+    /// </para>
+    /// <para>
+    /// Note: This class is explicitly not responsible for cleanup of Native resources. Of course,
+    /// derived classes can cleanup their Native resources (usually managed via
+    /// <see cref="SafeHandle"/> derived classes), by delegating explicit Disposal to their
+    /// <see cref="IDisposable"/> fields.
+    /// </para>
+    /// </remarks>
+    public abstract class DisposeAtProcessTermination : IDisposable
     {
         public DisposeAtProcessTermination()
         {
             Console.CancelKeyPress += OnCancelKeyPress;
             AppDomain.CurrentDomain.ProcessExit += OnProcessExit;
-            // It does not make sense to include a finalizer. As we are subscribed to static events,
-            // it will never be called.
+            // It does not make sense to include a Finalizer. We do not manage any native resource and:
+            // as we are subscribed to static events, it would never be called.
         }
 
         /// <summary>
