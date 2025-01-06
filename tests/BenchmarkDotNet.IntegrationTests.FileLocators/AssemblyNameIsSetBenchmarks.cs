fcd632e4316f49e9fa8ad2f7f3af564de232edc9
Ian Qvist <qvistian@gmail.com>
2025-01-06 22:36:34 +0100

    Create integration test

---
+++ tests/BenchmarkDotNet.IntegrationTests.FileLocators/AssemblyNameIsSetBenchmarks.cs
@@ -0,0 +1,13 @@
+﻿using BenchmarkDotNet.Attributes;
+
+namespace BenchmarkDotNet.IntegrationTests.FileLocators
+{
+    public class AssemblyNameIsSetBenchmarks
+    {
+        [Benchmark]
+        public string Benchmark()
+        {
+            return "This will only run when a FileLocator is set due to <AssemblyName> in the csproj";
+        }
+    }
+}
