2128ec107c0dba370fe0059758a32f9227d2d78f
filzrev <103790468+filzrev@users.noreply.github.com>
2025-06-06 03:41:27 +0900

    chore: remove custom build configuration from intronuget benchmark

--- samples/BenchmarkDotNet.Samples/IntroNuGet.cs
+++ samples/BenchmarkDotNet.Samples/IntroNuGet.cs
@@ -18,7 +18,7 @@ namespace BenchmarkDotNet.Samples
         // Specify jobs with different versions of the same NuGet package to benchmark.
         // The NuGet versions referenced on these jobs must be greater or equal to the
         // same NuGet version referenced in this benchmark project.
-        // Example: This benchmark project references Newtonsoft.Json 9.0.1
+        // Example: This benchmark project references Newtonsoft.Json 13.0.1
         private class Config : ManualConfig
         {
             public Config()
@@ -34,7 +34,6 @@ namespace BenchmarkDotNet.Samples
                 foreach (var version in targetVersions)
                 {
                     AddJob(baseJob.WithNuGet("Newtonsoft.Json", version)
-                                  .WithCustomBuildConfiguration(version)
                                   .WithId(version));
                 }
             }
