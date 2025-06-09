b60ad9f093e4d5567adf4aeb4901d4a722001d0a
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2025-06-09 20:46:11 +0100

    use file scoped namespace

--- src/BenchmarkDotNet/Helpers/PowerShellLocator.cs
+++ src/BenchmarkDotNet/Helpers/PowerShellLocator.cs
@@ -5,78 +5,77 @@ using System.Runtime.Versioning;
 using System.Text.RegularExpressions;
 using BenchmarkDotNet.Detectors;
 
-namespace BenchmarkDotNet.Helpers
+namespace BenchmarkDotNet.Helpers;
+
+/// <summary>
+/// Locates PowerShell on a system, currently only supports on Windows.
+/// </summary>
+internal class PowerShellLocator
 {
-    /// <summary>
-    /// Locates PowerShell on a system, currently only supports on Windows.
-    /// </summary>
-    internal class PowerShellLocator
-    {
-        private static readonly string WindowsPowershellPath =
-            $"{Environment.SystemDirectory}{Path.DirectorySeparatorChar}WindowsPowerShell{Path.DirectorySeparatorChar}" +
-            $"v1.0{Path.DirectorySeparatorChar}powershell.exe";
+    private static readonly string WindowsPowershellPath =
+        $"{Environment.SystemDirectory}{Path.DirectorySeparatorChar}WindowsPowerShell{Path.DirectorySeparatorChar}" +
+        $"v1.0{Path.DirectorySeparatorChar}powershell.exe";
 
-        #if NET6_0_OR_GREATER
+#if NET6_0_OR_GREATER
         [SupportedOSPlatform("windows")]
-        #endif
-        internal static string? LocateOnWindows()
+#endif
+    internal static string? LocateOnWindows()
+    {
+        if (OsDetector.IsWindows() == false)
+            return null;
+
+        string powershellPath;
+
+        try
         {
-            if (OsDetector.IsWindows() == false)
-                return null;
+            string programFiles = Environment.Is64BitOperatingSystem
+                ? Environment.GetFolderPath(Environment.SpecialFolder.ProgramFiles)
+                : Environment.GetFolderPath(Environment.SpecialFolder.ProgramFilesX86);
 
-            string powershellPath;
+            string powershell7PlusPath = $"{programFiles}{Path.DirectorySeparatorChar}Powershell{Path.DirectorySeparatorChar}";
 
-            try
+            bool checkForPowershell7Plus = true;
+
+            if (Directory.Exists(powershell7PlusPath))
             {
-                string programFiles = Environment.Is64BitOperatingSystem
-                    ? Environment.GetFolderPath(Environment.SpecialFolder.ProgramFiles)
-                    : Environment.GetFolderPath(Environment.SpecialFolder.ProgramFilesX86);
+                string[] subDirectories = Directory.EnumerateDirectories(powershell7PlusPath, "*", SearchOption.AllDirectories)
+                    .ToArray();
 
-                string powershell7PlusPath = $"{programFiles}{Path.DirectorySeparatorChar}Powershell{Path.DirectorySeparatorChar}";
+                //Use the highest number string directory for PowerShell so that we get the newest major PowerShell version
+                // Example version directories are 6, 7, and in the future 8.
+                string? subDirectory = subDirectories.Where(x => Regex.IsMatch(x, "[0-9]"))
+                    .Select(x => x)
+                    .OrderByDescending(x => x)
+                    .FirstOrDefault();
 
-                bool checkForPowershell7Plus = true;
-
-                if (Directory.Exists(powershell7PlusPath))
+                if (subDirectory is not null)
                 {
-                    string[] subDirectories = Directory.EnumerateDirectories(powershell7PlusPath, "*", SearchOption.AllDirectories)
-                        .ToArray();
-
-                    //Use the highest number string directory for PowerShell so that we get the newest major PowerShell version
-                    // Example version directories are 6, 7, and in the future 8.
-                    string? subDirectory = subDirectories.Where(x => Regex.IsMatch(x, "[0-9]"))
-                        .Select(x => x)
-                        .OrderByDescending(x => x)
-                        .FirstOrDefault();
-
-                    if (subDirectory is not null)
-                    {
-                        powershell7PlusPath = $"{subDirectory}{Path.DirectorySeparatorChar}pwsh.exe";
-                    }
-                    else
-                    {
-                        checkForPowershell7Plus = false;
-                    }
-                }
-
-                // Optimistically, use Cross-platform new PowerShell when available but fallback to Windows PowerShell if not available.
-                if (checkForPowershell7Plus)
-                {
-                    powershellPath = File.Exists(powershell7PlusPath) ? powershell7PlusPath : WindowsPowershellPath;
+                    powershell7PlusPath = $"{subDirectory}{Path.DirectorySeparatorChar}pwsh.exe";
                 }
                 else
                 {
-                    powershellPath = WindowsPowershellPath;
+                    checkForPowershell7Plus = false;
                 }
-
-                if (File.Exists(powershellPath) == false)
-                    powershellPath = "PowerShell";
             }
-            catch
+
+            // Optimistically, use Cross-platform new PowerShell when available but fallback to Windows PowerShell if not available.
+            if (checkForPowershell7Plus)
             {
-                powershellPath = "PowerShell";
+                powershellPath = File.Exists(powershell7PlusPath) ? powershell7PlusPath : WindowsPowershellPath;
+            }
+            else
+            {
+                powershellPath = WindowsPowershellPath;
             }
 
-            return powershellPath;
+            if (File.Exists(powershellPath) == false)
+                powershellPath = "PowerShell";
         }
+        catch
+        {
+            powershellPath = "PowerShell";
+        }
+
+        return powershellPath;
     }
 }
