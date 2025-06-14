ba2c4e87000db776d292fce68f14dd2dde4437be
filzrev <103790468+filzrev@users.noreply.github.com>
2025-06-14 21:01:36 +0900

    fix: auto-generate jobid between benchmark runs

--- src/BenchmarkDotNet/Jobs/JobIdGenerator.cs
+++ src/BenchmarkDotNet/Jobs/JobIdGenerator.cs
@@ -10,12 +10,23 @@ namespace BenchmarkDotNet.Jobs
             string presentation = CharacteristicSetPresenter.Display.ToPresentation(job);
             if (presentation == "")
                 return "DefaultJob";
-            int seed = presentation.GetHashCode();
+            int seed = GetStableHashCode(presentation);
             var random = new Random(seed);
             string id = "";
             for (int i = 0; i < 6; i++)
-                id += (char) ('A' + random.Next(26));
+                id += (char)('A' + random.Next(26));
             return "Job-" + id;
+        }
+
+        // Compute string hash value with DJB2 algorithm.
+        private static int GetStableHashCode(string value)
+        {
+            uint hash = 5381;
+            foreach (char c in value)
+            {
+                hash = ((hash << 5) + hash) + c; // hash * 32 + hash + c
+            }
+            return unchecked((int)hash);
         }
     }
 }
