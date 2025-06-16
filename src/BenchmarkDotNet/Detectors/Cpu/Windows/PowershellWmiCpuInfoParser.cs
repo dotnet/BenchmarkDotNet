7d4210d65feb632cd77d7e00685a5b89322be18a
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2025-06-16 21:41:24 +0100

    Fix "Unknown Processor" on Windows if WMIC is not present (#2749)
    * allow MosCpuDetector to run on .NET 5+
    
    * Revert "allow MosCpuDetector to run on .NET 5+"
    
    This reverts commit 495855a002ddee957993525ec933fe2ab9dcee63.
    
    * add WmiLightCpu Detector
    
    * Fix WmicCpuDetector being chosen if not available
    
    * enable NativeAOT
    
    * simplify IsApplicable
    
    * Update spacing of SupportedOsPlatform attribute
    
    Co-authored-by: Tim Cassell <cassell.timothy@gmail.com>
    
    * Revert "Fix WmicCpuDetector being chosen if not available"
    
    This reverts commit 5972f77faf4673f38195d16865f334afc44bd986.
    
    * add WMIC deprecation remarks
    
    * remove WmiLight code
    
    * update WmiCpuInfoParser to return null if Processor Name isn't detected
    
    * remove WmiLightCpuDetector reference from WindowsCpuDetector
    
    * Update WmicCpuInfoParser.cs
    
    * check if wmicOutput is null or empty instead
    
    * add PowershellWmiCpuDetector (parser still not complete)
    
    * return null if there's no version of powershell installed
    
    * fix Powershell 7+ check
    
    Co-authored-by: Tim Cassell <cassell.timothy@gmail.com>
    
    * rework search statement given that regex isn't supported
    
    * add parser code
    
    * add PowershellWmiCpuDetector to WindowsCpuDetector
    
    * rename variable to lower case
    
    * improve checking of latest powershell 7+ version
    
    * use explicit typing
    
    * fix frequency addition issue
    
    * revert to how WMIC parser handles processor frequency
    
    * add string is null or empty check to WmiCpuDetector
    
    * invoke Powershell as "PowerShell" if the file isn't found
    
    * add nominal Frequency detection and improve max frequency detection
    
    * fix issue with detecting latest Powershell
    
    * update comment
    
    * Update PowershellWmiCpuDetector.cs
    
    * refactor Powershell locating code to PowershellLocator
    
    * simplify frequency checks
    
    * Create PowershellWmiParserTests.cs
    
    * fix null being returned when object is expected.
    
    * rename test
    
    * simplify max frequency check
    
    Co-authored-by: Tim Cassell <cassell.timothy@gmail.com>
    
    * use """ for string in parser test
    
    * Update PowershellWmiCpuInfoParserTests.cs
    
    * reduce indentation with """
    
    * remove unnecessary test info
    
    * move string null check to caller
    
    * add nominal frequency support for MosCpuDetector
    
    * Update src/BenchmarkDotNet/Detectors/Cpu/Windows/PowershellWmiCpuDetector.cs
    
    Co-authored-by: Tim Cassell <cassell.timothy@gmail.com>
    
    * remove nullability of parser
    
    Co-authored-by: Tim Cassell <cassell.timothy@gmail.com>
    
    * check if tempMaxFrequency > 0 before assignment
    
    Co-authored-by: Tim Cassell <cassell.timothy@gmail.com>
    
    * remove nullability of WmicCpuInfoParser
    
    * use file scoped namespace
    
    * use double instead of int
    
    * add null check to LinuxCpuDetector
    
    * change nullability of LinuxCpuParser
    
    * update LinuxCpuInfoParser nominal and max frequency detection
    
    * Update LinuxCpuInfoParser.cs
    
    * fix nullability check
    
    * simplify nominal frequency comparison
    
    Co-authored-by: Tim Cassell <cassell.timothy@gmail.com>
    
    * add null check to macOS
    
    * don't accept null string from detector
    
    * fix LinuxCpuInfo parser test issues and improve robustness of Linux Cpu checks
    
    * fix Powershell Wmi Parser parsing issues
    
    * fix .net framework cpu parsing issue
    
    * do implicit conversion to double from uint
    
    ---------
    
    Co-authored-by: Tim Cassell <cassell.timothy@gmail.com>

--- src/BenchmarkDotNet/Detectors/Cpu/Windows/PowershellWmiCpuInfoParser.cs
+++ src/BenchmarkDotNet/Detectors/Cpu/Windows/PowershellWmiCpuInfoParser.cs
@@ -7,23 +7,20 @@ using Perfolizer.Models;
 
 namespace BenchmarkDotNet.Detectors.Cpu.Windows;
 
-internal static class WmicCpuInfoParser
+internal static class PowershellWmiCpuInfoParser
 {
-    /// <summary>
-    /// Parses wmic output and returns <see cref="CpuInfo"/>
-    /// </summary>
-    /// <param name="wmicOutput">Output of `wmic cpu get Name, NumberOfCores, NumberOfLogicalProcessors /Format:List`</param>
-    internal static CpuInfo Parse(string wmicOutput)
+    internal static CpuInfo Parse(string powershellWmiOutput)
     {
         HashSet<string> processorModelNames = new HashSet<string>();
+
         int physicalCoreCount = 0;
         int logicalCoreCount = 0;
-        int processorsCount = 0;
+        int processorCount = 0;
         double maxFrequency = 0.0;
         double nominalFrequency = 0.0;
 
-        List<Dictionary<string, string>> processors = SectionsHelper.ParseSections(wmicOutput, '=');
-        foreach (var processor in processors)
+        List<Dictionary<string, string>> processors = SectionsHelper.ParseSectionsForPowershellWmi(powershellWmiOutput, ':');
+        foreach (Dictionary<string, string> processor in processors)
         {
             if (processor.TryGetValue(WmicCpuInfoKeyNames.NumberOfCores, out string numberOfCoresValue) &&
                 int.TryParse(numberOfCoresValue, out int numberOfCores) &&
@@ -38,29 +35,29 @@ internal static class WmicCpuInfoParser
             if (processor.TryGetValue(WmicCpuInfoKeyNames.Name, out string name))
             {
                 processorModelNames.Add(name);
-                processorsCount++;
+                processorCount++;
             }
 
             if (processor.TryGetValue(WmicCpuInfoKeyNames.MaxClockSpeed, out string frequencyValue)
                 && double.TryParse(frequencyValue, out double frequency)
                 && frequency > 0)
             {
-                nominalFrequency = nominalFrequency == 0 ? frequency : Math.Min(nominalFrequency, frequency);
-                maxFrequency = Math.Max(maxFrequency, frequency);
+               nominalFrequency = nominalFrequency == 0 ? frequency : Math.Min(nominalFrequency, frequency);
+               maxFrequency = Math.Max(maxFrequency, frequency);
             }
         }
 
         string? processorName = processorModelNames.Count > 0 ? string.Join(", ", processorModelNames) : null;
-        Frequency? maxFrequencyActual = maxFrequency > 0 && processorsCount > 0
-            ? Frequency.FromMHz(maxFrequency)
-            : null;
+        Frequency? maxFrequencyActual = maxFrequency > 0 && processorCount > 0
+            ? Frequency.FromMHz(maxFrequency) : null;
 
-        Frequency? nominalFrequencyActual = nominalFrequency > 0 && processorsCount > 0 ? Frequency.FromMHz(nominalFrequency) : null;
+        Frequency? nominalFrequencyActual = nominalFrequency > 0 && processorCount > 0 ?
+            Frequency.FromMHz(nominalFrequency) : null;
 
         return new CpuInfo
         {
             ProcessorName = processorName,
-            PhysicalProcessorCount = processorsCount > 0 ? processorsCount : null,
+            PhysicalProcessorCount = processorCount > 0 ? processorCount : null,
             PhysicalCoreCount = physicalCoreCount > 0 ? physicalCoreCount : null,
             LogicalCoreCount = logicalCoreCount > 0 ? logicalCoreCount : null,
             NominalFrequencyHz = nominalFrequencyActual?.Hertz.RoundToLong(),
---
+++ src/BenchmarkDotNet/Helpers/PowerShellLocator.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Runtime.Versioning;
+using System.Text.RegularExpressions;
+using BenchmarkDotNet.Detectors;
+
+namespace BenchmarkDotNet.Helpers;
+
+/// <summary>
+/// Locates PowerShell on a system, currently only supports on Windows.
+/// </summary>
+internal class PowerShellLocator
+{
+    private static readonly string WindowsPowershellPath =
+        $"{Environment.SystemDirectory}{Path.DirectorySeparatorChar}WindowsPowerShell{Path.DirectorySeparatorChar}" +
+        $"v1.0{Path.DirectorySeparatorChar}powershell.exe";
+
+#if NET6_0_OR_GREATER
+        [SupportedOSPlatform("windows")]
+#endif
+    internal static string? LocateOnWindows()
+    {
+        if (OsDetector.IsWindows() == false)
+            return null;
+
+        string powershellPath;
+
+        try
+        {
+            string programFiles = Environment.Is64BitOperatingSystem
+                ? Environment.GetFolderPath(Environment.SpecialFolder.ProgramFiles)
+                : Environment.GetFolderPath(Environment.SpecialFolder.ProgramFilesX86);
+
+            string powershell7PlusPath = $"{programFiles}{Path.DirectorySeparatorChar}Powershell{Path.DirectorySeparatorChar}";
+
+            bool checkForPowershell7Plus = true;
+
+            if (Directory.Exists(powershell7PlusPath))
+            {
+                string[] subDirectories = Directory.EnumerateDirectories(powershell7PlusPath, "*", SearchOption.AllDirectories)
+                    .ToArray();
+
+                //Use the highest number string directory for PowerShell so that we get the newest major PowerShell version
+                // Example version directories are 6, 7, and in the future 8.
+                string? subDirectory = subDirectories.Where(x => Regex.IsMatch(x, "[0-9]"))
+                    .Select(x => x)
+                    .OrderByDescending(x => x)
+                    .FirstOrDefault();
+
+                if (subDirectory is not null)
+                {
+                    powershell7PlusPath = $"{subDirectory}{Path.DirectorySeparatorChar}pwsh.exe";
+                }
+                else
+                {
+                    checkForPowershell7Plus = false;
+                }
+            }
+
+            // Optimistically, use Cross-platform new PowerShell when available but fallback to Windows PowerShell if not available.
+            if (checkForPowershell7Plus)
+            {
+                powershellPath = File.Exists(powershell7PlusPath) ? powershell7PlusPath : WindowsPowershellPath;
+            }
+            else
+            {
+                powershellPath = WindowsPowershellPath;
+            }
+
+            if (File.Exists(powershellPath) == false)
+                powershellPath = "PowerShell";
+        }
+        catch
+        {
+            powershellPath = "PowerShell";
+        }
+
+        return powershellPath;
+    }
+}
