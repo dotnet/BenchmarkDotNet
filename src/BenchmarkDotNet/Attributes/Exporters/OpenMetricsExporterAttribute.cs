37e75fee41893f959ea7440fdf921ea9dbdbd263
Ilias Tsakiridis <i.tsakiridis@kaizengaming.com>
2025-07-02 23:23:14 +0300

    feat: add OpenMetrics exporter support and update related tests

---
+++ src/BenchmarkDotNet/Attributes/Exporters/OpenMetricsExporterAttribute.cs
@@ -0,0 +1,14 @@
+﻿using BenchmarkDotNet.Exporters;
+using BenchmarkDotNet.Exporters.OpenMetrics;
+using JetBrains.Annotations;
+
+namespace BenchmarkDotNet.Attributes
+{
+    [PublicAPI]
+    public class OpenMetricsExporterAttribute : ExporterConfigBaseAttribute
+    {
+        public OpenMetricsExporterAttribute() : base(OpenMetricsExporter.Default)
+        {
+        }
+    }
+}
