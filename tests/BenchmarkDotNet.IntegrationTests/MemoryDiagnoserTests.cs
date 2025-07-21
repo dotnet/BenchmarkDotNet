10403e917689da687f30e02d7d89c58bb800a7a3
filzrev <103790468+filzrev@users.noreply.github.com>
2025-07-22 06:43:57 +0900

    chore: fix flaky memorydiagnoser tests on macos

--- tests/BenchmarkDotNet.IntegrationTests/MemoryDiagnoserTests.cs
+++ tests/BenchmarkDotNet.IntegrationTests/MemoryDiagnoserTests.cs
@@ -244,11 +244,14 @@ namespace BenchmarkDotNet.IntegrationTests
         {
             long objectAllocationOverhead = IntPtr.Size * 2; // pointer to method table + object header word
             long arraySizeOverhead = IntPtr.Size; // array length
+            int warmupCount = OsDetector.IsMacOS()
+                ? 5  // Workaround setting for macos. https://github.com/dotnet/BenchmarkDotNet/issues/2779
+                : 0; // Other OS don't need warmup
 
             AssertAllocations(toolchain, typeof(TimeConsuming), new Dictionary<string, long>
             {
                 { nameof(TimeConsuming.SixtyFourBytesArray), 64 + objectAllocationOverhead + arraySizeOverhead }
-            }, warmupCount: 2);
+            }, warmupCount: warmupCount);
         }
 
         public class MultiThreadedAllocation
