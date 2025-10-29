796b0a97135cd8862e5b9a5ad16303c389c17ddb
Tim Cassell <cassell.timothy@gmail.com>
2025-10-29 18:16:47 -0400

    Use file-scoped namespace.

--- src/BenchmarkDotNet/Diagnosers/InProcessDiagnoserRouter.cs
+++ src/BenchmarkDotNet/Diagnosers/InProcessDiagnoserRouter.cs
@@ -3,23 +3,22 @@ using BenchmarkDotNet.Helpers;
 using JetBrains.Annotations;
 using System.ComponentModel;
 
-namespace BenchmarkDotNet.Diagnosers
-{
-    [UsedImplicitly]
-    [EditorBrowsable(EditorBrowsableState.Never)]
-    public struct InProcessDiagnoserRouter
-    {
-        public IInProcessDiagnoserHandler handler;
-        public int index;
-        public RunMode runMode;
+namespace BenchmarkDotNet.Diagnosers;
 
-        public readonly string ToSourceCode()
-            => $$"""
-                new {{typeof(InProcessDiagnoserRouter).GetCorrectCSharpTypeName()}}() {
-                    {{nameof(handler)}} = {{handler.ToSourceCode()}},
-                    {{nameof(index)}} = {{index}},
-                    {{nameof(runMode)}} = {{SourceCodeHelper.ToSourceCode(runMode)}}
-                }
-                """;
-    }
+[UsedImplicitly]
+[EditorBrowsable(EditorBrowsableState.Never)]
+public struct InProcessDiagnoserRouter
+{
+    public IInProcessDiagnoserHandler handler;
+    public int index;
+    public RunMode runMode;
+
+    public readonly string ToSourceCode()
+        => $$"""
+            new {{typeof(InProcessDiagnoserRouter).GetCorrectCSharpTypeName()}}() {
+                {{nameof(handler)}} = {{handler.ToSourceCode()}},
+                {{nameof(index)}} = {{index}},
+                {{nameof(runMode)}} = {{SourceCodeHelper.ToSourceCode(runMode)}}
+            }
+            """;
 }
