4be04d1cc1f8b820c3da0c47ea6c2433d206dbdb
Tim Cassell <cassell.timothy@gmail.com>
2025-11-29 03:01:18 -0500

    Fix compile errors after rebase.

--- src/BenchmarkDotNet/Running/BenchmarkRunInfo.cs
+++ src/BenchmarkDotNet/Running/BenchmarkRunInfo.cs
@@ -6,6 +6,9 @@ namespace BenchmarkDotNet.Running
 {
     public class BenchmarkRunInfo(BenchmarkCase[] benchmarksCase, Type type, ImmutableConfig config, bool containsBenchmarkDeclarations, CompositeInProcessDiagnoser compositeInProcessDiagnoser) : IDisposable
     {
+        public BenchmarkRunInfo(BenchmarkCase[] benchmarksCases, Type type, ImmutableConfig config, CompositeInProcessDiagnoser compositeInProcessDiagnoser)
+            : this(benchmarksCases, type, config, benchmarksCases.Length > 0, compositeInProcessDiagnoser) { }
+
         public void Dispose()
         {
             foreach (var benchmarkCase in BenchmarksCases)
