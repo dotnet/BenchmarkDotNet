ce085a1e99ca47b649414d67c09682c9c57981b9
Carl Furtado <carlzfurtado@gmail.com>
2025-12-01 08:43:00 -0500

    Implement param equality comparison directly

--- src/BenchmarkDotNet/Parameters/ParameterEqualityComparer.cs
+++ src/BenchmarkDotNet/Parameters/ParameterEqualityComparer.cs
@@ -1,5 +1,8 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
+using System.Linq;
 
 namespace BenchmarkDotNet.Parameters
 {
@@ -7,14 +10,57 @@ namespace BenchmarkDotNet.Parameters
     {
         public static readonly ParameterEqualityComparer Instance = new ParameterEqualityComparer();
 
-        public bool Equals(ParameterInstances? x, ParameterInstances? y)
+        public bool Equals(ParameterInstances x, ParameterInstances y)
         {
-            return ParameterComparer.Instance.Compare(x, y) == 0;
+            if (x == null && y == null) return true;
+            if (x != null && y == null) return false;
+            if (x == null) return false;
+
+            for (int i = 0; i < Math.Min(x.Count, y.Count); i++)
+            {
+                var isEqual = ValuesEqual(x[i]?.Value, y[i]?.Value);
+
+                if (!isEqual) return false;
+            }
+
+            return true;
         }
 
-        public int GetHashCode([DisallowNull] ParameterInstances obj)
+        private bool ValuesEqual(object x, object y)
         {
-            return obj.ValueInfo.GetHashCode();
+            if (x == null && y == null) return true;
+
+            // Detect IComparable implementations.
+            // This works for all primitive types in addition to user types that implement IComparable.
+            if (x != null && y != null && x.GetType() == y.GetType())
+            {
+                if (x is IEnumerable xEnumerable  && y is IEnumerable yEnumerable) // collection equality support
+                {
+                    if (x is Array xArr && y is Array yArr) // check rank here for arrays because their values will get compared when flattened
+                    {
+                        if (xArr.Rank != yArr.Rank) return false;
+                        if (xArr.Length != yArr.Length) return false;
+                    }
+
+                    var xFlat = xEnumerable.OfType<object>().ToArray();
+                    var yFlat = yEnumerable.OfType<object>().ToArray();
+
+                    return StructuralComparisons.StructuralEqualityComparer.Equals(xFlat, yFlat);
+                }
+                else
+                {
+                    // The user should define a value-based Equals check
+                    return x.Equals(y);
+                }
+            }
+
+            // The objects are of different types or one is null, they cannot be equal
+            return false;
+        }
+
+        public int GetHashCode(ParameterInstances obj)
+        {
+            return obj?.ValueInfo.GetHashCode() ?? 0;
         }
     }
 }
