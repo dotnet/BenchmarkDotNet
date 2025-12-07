39817d30c9a13cba39724c105e8e2813f84c30de
Tim Cassell <cassell.timothy@gmail.com>
2025-12-07 03:02:32 -0500

    Validate NoInlining in the benchmark process.

---
+++ src/BenchmarkDotNet/Validators/BenchmarkProcessValidator.cs
@@ -0,0 +1,33 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Environments;
+using BenchmarkDotNet.Extensions;
+using BenchmarkDotNet.Jobs;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace BenchmarkDotNet.Validators
+{
+    public static class BenchmarkProcessValidator
+    {
+        public static IEnumerable<ValidationError> Validate(Job job, object benchmarkInstance)
+        {
+            foreach (var validationError in BenchmarkEnvironmentInfo.Validate(job))
+            {
+                yield return validationError;
+            }
+
+            var inlineableBenchmarks = benchmarkInstance.GetType().BaseType
+                .GetMethods(BindingFlags.Static | BindingFlags.Instance | BindingFlags.Public)
+                .Where(method => method.HasAttribute<BenchmarkAttribute>() && !method.MethodImplementationFlags.HasFlag(MethodImplAttributes.NoInlining));
+            foreach (var benchmark in inlineableBenchmarks)
+            {
+                yield return new ValidationError(
+                    true,
+                    $"Benchmark method `{benchmark.Name}` does not have MethodImplOptions.NoInlining flag set." +
+                    $" You may need to rebuild your project, or apply it manually if you are not using MSBuild to build your project."
+                );
+            }
+        }
+    }
+}
