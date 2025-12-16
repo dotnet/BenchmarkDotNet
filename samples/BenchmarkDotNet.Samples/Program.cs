848196a73e902af052252f4f8d8915b59a414f6f
filzrev <103790468+filzrev@users.noreply.github.com>
2025-12-17 03:32:01 +0900

    chore: Update BenchmarkDotNet.Samples project (#2898)
    * chore: update samples project
    
    * chore-add-support-DebugBuildConfig
    
    * chore: fix ThreadingDiagnoser error when running benchmark on .NET Framework
    
    * chore: fix wrong #if directive
    
    * chore: reflect review comments and cleanup codes

--- samples/BenchmarkDotNet.Samples/Program.cs
+++ samples/BenchmarkDotNet.Samples/Program.cs
@@ -1,9 +1,58 @@
-﻿using BenchmarkDotNet.Running;
+﻿using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Engines;
+using BenchmarkDotNet.Exporters;
+using BenchmarkDotNet.Extensions;
+using BenchmarkDotNet.Loggers;
+using BenchmarkDotNet.Running;
+using System;
+using System.Linq;
 
-namespace BenchmarkDotNet.Samples
+namespace BenchmarkDotNet.Samples;
+
+public class Program
 {
-    public class Program
+    public static int Main(string[] args)
     {
-        public static void Main(string[] args) => BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args);
+#if DEBUG
+        ConsoleLogger.Default.WriteLineWarning("Benchmark is executed with DEBUG configuration.");
+        ConsoleLogger.Default.WriteLine();
+#endif
+
+        if (args.Length != 0)
+        {
+            ConsoleLogger.Default.WriteLine($"Start benchmarks with args: {string.Join(" ", args)}");
+            ConsoleLogger.Default.WriteLine();
+        }
+
+        IConfig? config = GetConfig(ref args);
+
+        var summaries = BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly)
+                                         .Run(args, config)
+                                         .ToArray();
+
+        if (summaries.HasError())
+            return 1;
+
+        return 0;
+    }
+
+    private static IConfig? GetConfig(ref string[] args)
+    {
+#if !DEBUG
+        return null; // `DefaultConfig.Instance` is used.
+#else
+        bool isInProcess = args.Contains("--inProcess");
+        if (isInProcess)
+            args = args.Where(x => x != "--inProcess").ToArray();
+
+        DebugConfig config = isInProcess
+            ? new DebugInProcessConfig()
+            : new DebugBuildConfig();
+
+        return config.AddAnalyser(DefaultConfig.Instance.GetAnalysers().ToArray())
+                     .AddExporter(MarkdownExporter.Default)
+                     .AddValidator(DefaultConfig.Instance.GetValidators().ToArray())
+                     .WithArtifactsPath(DefaultConfig.Instance.ArtifactsPath);
+#endif
     }
 }
