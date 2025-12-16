3db7350dcc11819b30336252ba6a82e765e71479
Jonathan Peppers <jonathan.peppers@microsoft.com>
2025-12-16 12:32:24 -0600

    Port Xamarin samples to .NET MAUI (#2929)
    * Port Xamarin samples to .NET MAUI
    
    Fixes: https://github.com/dotnet/BenchmarkDotNet/issues/2910
    
    Copilot was able to automate much of this. I got "nerd sniped" to fix
    some of the issues I found along the way.
    
    There are some fixes required for iOS and Android to work, I will send
    in separate PRs.
    
    * `dotnet sln migrate BenchmarkDotNet.Maui.sln`

---
+++ samples/BenchmarkDotNet.Samples.Maui/App.xaml.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace BenchmarkDotNet.Samples.Maui;
+
+public partial class App : Application
+{
+    public App()
+    {
+        InitializeComponent();
+    }
+
+    protected override Window CreateWindow(IActivationState? activationState)
+    {
+        return new Window(new AppShell());
+    }
+}
---
+++ samples/BenchmarkDotNet.Samples.Maui/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace BenchmarkDotNet.Samples.Maui;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+    }
+}
