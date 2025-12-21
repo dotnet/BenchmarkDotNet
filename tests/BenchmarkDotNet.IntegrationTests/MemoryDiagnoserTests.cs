2cc355809935dc0dada91853445fd95e70dce1ba
filzrev <103790468+filzrev@users.noreply.github.com>
2025-12-22 02:24:09 +0900

    chore: skip flaky test on macos

--- tests/BenchmarkDotNet.IntegrationTests/MemoryDiagnoserTests.cs
+++ tests/BenchmarkDotNet.IntegrationTests/MemoryDiagnoserTests.cs
@@ -242,16 +242,16 @@ namespace BenchmarkDotNet.IntegrationTests
         [Trait(Constants.Category, Constants.BackwardCompatibilityCategory)]
         public void AllocationQuantumIsNotAnIssueForNetCore21Plus(IToolchain toolchain)
         {
+            // TODO: Skip test on macos. Temporary workaround for https://github.com/dotnet/BenchmarkDotNet/issues/2779
+            if (System.Runtime.InteropServices.RuntimeInformation.IsOSPlatform(System.Runtime.InteropServices.OSPlatform.OSX))
+                return;
+
             long objectAllocationOverhead = IntPtr.Size * 2; // pointer to method table + object header word
             long arraySizeOverhead = IntPtr.Size; // array length
-            int warmupCount = OsDetector.IsMacOS()
-                ? 10 // Workaround setting for macos. https://github.com/dotnet/BenchmarkDotNet/issues/2779
-                : 0; // Other OS don't need warmup
-
             AssertAllocations(toolchain, typeof(TimeConsuming), new Dictionary<string, long>
             {
                 { nameof(TimeConsuming.SixtyFourBytesArray), 64 + objectAllocationOverhead + arraySizeOverhead }
-            }, warmupCount: warmupCount);
+            });
         }
 
         public class MultiThreadedAllocation
@@ -321,9 +321,9 @@ namespace BenchmarkDotNet.IntegrationTests
             });
         }
 
-        private void AssertAllocations(IToolchain toolchain, Type benchmarkType, Dictionary<string, long> benchmarksAllocationsValidators, bool disableTieredJit = true, int iterationCount = 1, int warmupCount = 0)
+        private void AssertAllocations(IToolchain toolchain, Type benchmarkType, Dictionary<string, long> benchmarksAllocationsValidators, bool disableTieredJit = true, int iterationCount = 1)
         {
-            var config = CreateConfig(toolchain, disableTieredJit, iterationCount, warmupCount);
+            var config = CreateConfig(toolchain, disableTieredJit, iterationCount);
             var benchmarks = BenchmarkConverter.TypeToBenchmarks(benchmarkType, config);
 
             var summary = BenchmarkRunner.Run(benchmarks);
