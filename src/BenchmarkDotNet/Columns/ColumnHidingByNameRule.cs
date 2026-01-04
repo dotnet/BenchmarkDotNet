5c1db389b64a70bc139b01dab0bc18f03eef7434
filzrev <103790468+filzrev@users.noreply.github.com>
2026-01-05 04:49:06 +0900

    fix: duplicated config items issue

--- src/BenchmarkDotNet/Columns/ColumnHidingByNameRule.cs
+++ src/BenchmarkDotNet/Columns/ColumnHidingByNameRule.cs
@@ -1,14 +1,31 @@
 ﻿using JetBrains.Annotations;
+using System;
 
 namespace BenchmarkDotNet.Columns
 {
     [PublicAPI]
-    public class ColumnHidingByNameRule: IColumnHidingRule
+    public sealed class ColumnHidingByNameRule : IColumnHidingRule, IEquatable<ColumnHidingByNameRule>
     {
         public string Name { get; }
 
         public ColumnHidingByNameRule(string name) => Name = name;
 
         public bool NeedToHide(IColumn column) => column.ColumnName == Name;
+
+        public bool Equals(ColumnHidingByNameRule? other)
+        {
+            if (ReferenceEquals(null, other))
+                return false;
+            if (ReferenceEquals(this, other))
+                return true;
+
+            return Name == other.Name;
+        }
+
+        public override bool Equals(object? obj)
+            => Equals(obj as ColumnHidingByNameRule);
+
+        public override int GetHashCode()
+            => HashCode.Combine(Name);
     }
 }
