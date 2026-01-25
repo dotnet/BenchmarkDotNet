4094e330f3179d2b692fabcb31160dc23c40b8c6
Tim Cassell <cassell.timothy@gmail.com>
2026-01-25 00:00:10 -0500

    Add thread-safety and wait-blocking to BenchmarkSynchronizationContext.

--- src/BenchmarkDotNet/Engines/BenchmarkSynchronizationContext.cs
+++ src/BenchmarkDotNet/Engines/BenchmarkSynchronizationContext.cs
@@ -10,12 +10,40 @@ namespace BenchmarkDotNet.Engines;
 // Used to ensure async continuations are posted back to the same thread that the benchmark process was started on.
 [UsedImplicitly]
 [EditorBrowsable(EditorBrowsableState.Never)]
-public sealed class BenchmarkSynchronizationContext : SynchronizationContext, IDisposable
+public readonly ref struct BenchmarkSynchronizationContext : IDisposable
 {
-    private readonly SynchronizationContext previousContext;
-    private readonly Queue<(SendOrPostCallback d, object? state)> queue = new();
+    private readonly BenchmarkDotNetSynchronizationContext context;
 
-    private BenchmarkSynchronizationContext(SynchronizationContext previousContext)
+    private BenchmarkSynchronizationContext(BenchmarkDotNetSynchronizationContext context)
+    {
+        this.context = context;
+    }
+
+    public static BenchmarkSynchronizationContext CreateAndSetCurrent()
+    {
+        var context = new BenchmarkDotNetSynchronizationContext(SynchronizationContext.Current);
+        SynchronizationContext.SetSynchronizationContext(context);
+        return new(context);
+    }
+
+    public void Dispose()
+        => context.Dispose();
+
+    public void ExecuteUntilComplete(ValueTask valueTask)
+        => context.ExecuteUntilComplete(valueTask);
+
+    public T ExecuteUntilComplete<T>(ValueTask<T> valueTask)
+        => context.ExecuteUntilComplete(valueTask);
+}
+
+internal sealed class BenchmarkDotNetSynchronizationContext : SynchronizationContext
+{
+    private readonly SynchronizationContext? previousContext;
+    private readonly Queue<(SendOrPostCallback d, object? state)> queue = new();
+    private bool isDisposed;
+    volatile private bool isCompleted;
+
+    internal BenchmarkDotNetSynchronizationContext(SynchronizationContext? previousContext)
     {
         this.previousContext = previousContext;
     }
@@ -24,53 +52,133 @@ public sealed class BenchmarkSynchronizationContext : SynchronizationContext, ID
         => this;
 
     public override void Post(SendOrPostCallback d, object? state)
-        => queue.Enqueue((d ?? throw new ArgumentNullException(nameof(d)), state));
-
-    public static BenchmarkSynchronizationContext CreateAndSetCurrent()
     {
-        var context = new BenchmarkSynchronizationContext(Current);
-        SetSynchronizationContext(context);
-        return context;
-    }
+        if (d is null) throw new ArgumentNullException(nameof(d));
 
-    public void Dispose()
-        => SetSynchronizationContext(previousContext);
-
-    public void ExecuteUntilComplete(ValueTask valueTask)
-    {
-        var spinner = new SpinWait();
-        while (!valueTask.IsCompleted)
+        lock (queue)
         {
-            DoSpin(ref spinner);
+            ThrowIfDisposed();
+
+            queue.Enqueue((d, state));
+            Monitor.Pulse(queue);
         }
-        valueTask.GetAwaiter().GetResult();
     }
 
-    public T ExecuteUntilComplete<T>(ValueTask<T> valueTask)
+    private void ThrowIfDisposed()
     {
-        var spinner = new SpinWait();
-        while (!valueTask.IsCompleted)
+        if (isDisposed) throw new ObjectDisposedException(nameof(BenchmarkDotNetSynchronizationContext));
+    }
+
+    internal void Dispose()
+    {
+        lock (queue)
         {
-            DoSpin(ref spinner);
+            ThrowIfDisposed();
+            isDisposed = true;
+
+            // Flush any remaining posted callbacks.
+            while (TryDequeue(out var callbackAndState))
+            {
+                callbackAndState.d(callbackAndState.state);
+            }
         }
-        return valueTask.GetAwaiter().GetResult();
+        SetSynchronizationContext(previousContext);
     }
 
-    private void DoSpin(ref SpinWait spinner)
+    internal void ExecuteUntilComplete(ValueTask valueTask)
     {
-        if (queue.Count <= 0)
+        ThrowIfDisposed();
+
+        var awaiter = valueTask.GetAwaiter();
+        if (valueTask.IsCompleted)
         {
-            spinner.SpinOnce();
+            awaiter.GetResult();
             return;
         }
 
-        do
+        isCompleted = false;
+        awaiter.UnsafeOnCompleted(OnCompleted);
+        ExecuteUntilComplete();
+        awaiter.GetResult();
+    }
+
+    internal T ExecuteUntilComplete<T>(ValueTask<T> valueTask)
+    {
+        ThrowIfDisposed();
+
+        var awaiter = valueTask.GetAwaiter();
+        if (valueTask.IsCompleted)
         {
-            var (d, state) = queue.Dequeue();
-            d(state);
+            return awaiter.GetResult();
         }
-        while (queue.Count > 0);
-        // Reset spinner after any posted callback is executed.
-        spinner = new();
+
+        isCompleted = false;
+        awaiter.UnsafeOnCompleted(OnCompleted);
+        ExecuteUntilComplete();
+        return awaiter.GetResult();
+    }
+
+    private void OnCompleted()
+    {
+        isCompleted = true;
+        lock (queue)
+        {
+            Monitor.Pulse(queue);
+        }
+    }
+
+    private void ExecuteUntilComplete()
+    {
+        var spinner = new SpinWait();
+        while (true)
+        {
+            if (TryDequeue(out var callbackAndState))
+            {
+                do
+                {
+                    callbackAndState.d(callbackAndState.state);
+                }
+                while (TryDequeue(out callbackAndState));
+                // Reset spinner after any posted callback is executed.
+                spinner = new();
+            }
+
+            if (isCompleted)
+            {
+                return;
+            }
+
+            if (spinner.NextSpinWillYield)
+            {
+                // Yield the thread and wait for completion or for a posted callback.
+                lock (queue)
+                {
+                    Monitor.Wait(queue);
+                }
+                // Reset the spinner.
+                spinner = new();
+                continue;
+            }
+
+            spinner.SpinOnce();
+        }
+    }
+
+    private bool TryDequeue(out (SendOrPostCallback d, object? state) callbackAndState)
+    {
+        lock (queue)
+        {
+#if NETSTANDARD2_0
+            if (queue.Count > 0)
+            {
+                callbackAndState = queue.Dequeue();
+                return true;
+            }
+            callbackAndState = default;
+            return false;
+#else
+            return queue.TryDequeue(out callbackAndState);
+#endif
+        }
     }
 }
