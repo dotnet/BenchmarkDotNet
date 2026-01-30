a731881a779b55330890fabff6fee374ae639890
filzrev <103790468+filzrev@users.noreply.github.com>
2026-01-30 20:13:02 +0900

    chore: remove dead code

--- build/build.ps1
+++ build/build.ps1
@@ -6,22 +6,6 @@ $DotNetInstallerUri = 'https://dot.net/v1/dotnet-install.ps1';
 $BuildPath = Split-Path $MyInvocation.MyCommand.Path -Parent
 $PSScriptRoot = Split-Path $PSScriptRoot -Parent
 
-if ($PSVersionTable.PSEdition -ne 'Core') {
-    # Attempt to set highest encryption available for SecurityProtocol.
-    # PowerShell will not set this by default (until maybe .NET 4.6.x). This
-    # will typically produce a message for PowerShell v2 (just an info
-    # message though)
-    try {
-        # Set TLS 1.2 (3072), then TLS 1.1 (768), then TLS 1.0 (192), finally SSL 3.0 (48)
-        # Use integers because the enumeration values for TLS 1.2 and TLS 1.1 won't
-        # exist in .NET 4.0, even though they are addressable if .NET 4.5+ is
-        # installed (.NET 4.5 is an in-place upgrade).
-        [System.Net.ServicePointManager]::SecurityProtocol = 3072 -bor 768 -bor 192 -bor 48
-      } catch {
-        Write-Output 'Unable to set PowerShell to use TLS 1.2 and TLS 1.1 due to old .NET Framework installed. If you see underlying connection closed or trust errors, you may need to upgrade to .NET Framework 4.5+ and PowerShell v3'
-      }
-}
-
 ###########################################################################
 # INSTALL .NET CORE CLI
 ###########################################################################
