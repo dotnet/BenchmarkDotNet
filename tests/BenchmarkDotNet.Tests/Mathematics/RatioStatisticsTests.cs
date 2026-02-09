84fcd877a8049c90b98e37a71a09909b9482e713
Azamat Suleimanov <a2amat@yandex.ru>
2026-02-10 00:08:10 +0100

    fix summary ratio and ratioSD values for baseline #2648 (#2988)
    * fix summary ratio and ratioSD values for baseline #2648
    
    * adjust *.verified.txt
    
    ---------
    
    Co-authored-by: Azamat Suleimanov <>

---
+++ tests/BenchmarkDotNet.Tests/Mathematics/RatioStatisticsTests.cs
@@ -0,0 +1,18 @@
+﻿using BenchmarkDotNet.Mathematics;
+using Xunit;
+
+namespace BenchmarkDotNet.Tests.Mathematics;
+
+public class RatioStatisticsTests
+{
+    [Fact]
+    public void SelfDivisionTest()
+    {
+        var stat = new Statistics(100, 200, 300);
+
+        var divided = new RatioStatistics(stat, stat);
+
+        Assert.Equal(1, divided.Mean);
+        Assert.Equal(0, divided.StandardDeviation);
+    }
+}
