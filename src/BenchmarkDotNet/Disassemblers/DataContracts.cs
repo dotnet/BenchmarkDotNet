4fcdba75a3340a9c4cff683008390cc1173e8ed2
filzrev <103790468+filzrev@users.noreply.github.com>
2026-02-17 19:29:36 +0900

    chore: simplify nullable relating code

--- src/BenchmarkDotNet/Disassemblers/DataContracts.cs
+++ src/BenchmarkDotNet/Disassemblers/DataContracts.cs
@@ -32,8 +32,8 @@ public abstract class SourceCode
 
 public sealed class Sharp : SourceCode
 {
-    public string Text { get; set; } = default!;
-    public string FilePath { get; set; } = default!;
+    public required string Text { get; set; }
+    public required string FilePath { get; set; }
     public int LineNumber { get; set; }
 }
 
