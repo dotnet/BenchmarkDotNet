bb012d73d496a0e3c02d0438069b52fc13112df2
Tim Cassell <cassell.timothy@gmail.com>
2026-03-09 17:18:55 -0400

    Fix discoverer.

--- tests/BenchmarkDotNet.Tests/XUnit/InlineDataEnvSpecificDiscoverer.cs
+++ tests/BenchmarkDotNet.Tests/XUnit/InlineDataEnvSpecificDiscoverer.cs
@@ -9,15 +9,25 @@ public class InlineDataEnvSpecificDiscoverer : IDataDiscoverer
 {
     public IEnumerable<object[]> GetData(IAttributeInfo dataAttribute, IMethodInfo testMethod)
     {
-        // InlineDataEnvSpecific stores the test data in its first constructor parameter (object[] data)
-        // The other parameters (reason, requirements) are used for determining Skip status
-        // We just need to extract and return the data array
-        var args = dataAttribute.GetConstructorArguments().ToArray();
+        // InlineDataEnvSpecific has two constructors:
+        // 1. InlineDataEnvSpecific(object data, string reason, params EnvRequirement[] requirements)
+        // 2. InlineDataEnvSpecific(object[] data, string reason, params EnvRequirement[] requirements)
+        // GetConstructorArguments returns arguments from the constructor that was actually called
 
-        // First argument should be the object[] data
-        if (args.Length > 0 && args[0] is object[] data)
+        var args = dataAttribute.GetConstructorArguments().ToArray();
+        if (args.Length == 0)
+            yield break;
+
+        // First argument is either a single object or object[] - wrap accordingly
+        if (args[0] is object[] dataArray)
         {
-            yield return data;
+            // Array constructor was used
+            yield return dataArray;
+        }
+        else
+        {
+            // Single object constructor was used - wrap in array
+            yield return new[] { args[0] };
         }
     }
 
