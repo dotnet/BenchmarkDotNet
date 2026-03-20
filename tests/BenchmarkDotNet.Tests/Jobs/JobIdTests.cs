16e0f4f00610952098fe1d8005ab402b8490488b
filzrev <103790468+filzrev@users.noreply.github.com>
2026-03-20 19:48:01 +0900

    chore: set jobid when using inprocessattribute (#3053)

---
+++ tests/BenchmarkDotNet.Tests/Jobs/JobIdTests.cs
@@ -0,0 +1,31 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Jobs;
+using System.Linq;
+using Xunit;
+
+namespace BenchmarkDotNet.Tests.Jobs;
+
+public class JobIdTests
+{
+    [Theory]
+    [MemberData(nameof(GetTheoryData), DisableDiscoveryEnumeration = true)]
+    public void AutoGenerateJobId(string expectedId, Job job)
+    {
+        // Act
+        var result = job.ResolvedId;
+
+        // Assert
+        Assert.Equal(expectedId, result);
+    }
+
+    public static TheoryData<string, Job> GetTheoryData() => new()
+    {
+        {"InProcess", Job.InProcess },
+
+        // If baseJob don't have id. Set "InProcess".
+        {"InProcess", InProcessAttribute.GetJob(Job.Default, InProcessToolchainType.Auto, true) },
+
+        // If baseJob has exisiting id, it should not be overwritten.
+        {"Dry", InProcessAttribute.GetJob(Job.Dry, InProcessToolchainType.Auto, true) },
+    };
+}
