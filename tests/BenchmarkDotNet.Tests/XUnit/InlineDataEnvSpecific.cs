35a945e35b3e9fccd8c5f825d481c7e945bf7fec
filzrev <103790468+filzrev@users.noreply.github.com>
2026-03-29 19:51:06 +0900

    chore: fix custom inlinedata attribute

--- tests/BenchmarkDotNet.Tests/XUnit/InlineDataEnvSpecific.cs
+++ tests/BenchmarkDotNet.Tests/XUnit/InlineDataEnvSpecific.cs
@@ -1,25 +1,53 @@
 using System.Reflection;
+using Xunit;
 using Xunit.Sdk;
 using Xunit.v3;
 
 namespace BenchmarkDotNet.Tests.XUnit;
 
-[DataDiscoverer("BenchmarkDotNet.Tests.XUnit.InlineDataEnvSpecificDiscoverer", "BenchmarkDotNet.Tests")]
 [AttributeUsage(AttributeTargets.Method, AllowMultiple = true)]
 public class InlineDataEnvSpecific : DataAttribute
 {
-    readonly object[] data;
+    /// <summary>
+    /// Gets the data to be passed to the test.
+    /// </summary>
+    // If the user passes null to the constructor, we assume what they meant was a
+    // single null value to be passed to the test.
+    public object?[] Data { get; }
 
-    public InlineDataEnvSpecific(object data, string reason, params EnvRequirement[] requirements)
+    public InlineDataEnvSpecific(object? data, string reason, params EnvRequirement[] requirements)
         : this([data], reason, requirements) { }
 
-    public InlineDataEnvSpecific(object[] data, string reason, params EnvRequirement[] requirements)
+    public InlineDataEnvSpecific(object?[] data, string reason, params EnvRequirement[] requirements)
     {
-        this.data = data;
+        // If the user passes null to the constructor, we assume what they meant was a
+        // single null value to be passed to the test.
+        Data = data ?? [null];
         string? skip = EnvRequirementChecker.GetSkip(requirements);
         if (skip != null)
             Skip = $"{skip} ({reason})";
     }
 
-    public override IEnumerable<object[]> GetData(MethodInfo testMethod) => [data];
+    public override ValueTask<IReadOnlyCollection<ITheoryDataRow>> GetData(
+        MethodInfo testMethod,
+        DisposalTracker disposalTracker)
+    {
+        var traits = new Dictionary<string, HashSet<string>>(StringComparer.OrdinalIgnoreCase);
+        TestIntrospectionHelper.MergeTraitsInto(traits, Traits);
+
+        return new([
+            new TheoryDataRow(Data)
+            {
+                Explicit = ExplicitAsNullable,
+                Label = Label,
+                Skip = Skip,
+                TestDisplayName = TestDisplayName,
+                Timeout = TimeoutAsNullable,
+                Traits = traits,
+            }
+        ]);
+    }
+
+    public override bool SupportsDiscoveryEnumeration()
+        => true;
 }
