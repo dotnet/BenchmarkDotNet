902abe0dd35dc07e5d41a12c5e559f5d75fe810e
Tim Cassell <cassell.timothy@gmail.com>
2026-04-03 13:19:14 -0400

    Expose `performExtraBenchmarksRun` on `[EventPipeProfiler]` (#3075)

--- src/BenchmarkDotNet/Attributes/EventPipeProfilerAttribute.cs
+++ src/BenchmarkDotNet/Attributes/EventPipeProfilerAttribute.cs
@@ -8,9 +8,11 @@ namespace BenchmarkDotNet.Attributes
     {
         public IConfig Config { get; }
 
-        public EventPipeProfilerAttribute(EventPipeProfile profile)
+        /// <param name="profile">A named pre-defined set of provider configurations that allows common tracing scenarios to be specified succinctly.</param>
+        /// <param name="performExtraBenchmarksRun">if set to true, benchmarks will be executed one more time with the profiler attached. If set to false, there will be no extra run but the results will contain overhead.</param>
+        public EventPipeProfilerAttribute(EventPipeProfile profile, bool performExtraBenchmarksRun = true)
         {
-            Config = ManualConfig.CreateEmpty().AddDiagnoser(new EventPipeProfiler(profile));
+            Config = ManualConfig.CreateEmpty().AddDiagnoser(new EventPipeProfiler(profile, performExtraBenchmarksRun: performExtraBenchmarksRun));
         }
     }
 }
