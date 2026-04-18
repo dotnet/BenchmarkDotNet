af500020f3d43179746f07015aa7080282266bfb
filzrev <103790468+filzrev@users.noreply.github.com>
2026-04-19 07:19:06 +0900

    chore: avoid exception is thrown on output redirected

--- src/BenchmarkDotNet/ConsoleArguments/ConfigParser.cs
+++ src/BenchmarkDotNet/ConsoleArguments/ConfigParser.cs
@@ -747,6 +747,9 @@ namespace BenchmarkDotNet.ConsoleArguments
 
         private static int GetMaximumDisplayWidth()
         {
+            if (Console.IsOutputRedirected)
+                return MinimumDisplayWidth;
+
             try
             {
                 return Console.WindowWidth;
