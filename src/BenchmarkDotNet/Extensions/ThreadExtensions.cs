7d1d68a6a5545e52f985ded0a034788b449a5de8
Tim Cassell <cassell.timothy@gmail.com>
2026-04-19 10:55:38 -0400

    Fix Thread.TrySetPriority (#3093)

--- src/BenchmarkDotNet/Extensions/ThreadExtensions.cs
+++ src/BenchmarkDotNet/Extensions/ThreadExtensions.cs
@@ -1,33 +1,24 @@
-﻿using BenchmarkDotNet.Loggers;
-using System.ComponentModel;
-using System.Diagnostics.CodeAnalysis;
+using BenchmarkDotNet.Loggers;
 
-namespace BenchmarkDotNet.Extensions
+namespace BenchmarkDotNet.Extensions;
+
+internal static class ThreadExtensions
 {
-    // we need it public to reuse it in the auto-generated dll
-    // but we hide it from intellisense with following attribute
-    [EditorBrowsable(EditorBrowsableState.Never)]
-    public static class ThreadExtensions
+    public static bool TrySetPriority(this Thread thread, ThreadPriority priority, ILogger logger)
     {
-        [SuppressMessage("ReSharper", "UnusedMethodReturnValue.Global")] // TODO: check result
-        public static bool TrySetPriority(
-            this Thread thread,
-            ThreadPriority priority,
-            ILogger logger)
+        if (!thread.IsAlive)
         {
-            try
-            {
-                thread.Priority = priority;
-                return true;
-            }
-            catch (Exception ex)
-            {
-                logger.WriteLineError(
-                    $"// ! Failed to set up priority {priority} for thread {thread}. Make sure you have the right permissions. Message: {ex.Message}");
-            }
-
             return false;
-
+        }
+        try
+        {
+            thread.Priority = priority;
+            return true;
+        }
+        catch (Exception ex)
+        {
+            logger.WriteLineError($"// ! Failed to set up priority {priority} for thread {thread}. Make sure you have the right permissions. Message: {ex.Message}");
+            return false;
         }
     }
 }
