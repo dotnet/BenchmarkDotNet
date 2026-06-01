fdf082e7542e30c743bfed9387a2380a21541766
filzrev <103790468+filzrev@users.noreply.github.com>
2026-06-01 18:50:15 +0900

    chore: add consolecapture setting and parallel setting

--- tests/BenchmarkDotNet.IntegrationTests.ManualRunning/Properties/AssemblyInfo.cs
+++ tests/BenchmarkDotNet.IntegrationTests.ManualRunning/Properties/AssemblyInfo.cs
@@ -1,6 +1,6 @@
 using System.Runtime.InteropServices;
 
-[assembly: Guid("74362bb1-9f64-4be5-b079-b4ac19dae5db")]
+[assembly: Guid("4d7c0994-2750-45dc-bbe8-750b158ea826")]
 
 [assembly: CaptureConsole]
 [assembly: CollectionBehavior(CollectionBehavior.CollectionPerAssembly, DisableTestParallelization = true)]
