b374e60bd2f03dd7b0ceafd37068c7bbdcaab634
bugRanger <simpledowork@gmail.com>
2026-06-24 19:15:17 +0700

    Fix doc comment for profile

--- src/BenchmarkDotNet/Diagnosers/IHardwareCounterProfile.cs
+++ src/BenchmarkDotNet/Diagnosers/IHardwareCounterProfile.cs
@@ -1,9 +1,11 @@
 namespace BenchmarkDotNet.Diagnosers;
 
 /// <summary>
-/// Предоставляет доступ к настройке профиля счетчика,
-/// т.к. счетчик может иметь различное именование в зависимости от модели CPU или может иметь различные версии с более детальной информацией.
+/// Hardware counter profile.
 /// </summary>
+/// <remarks>
+/// Use a profile when the counter in the environment under test has a different name or can provide multiple counters with more detailed information.
+/// </remarks>
 public interface IHardwareCounterProfile
 {
     IEnumerable<string> GetVariants(HardwareCounter hardwareCounter);
