cc01d626de2ef9c3dd2516045f984f151c2ab03b
filzrev <103790468+filzrev@users.noreply.github.com>
2026-07-19 17:25:53 +0900

    chore: update parallel settings that introduced on xunit v3 4.0.0-pre.154

--- tests/BenchmarkDotNet.IntegrationTests/Properties/AssemblyInfo.cs
+++ tests/BenchmarkDotNet.IntegrationTests/Properties/AssemblyInfo.cs
@@ -1,6 +1,9 @@
 using System.Runtime.InteropServices;
+using Xunit.Sdk;
+using Xunit.v3;
 
 [assembly: Guid("74362bb1-9f64-4be5-b079-b4ac19dae5db")]
 
 [assembly: CaptureConsole]
-[assembly: CollectionBehavior(CollectionBehavior.CollectionPerAssembly, DisableTestParallelization = true)]
+[assembly: CollectionBehavior(CollectionBehavior.CollectionPerAssembly)]
+[assembly: Parallelization(Mode = ParallelMode.None)]
