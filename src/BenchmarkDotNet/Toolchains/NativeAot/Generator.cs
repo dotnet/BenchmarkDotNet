c434162a3218e39fa6785dd8416f6013f1f86a3c
filzrev <103790468+filzrev@users.noreply.github.com>
2026-07-20 04:00:16 +0900

    chore: disable mtp and xunit entrypoint generation on benchmark project

--- src/BenchmarkDotNet/Toolchains/NativeAot/Generator.cs
+++ src/BenchmarkDotNet/Toolchains/NativeAot/Generator.cs
@@ -151,7 +151,6 @@ namespace BenchmarkDotNet.Toolchains.NativeAot
                 <UseSharedCompilation>false</UseSharedCompilation>
                 <Deterministic>true</Deterministic>
                 <RunAnalyzers>false</RunAnalyzers>
-                <IsTestingPlatformApplication>false</IsTestingPlatformApplication>
                 <PublishAot Condition="$([MSBuild]::VersionGreaterThan('$(NETCoreSdkVersion)', '6.0'))">true</PublishAot>
                 <IlcOptimizationPreference>{ilcOptimizationPreference}</IlcOptimizationPreference>
                 <OptimizationPreference>{ilcOptimizationPreference}</OptimizationPreference>
