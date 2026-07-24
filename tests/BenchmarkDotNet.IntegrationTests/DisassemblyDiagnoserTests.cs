2fbd7ff10f0fa67d65e8a2ddd628c5adebe5e252
filzrev <103790468+filzrev@users.noreply.github.com>
2026-07-25 04:01:11 +0900

    chore: add workaround code when benchmark method got inlined

--- tests/BenchmarkDotNet.IntegrationTests/DisassemblyDiagnoserTests.cs
+++ tests/BenchmarkDotNet.IntegrationTests/DisassemblyDiagnoserTests.cs
@@ -163,7 +163,10 @@ namespace BenchmarkDotNet.IntegrationTests
 
             CanExecute<WithInlineable>(CreateConfig(jit, platform, toolchain, disassemblyDiagnoser, RunStrategy.Monitoring));
 
-            var disassemblyResult = disassemblyDiagnoser.Results.Values.Single(result => result.Methods.Count(method => method.Name.Contains(nameof(WithInlineable.JustReturn))) == 1);
+            var disassemblyResult = disassemblyDiagnoser.Results.Values.SingleOrDefault(result => result.Methods.Count(method => method.Name.Contains(nameof(WithInlineable.JustReturn))) == 1);
+
+            if (disassemblyResult == null)
+                disassemblyResult = disassemblyDiagnoser.Results.Values.Single(); // Benchmark method is got inlined.
 
             Assert.Empty(disassemblyResult.Errors);
             Assert.Contains(disassemblyResult.Methods, method => method.Maps.Any(map => map.SourceCodes.OfType<Asm>().All(asm => asm.ToString()!.Contains("ret"))));
