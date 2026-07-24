d56ebbf2b9f80c49be97a7f7e593a015805af2e9
filzrev <103790468+filzrev@users.noreply.github.com>
2026-07-25 08:46:08 +0900

    chore: remove temporary workaround setting for macos from ThreadingDiagnoserTests

--- tests/BenchmarkDotNet.IntegrationTests/ThreadingDiagnoserTests.cs
+++ tests/BenchmarkDotNet.IntegrationTests/ThreadingDiagnoserTests.cs
@@ -185,10 +185,6 @@ namespace BenchmarkDotNet.IntegrationTests
                 // precision = 3 is not enough (e.g., sometimes the actual value may be equal 1.0009765625 while the expected value is 1.0)
                 int precision = 2;
 
-                // On macos, there are some additional background operations (https://github.com/dotnet/BenchmarkDotNet/issues/3098)
-                if (OsDetector.IsMacOS())
-                    precision = 1;
-
                 Assert.Equal(assertion.Value.expectedValue, metric.Value.Value, precision);
             }
         }
