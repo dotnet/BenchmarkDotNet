da1b1acc05eb9948fa313d5cedae61ac5f7575e8
filzrev <103790468+filzrev@users.noreply.github.com>
2026-07-29 01:32:06 +0900

    chore: update PerfonarTest based on latest update

--- tests/BenchmarkDotNet.Tests/Perfonar/PerfonarTests.cs
+++ tests/BenchmarkDotNet.Tests/Perfonar/PerfonarTests.cs
@@ -24,7 +24,7 @@ public class PerfonarTests(ITestOutputHelper output)
     {
         var table = TableDataMap[key];
         string markdown = table.ToMarkdown(new PerfonarTableStyle());
-        string json = LightJsonSerializer.Serialize(table.RootEntry, new LightJsonSettings { Indent = true }).Replace("\r\n","\n");
+        string json = LightJsonSerializer.Serialize(table.RootEntry, new LightJsonSettings { Indent = true, NewLine = "\n" });
         return VerifyString(key, markdown + "\n" + json);
     }
 
