769d17dacc049c66fa5cc2384a03cec59c57e7fd
Ifeanyi Shadrach Odom <ifeanyishadrach452@gmail.com>
2026-08-24 08:48:00 +0100

    Refactor JobConfigBaseAttribute for lazy config & categories
    Refactor JobConfigBaseAttribute to use lazy config initialization and support job categories. Add a Categories property for job filtering and update constructors to accommodate these changes. Improve comments and code clarity.

--- src/BenchmarkDotNet/Attributes/Jobs/JobConfigbaseAttribute.cs
+++ src/BenchmarkDotNet/Attributes/Jobs/JobConfigbaseAttribute.cs
@@ -9,13 +9,27 @@ namespace BenchmarkDotNet.Attributes
     [AttributeUsage(AttributeTargets.Class | AttributeTargets.Assembly)]
     public class JobConfigBaseAttribute : Attribute, IConfigSource
     {
+        private readonly Job? job;
+        private IConfig? config;
+
         // CLS-Compliant Code requires a constructor which use only CLS-compliant types
         [PublicAPI]
-        public JobConfigBaseAttribute() => Config = ManualConfig.CreateEmpty();
+        public JobConfigBaseAttribute() { }
 
-        protected JobConfigBaseAttribute(Job job) => Config = ManualConfig.CreateEmpty().AddJob(job);
+        protected JobConfigBaseAttribute(Job job) => this.job = job;
 
-        public IConfig Config { get; }
+        /// <summary>
+        /// The categories of the job defined by this attribute. Categories are metadata used to select which jobs are
+        /// executed (see <see cref="BenchmarkDotNet.Filters.JobCategoryFilter"/>), they don't affect how the job is executed.
+        /// </summary>
+        [PublicAPI] public string[] Categories { get; set; } = [];
+
+        // Named attribute properties are assigned after the constructor has run, so the config cannot be built there:
+        // Categories would still be empty. It is read long after the attribute is constructed, so building it lazily
+        // is enough to see the categories the user has set.
+        public IConfig Config => config ??= job == null
+            ? ManualConfig.CreateEmpty()
+            : ManualConfig.CreateEmpty().AddJob(Categories.Length == 0 ? job : job.WithCategories(Categories).Freeze());
 
         protected static Job GetJob(Job sourceJob, RuntimeMoniker runtimeMoniker, Jit? jit, Platform? platform)
         {
