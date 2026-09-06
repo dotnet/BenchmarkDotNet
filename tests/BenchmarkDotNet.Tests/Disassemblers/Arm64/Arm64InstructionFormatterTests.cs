a1233fce504da92290ce1ee79dff8db840e18f0d
filzrev <103790468+filzrev@users.noreply.github.com>
2026-09-07 08:08:30 +0900

    chore: update AsmArm64 version and update related tests

--- tests/BenchmarkDotNet.Tests/Disassemblers/Arm64/Arm64InstructionFormatterTests.cs
+++ tests/BenchmarkDotNet.Tests/Disassemblers/Arm64/Arm64InstructionFormatterTests.cs
@@ -2,26 +2,33 @@
 #if NET
 using AsmArm64;
 using AwesomeAssertions;
+using BenchmarkDotNet.Diagnosers;
 using BenchmarkDotNet.Disassemblers;
 using Iced.Intel;
-using System.Text.RegularExpressions;
 using static AsmArm64.Arm64RegisterW;
 using static AsmArm64.Arm64RegisterX;
-
 namespace BenchmarkDotNet.Tests.Disassemblers.Arm64;
 
-public partial class Arm64InstructionFormatterTests
+public partial class Arm64InstructionFormatterTests : Arm64DisassemblerTestBase
 {
-    private readonly ITestOutputHelper Output;
-
-    private static readonly FormatterOptions FormatterOptions = new()
+    private static readonly FormatterOptions DefaultFormatterOptions = new()
     {
-        // FirstOperandCharIndex = 10, // Use DisassemblyDiagnoserConfig default config value.
+        FirstOperandCharIndex = 10, // Use DisassemblyDiagnoserConfig default config value.
+    };
+
+    private static readonly Arm64InstructionFormattingOptions AsmArm64FormattingOptions = new()
+    {
+        AliasMode = Arm64InstructionAliasMode.BaseInstruction,
+        UseUppercaseText = false,
+        UseUppercaseHex = false,
+        ImmediateFormat = Arm64NumericFormat.Hexadecimal,
+        MemoryOffsetFormat = Arm64NumericFormat.Hexadecimal,
+        LabelOffsetFormat = Arm64NumericFormat.Auto,
     };
 
     public Arm64InstructionFormatterTests(ITestOutputHelper output)
+        : base(output)
     {
-        Output = output;
     }
 
     [Theory]
@@ -32,72 +39,95 @@ public partial class Arm64InstructionFormatterTests
         var asm = rawInstruction.ToArm64Asm(address: 0);
 
         // Act
-        var result = asm.Format(FormatterOptions);
+        var result = asm.Format(new FormatterOptions());
 
         // Assert
         result.Should().Be(expected);
 
         // Verify AsmArm64 compatibility
         var instruction = rawInstruction.ToAsmArm64Instruction();
-        var text = instruction.ToString("X", null);
-        text = ReplaceNoCompatibleText(instruction, text);
+        var text = instruction.ToString(AsmArm64FormattingOptions);
 
         text.Should().Be(expected);
-
     }
 
     [Fact]
-    public void FormatInstruction_B_WithReferencedAddress()
+    public void FormatInstruction_IntroDisassembly_SumLocal()
     {
         // Arrange
-        var rawInstruction = Arm64InstructionFactory.B(0x100);
-        var asm = rawInstruction.ToArm64Asm(0, 0x10_000);
-
-        var options = new FormatterOptions()
+        var rawInstructions = new[]
         {
-            FirstOperandCharIndex = 6,
+            Arm64InstructionFactory.STP(X29, X30, new Arm64ImmediateMemoryAccessor(Arm64RegisterSP.SP, -0x10).Pre),
+            Arm64InstructionFactory.MOV(X29, Arm64RegisterSP.SP),
+            Arm64InstructionFactory.LDR(X0, new Arm64ImmediateMemoryAccessor(X0, 8)),
+            Arm64InstructionFactory.MOV(W1, WZR),
+            Arm64InstructionFactory.LDR(W2, new Arm64ImmediateMemoryAccessor(X0, 8)),
+            Arm64InstructionFactory.CMP(W2, 0),
+            Arm64InstructionFactory.B(Arm64ConditionalKind.LE, 24),
+            Arm64InstructionFactory.ADD(X0,X0, 0x10),
+            // M00_L00:
+            Arm64InstructionFactory.LDR(W3, new Arm64BaseMemoryAccessor(X0), 4),
+            Arm64InstructionFactory.ADD(W1,W3,W1),
+            Arm64InstructionFactory.SUB(W2,W2, 1),
+            Arm64InstructionFactory.CBNZ(W2, -12),
+            // M00_L01:
+            Arm64InstructionFactory.MOV(W0, W1),
+            Arm64InstructionFactory.LDP(X29, X30, new Arm64BaseMemoryAccessor(Arm64RegisterSP.SP), 0x10),
+            Arm64InstructionFactory.RET()
         };
+        PrintInstructions(rawInstructions);
 
+        const ulong BaseAddress = 0xFF01097000A0;
         var symbols = new Dictionary<ulong, string>
         {
+            [0xFF01097000C0] = "M00_L00",
+            [0xFF01097000D0] = "M00_L01",
         };
+        Arm64Asm[] asms = GetArm64Asms(rawInstructions, BaseAddress, symbols);
 
         // Act
-        var result = Arm64InstructionFormatter.Format(asm, options, printInstructionAddresses: true, pointerSize: 8, symbols);
+        List<string> results = new();
+        foreach (var asm in asms)
+        {
+            var result = Arm64InstructionFormatter.Format(asm, DefaultFormatterOptions, printInstructionAddresses: true, pointerSize: 8, symbols);
+            results.Add(result);
+        }
 
-        // TODO:
         // Assert
-        result.Should().Be("0 b     #0x100");
+        results.Should().BeEquivalentTo(
+        [
+            "FF01097000A0 stp       x29, x30, [sp, #-0x10]!",
+            "FF01097000A4 mov       x29, sp",
+            "FF01097000A8 ldr       x0, [x0, #8]",
+            "FF01097000AC mov       w1, wzr",
+            "FF01097000B0 ldr       w2, [x0, #8]",
+            "FF01097000B4 cmp       w2, #0",
+            "FF01097000B8 b.le      M00_L01",
+            "FF01097000BC add       x0, x0, #0x10",
+            // M00_L00:
+            "FF01097000C0 ldr       w3, [x0], #4",
+            "FF01097000C4 add       w1, w3, w1",
+            "FF01097000C8 sub       w2, w2, #1",
+            "FF01097000CC cbnz      w2, M00_L00",
+            // M00_L01:
+            "FF01097000D0 mov       w0, w1",
+            "FF01097000D4 ldp       x29, x30, [sp], #0x10",
+            "FF01097000D8 ret       ",
+        ]);
     }
 
-    // Temporary workaround code to pass AsmArm64/Capstone compatibility test.
-    // See: https://github.com/xoofx/AsmArm64/issues/15
-    private static string ReplaceNoCompatibleText(Arm64Instruction instruction, string text)
-    {
-        // Arm64LabelOffset seems not support `X` format, so we need to manually replace label from decimal format to hex format
-        if (instruction.Flags.HasFlag(Arm64InstructionFlags.HasLabel))
+    private static Arm64Asm[] GetArm64Asms(uint[] rawInstructions, ulong baseAddress, Dictionary<ulong, string> symbols)
+    {      
+        var clrRuntime = CreateMockClrRuntime(0);
+        var state = new State(clrRuntime, DummyTargetFrameworkVersion);
+        foreach (var symbol in symbols)
         {
-            text = Regex.Replace(text, @"#(-?\d+)\b", m =>
-            {
-                var value = int.Parse(m.Groups[1].Value);
-                return Math.Abs(value) < 10
-                    ? $"#{m.Groups[1].Value}"
-                    : $"#0x{value:X}";
-            });
+            state.AddressToNameMapping.Add(symbol.Key, symbol.Value);
         }
 
-        // AsmArm64 use aliases for mnemonic for some instructions, so we need to replace them with the original instruction name
-        switch (instruction.Id)
-        {
-            case Arm64InstructionId.MOV_movz_32_movewide:
-            case Arm64InstructionId.MOV_movz_64_movewide:
-                text = Regex.Replace(text, "^mov", "movz");
-                break;
-            default:
-                break;
-        }
-
-        return text;
+        var bytes = rawInstructions.ToLittleEndianBytes();
+        var helper = new Arm64DisassemblerHelper();
+        return helper.Decode(bytes, baseAddress, state, depth: 0, DummyCurrentMethod, DisassemblySyntax.Masm);
     }
 
     public static class TestData
@@ -106,12 +136,13 @@ public partial class Arm64InstructionFormatterTests
         {
             { Arm64InstructionFactory.B(0x8),                     "b #8"}, // Label value that lower than 0x10 use decimal format.
             { Arm64InstructionFactory.B(0x100),                   "b #0x100"},
-            // TODO: MOVZ instruction is printed as `mov` in AsmArm64.
-            { Arm64InstructionFactory.MOVZ(W0, 0x100),            "movz w0, #0x100"},
+            { Arm64InstructionFactory.MOVZ(W0, 0x8),              "movz w0, #0x8"},
             { Arm64InstructionFactory.MOVZ(X0, 0x100),            "movz x0, #0x100"},
-            // TODO: MOVZ with shift amount representation is different in AsmArm64.
-            // { Arm64InstructionFactory.MOVZ(X0, 0x100, amount:16), "movz x0, #0x100, lsl #16"}
-            // TODO: Minus label offset representation is different between AsmArm64 and Gee.External.Capstone.  
+            { Arm64InstructionFactory.MOVZ(X0, 0xFFF),            "movz x0, #0xfff"},
+            { Arm64InstructionFactory.MOVZ(X0, 0x100),            "movz x0, #0x100"},
+            { Arm64InstructionFactory.MOVZ(X0, 0x100, amount:16), "movz x0, #0x100, lsl #16"},
+
+            // TODO: `Gee.External.Capstone` seems not support negative label offset and wrong text is outputted. 
             // { Arm64InstructionFactory.B(-8),                      "b #-8"}, // Gee.External.Capstone don't support minus label offset.
         };
     }
